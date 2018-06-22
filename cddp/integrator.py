0002913118649f9d67d91b52b7bb0fcc04700321
Carlos Mastalli <carlos.mastalli@laas.fr>
2018-06-22 19:24:21 +0200

    [core] Using the standard order in the integrator class
    There are other little changes:
    1. Assuming the system is time-invariant, just to follow current
    assumptions. Note that it might change
    2. Added documentation to the integrator classes

--- cddp/integrator.py
+++ cddp/integrator.py
@@ -1,46 +1,52 @@
-from abc import ABCMeta, abstractmethod
+import abc
 
 
-class IntegratorBase:
-  __metaclass__ = ABCMeta
+class Integrator(object):
+  """ This abstract class declared the virtual method for any integrator.
+  """
+  __metaclass__ = abc.ABCMeta
 
   def __init__(self):
     pass
 
-  abstractmethod
-  def integrate(self,model,data,t,x0,u,dt):
-    pass
+  @abc.abstractmethod
+  def integrate(self, model, data, x, u, dt): pass
 
-class EulerIntegrator(IntegratorBase):
 
+class EulerIntegrator(Integrator):
+  """ Integrates the function using the forward Euler method
+  """
   @staticmethod
-  def integrate(model,data,t,x0,u,dt):
-    return x0 + model.f(x0,u,data)*dt
+  def integrate(model, data, x, u, dt):
+    return x + model.f(data, x, u) * dt
 
-class RK4Integrator(IntegratorBase):
 
+class RK4Integrator(Integrator):
+  """ Integrates the function using the fourth-order Runge-Kutta method
+  """
   @staticmethod
-  def integrate(model,data,t,x0,u,dt):
-    k1 = model.f(x0,u,data)
+  def integrate(model, data, x, u, dt):
+    k1 = model.f(data, x, u)
 
-    x2 = x0 + dt/2.*k1
-    k2 = model.f(x2,u,data)
+    x2 = x + dt / 2. * k1
+    k2 = model.f(data, x2, u)
 
-    x3 = x0 + dt/2.*k2
-    k3 = model.f(x3,u,data)
+    x3 = x + dt / 2. * k2
+    k3 = model.f(data, x3, u)
 
-    x4 = x0 + dt*k3
-    k4 = model.f(x4,u,data)
-    return x0 + dt/6.*(k1 + 2.*k2 + 2.*k3 + k4)
+    x4 = x + dt * k3
+    k4 = model.f(data, x4, u)
+    return x + dt / 6. * (k1 + 2. * k2 + 2. * k3 + k4)
 
-def computeFlow(integrator,model,data,timeline,x0,controls):
+
+def computeFlow(integrator, model, data, timeline, x, controls):
   N = len(timeline)
-  x_flow = [None]*N
-  x_flow[0] = x0
+  x_flow = [None] * N
+  x_flow[0] = x
   for k in range(len(timeline)-1):
     dt = timeline[k+1] - timeline[k]
     t = timeline[k]
     control = controls[k]
-    x_flow[k+1] = integrator.integrate(model,data,t,x_flow[k],control,dt)
+    x_flow[k+1] = integrator.integrate(model, data, t, x_flow[k], control, dt)
 
   return x_flow
