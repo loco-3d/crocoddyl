292d48c743aca0ea72f020d4c92c07a699849895
Carlos Mastalli <carlos.mastalli@laas.fr>
2018-08-08 18:17:00 +0200

    [wip] Cleaned up a bit the script for testing the arm benchmark
    This scripts is starting to works. It's needed to futher check the
    solver, to add visualization option and to reorganize it.

--- models/arm.py
+++ models/arm.py
@@ -44,97 +44,75 @@ class Arm(cddp.NumDiffDynamicalSystem):
 
 
 
-
-
-
-
-
-
-
-# np.set_printoptions(linewidth=400, suppress=True, threshold=np.nan)
-# import rospkg
-# path = rospkg.RosPack().get_path('talos_data')
-# urdf = path + '/robots/talos_left_arm.urdf'
-
-# dynamics = Arm(urdf, path)
-# x0 = np.zeros((dynamics.getConfigurationDimension(), 1))
-# u0 = np.random.rand(dynamics.getControlDimension(), 1)
-# x0[0:7] = np.array([ [0.1], [0.2], [0.3], [0.4], [0.5], [0.6], [0.7] ])
-# # x0[10] = 2.
-
-# data = dynamics.createData()
-# # print dynamics.f(data, x0, u0)
-# # fx, fu = dynamics.computeDerivatives(data, x0, u0, 0.01)
-# fx = dynamics.fx(data, x0, u0)
-# fu = dynamics.fu(data, x0, u0)
-# # print fx
-# # print fu
-# # print u0
-
-# delta_x = 0.001*np.ones((dynamics.getConfigurationDimension(), 1))
-# delta_u = 0.001*np.ones((dynamics.getControlDimension(), 1))
-# f0 = fx * delta_x + fu * delta_u
-# f1 = dynamics.f(data, x0, u0).copy()
-# f2 = dynamics.f(data, x0 + delta_x, u0 + delta_u).copy()
-# print f0.T 
-# print (f2 - f1).T
-# print (f0 - (f2 - f1)).T
-
-
-
-
 # # class SE3Task
+import math
 
+class SE3RunningCost(cddp.RunningResidualQuadraticCost):
+  def __init__(self, robot, ee_frame, M_des):
+    self.robot = robot
+    self._frame_idx = self.robot.model.getFrameId(ee_frame)
+    self.M_des = M_des
+    cddp.RunningResidualQuadraticCost.__init__(self, 6)
 
-# class SE3RunningCost(cddp.RunningResidualQuadraticCost):
-#   def __init__(self, robot, ee_frame, M_des):
-#     self.robot = robot
-#     self._frame_idx = self.robot.model.getFrameId(ee_frame)
-#     self.M_des = M_des
-#     cddp.RunningResidualQuadraticCost.__init__(self, 6)
-  
-#   def r(self, data, x, u):
-#     q = x[:self.robot.nq]
-#     np.copyto(data.r,
-#       se3.log(self.M_des.inverse() * self.robot.framePosition(q, self._frame_idx)).vector)
-#     return data.r
-  
-#   def rx(self, data, x, u):
-#     q = x[:self.robot.nq]
-#     data.rx[:, :self.robot.nq] = \
-#       se3.jacobian(self.robot.model, self.robot.data, q,
-#                   self.robot.model.frames[self._frame_idx].parent, False, True)
-#     return data.rx
+  def r(self, data, x, u):
+    q = x[:self.robot.nq]
+    np.copyto(data.r,
+      se3.log(self.M_des.inverse() * self.robot.framePosition(q, self._frame_idx)).vector)
+    return data.r
 
-#   def ru(self, data, x, u):
-#     return data.ru
+  def rx(self, data, x, u):
+    q = x[:self.robot.nq]
+    data.rx[:, :self.robot.nq] = \
+      se3.jacobian(self.robot.model, self.robot.data, q,
+                   self.robot.model.frames[self._frame_idx].parent, False, True)
+    return data.rx
 
-# frame_name = 'gripper_left_joint'
-# M_des = se3.SE3(np.eye(3), np.array([ [0.], [0.], [0.5] ]))
-# se3_cost = SE3RunningCost(dynamics.robot, frame_name, M_des)
-# cost_data = se3_cost.createData(data.nv, data.m)
-# se3_cost.r(cost_data, 0.*x0, u0)
-# print se3_cost.rx(cost_data, 0.*x0, u0)
-
-# w_se3 = np.ones(6)
-# se3_cost.setWeights(w_se3)
+  def ru(self, data, x, u):
+    return data.ru
 
 
 
 
 
-# from simple_cost import StateControlQuadraticRegularization
-# cost_manager = cddp.CostManager()
 
+np.set_printoptions(linewidth=400, suppress=True, threshold=np.nan)
 
-# wx = 1e-4 * np.hstack([ np.zeros(dynamics.robot.nq), np.ones(dynamics.robot.nv) ])
-# wu = 1e-4 * np.ones(dynamics.getControlDimension())
-# xu_reg = StateControlQuadraticRegularization()
-# xu_reg.setWeights(wx, wu)
+# Creating the system model
+import rospkg
+path = rospkg.RosPack().get_path('talos_data')
+urdf = path + '/robots/talos_left_arm.urdf'
+system = Arm(urdf, path)
+x0 = np.zeros((system.getConfigurationDimension(), 1))
 
-# cost_manager.addRunning(xu_reg)
-# cost_manager.addRunning(se3_cost)
+# Defining the SE3 task
+frame_name = 'gripper_left_joint'
+M_des = se3.SE3(np.eye(3), np.array([ [0.], [0.], [0.5] ]))
+se3_cost = SE3RunningCost(system.robot, frame_name, M_des)
+w_se3 = np.ones(6)
+se3_cost.setWeights(w_se3)
 
-# timeline = np.arange(0.0, 1., 0.01)  # np.linspace(0., 0.5, 51)
-# # ddp = cddp.DDP(dynamics, cost_manager, timeline)
-# # ddp.compute(0.*x0)
+# Defining the velocity and control regularization
+from simple_cost import StateControlQuadraticRegularization
+xu_reg = StateControlQuadraticRegularization()
+wx = 1e-4 * np.hstack([ np.zeros(system.robot.nq), np.ones(system.robot.nv) ])
+wu = 1e-4 * np.ones(system.getControlDimension())
+xu_reg.setWeights(wx, wu)
+
+# Adding the cost functions to the cost manager
+cost_manager = cddp.CostManager()
+cost_manager.addRunning(xu_reg)
+cost_manager.addRunning(se3_cost)
+
+# Setting up the DDP problem
+timeline = np.arange(0.0, 0.25, 1e-3)  # np.linspace(0., 0.5, 51)
+ddp = cddp.DDP(system, cost_manager, timeline)
+
+# Solving the problem
+ddp.compute(x0)
+
+# Printing the final goal
+robot = se3.robot_wrapper.RobotWrapper(urdf, path)
+frame_idx = robot.model.getFrameId(frame_name)
+xf = ddp.intervals[-1].x
+qf = xf[:7]
+print robot.framePosition(qf, frame_idx)
