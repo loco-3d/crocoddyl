16aae9de457414a4fc3bef9b10e9479399516a5b
Carlos Mastalli <carlos.mastalli@laas.fr>
2018-08-09 14:39:31 +0200

    [wip] Added visualization of the arm planned motion for debugging

--- models/arm.py
+++ models/arm.py
@@ -77,16 +77,31 @@ class SE3RunningCost(cddp.RunningResidualQuadraticCost):
 
 np.set_printoptions(linewidth=400, suppress=True, threshold=np.nan)
 
+display = True
+if display:
+  import gepetto.corbaserver
+  cl = gepetto.corbaserver.Client()
+  gui = cl.gui
+
+
 # Creating the system model
 import rospkg
 path = rospkg.RosPack().get_path('talos_data')
 urdf = path + '/robots/talos_left_arm.urdf'
 system = Arm(urdf, path)
 x0 = np.zeros((system.getConfigurationDimension(), 1))
+x0[:system.robot.nq] = np.matrix([ 0.173046, 1., -0.525366, 0., 0., 0.1,-0.005]).T
+
+robot = system.robot
+if display:
+  if gui.nodeExists("world"):
+    gui.deleteNode("world",True)
+  robot.initDisplay(loadModel=True)
+  robot.display(x0[:robot.nq])
 
 # Defining the SE3 task
 frame_name = 'gripper_left_joint'
-M_des = se3.SE3(np.eye(3), np.array([ [0.], [0.], [0.5] ]))
+M_des = se3.SE3(np.eye(3), np.array([ [0.], [0.], [0.4] ]))
 se3_cost = SE3RunningCost(system.robot, frame_name, M_des)
 w_se3 = np.ones(6)
 se3_cost.setWeights(w_se3)
@@ -116,3 +131,25 @@ frame_idx = robot.model.getFrameId(frame_name)
 xf = ddp.intervals[-1].x
 qf = xf[:7]
 print robot.framePosition(qf, frame_idx)
+
+gui.refresh()
+ball_size = 0.04
+traj_node = "world/ee_ball"
+if gui.nodeExists(traj_node):
+  gui.deleteNode(traj_node,True)
+gui.addSphere(traj_node, ball_size, [0.,1.,0.,1.])
+if display:
+  from time import sleep
+  robot.initDisplay(loadModel=True)
+  it = 0
+  for k in ddp.intervals:
+    qk = k.x[:robot.nq]
+    robot.display(qk)
+    M_pos = robot.framePosition(qk, frame_idx)
+    gui.applyConfiguration(traj_node, se3.utils.se3ToXYZQUAT(M_pos))
+    gui.refresh()
+
+    # gui.addSphere(traj_node+str(it), ball_size/5, [0.5,0.5,0.,1.])
+    # gui.applyConfiguration(traj_node+str(it), se3.utils.se3ToXYZQUAT(M_pos))
+    sleep(1e-3)
+    it += 1
