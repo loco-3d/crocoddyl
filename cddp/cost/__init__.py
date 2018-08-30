5c6e61cbd4468a2efc920c2e8808679ee04385fd
Carlos Mastalli <carlos.mastalli@laas.fr>
2018-08-30 18:59:38 +0200

    [cleanup Renamed cost files

--- cddp/cost/__init__.py
+++ cddp/cost/__init__.py
@@ -2,6 +2,6 @@ from cost import *
 from soft_constraint import *
 from cost_manager import *
 from quadratic_cost import *
-from se3 import *
-from goal_xu import *
+from lie_cost import *
+from xu_cost import *
 from joint_lim import *
