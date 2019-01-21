954729a002de90eff49996d15e226f1a1b79fbeb
Rohan Budhiraja <proyan@users.noreply.github.com>
2019-01-21 23:24:55 +0100

    [locomotion problem][initial guess] update comment

--- unittest/conf_talos_1step.py
+++ unittest/conf_talos_1step.py
@@ -18,5 +18,8 @@ TRAJ_DIR = os.getcwd()+"/traj_1step/"
 
 X_init = pickle.load( open(TRAJ_DIR+"X_init.out","rb")) #loads the state x
 U_init = pickle.load( open(TRAJ_DIR+"U_init.out","rb")) # loads the control u
-f_init = pickle.load( open(TRAJ_DIR+"f_init.out","rb")) #loads the forces ("lambda") in order LF, RF
+
+#loads the forces ("lambda") in the local frame in order LF, RF
+#where lf: 'leg_left_6_joint', 'rf' : 'leg_right_6_joint'
+f_init = pickle.load( open(TRAJ_DIR+"f_init.out","rb")) 
 ddq_init = pickle.load( open(TRAJ_DIR+"ddq_init.out","rb")) #loads the acceleration
