c009e160c3383ab22522dc042e43e84f1a5cda2d
Mansard <nmansard@laas.fr>
2019-01-21 18:38:27 +0100

    [refact] Reintroduced armature in contact problem to make unittest work again.

--- unittest/contact.py
+++ unittest/contact.py
@@ -15,6 +15,10 @@ absmax = lambda A: np.max(abs(A))
 absmin = lambda A: np.min(abs(A))
 
 robot = loadTalosArm(freeFloating=True)
+robot.model.armature = np.matrix([ 0 ]*robot.model.nv).T
+for j in robot.model.joints[1:]:
+    if j.shortname()!='JointModelFreeFlyer':
+        robot.model.armature[j.idx_v:j.idx_v+j.nv]=1
 qmin = robot.model.lowerPositionLimit; qmin[:7]=-1; robot.model.lowerPositionLimit = qmin
 qmax = robot.model.upperPositionLimit; qmax[:7]= 1; robot.model.upperPositionLimit = qmax
 
@@ -662,6 +666,7 @@ model.calc(data,x,u)
 assert( len(filter(lambda x:x>0,eig(data.K)[0])) == model.nv )
 assert( len(filter(lambda x:x<0,eig(data.K)[0])) == model.ncontact )
 _taucheck = pinocchio.rnea(rmodel,rdata,q,v,a2m(data.a),data.contact.forces)
+_taucheck.flat[:] += rmodel.armature.flat*data.a
 assert( absmax(_taucheck[:6])<1e-6 )
 assert( absmax(m2a(_taucheck[6:])-u)<1e-6 )
 
