5ca92e7c3156faef373476ce4a0f12735444a737
Mansard <nmansard@laas.fr>
2019-01-22 15:44:46 +0100

    Test_track: add 6d cost.

--- unittest/test_ddp_track.py
+++ unittest/test_ddp_track.py
@@ -3,7 +3,7 @@ from pinocchio.utils import *
 from numpy.linalg import inv,pinv,norm,svd,eig
 from numpy import dot,asarray
 import warnings
-from crocoddyl import CostModelSum,CostModelPosition,CostModelState,CostModelControl,DifferentialActionModelFloatingInContact,IntegratedActionModelEuler,ActuationModelFreeFloating,StatePinocchio,ContactModel6D,ContactModelMultiple,ActivationModelWeightedQuad,m2a,a2m,CostModelPlacementVelocity
+from crocoddyl import CostModelSum,CostModelPosition,CostModelState,CostModelControl,DifferentialActionModelFloatingInContact,IntegratedActionModelEuler,ActuationModelFreeFloating,StatePinocchio,ContactModel6D,ContactModelMultiple,ActivationModelWeightedQuad,m2a,a2m,CostModelPlacementVelocity,CostModelPosition6D
 from robots import loadTalosLegs
 
 robot = loadTalosLegs()
@@ -23,10 +23,9 @@ def createModel():
     contact6 = ContactModel6D(rmodel,rmodel.getFrameId(contactName),ref=None)
     contactModel.addContact(name='contact',contact=contact6)
     costModel = CostModelSum(rmodel,nu=actModel.nu)
-    cost1 = CostModelPosition(rmodel,nu=actModel.nu,
-                              frame=rmodel.getFrameId(opPointName),
-                              ref=np.array([.5,.4,.3]),
-                              activation=ActivationModelWeightedQuad(np.array([1,0,1])))
+    cost1 = CostModelPosition6D(rmodel,nu=actModel.nu,
+                                frame=rmodel.getFrameId(opPointName),
+                                ref=pinocchio.SE3(eye(3),np.matrix([.2,.0848,0.]).T))
     cost2 = CostModelState(rmodel,State,ref=State.zero(),nu=actModel.nu)
     cost3 = CostModelControl(rmodel,nu=actModel.nu)
     costModel.addCost( name="pos", weight = 10, cost = cost1)
@@ -58,27 +57,23 @@ models = [ createModel() for _ in range(T+1) ]
 for k,model in enumerate(models[:-1]):
     t = k*timeStep
     model.timeStep = timeStep
-    model.differential.costs.addCost(name='veleff',
-                                     cost=CostModelPlacementVelocity(rmodel,OPPOINTFRAME),
-                                     weight=10000)
-    model.differential.costs['veleff' ].weight = 100
-    model.differential.costs['pos' ].weight =    1
+    model.differential.costs['pos' ].weight =    100
     model.differential.costs['regx'].weight = .1
     model.differential.costs['regx'].cost.weights = np.array([0]*6+[0.01]*(rmodel.nv-6)+[10]*rmodel.nv)
     model.differential.costs['regu'].weight = 0.001
-    model.differential.costs['pos' ].cost.ref[:] = [ .2, .2, 0.0 ]
+    model.differential.costs['pos' ].cost.ref.translation = np.matrix([ .2*t/DT, .0848, 0.0 ]).T
 
 termmodel = models[-1]
 termmodel.differential.costs.addCost(name='veleff',
                                      cost=CostModelPlacementVelocity(rmodel,OPPOINTFRAME),
                                      weight=10000)
 
-termmodel.differential.costs['veleff' ].weight = 1000
+termmodel.differential.costs['veleff' ].weight = 10000
 termmodel.differential.costs['pos' ]   .weight = 300000
 termmodel.differential.costs['regx']   .weight = 1
 termmodel.differential.costs['regu']   .weight = 0.01
 termmodel.differential.costs['regx'].cost.weights = np.array([0]*6+[0.01]*(rmodel.nv-6)+[10]*rmodel.nv)
-termmodel.differential.costs['pos'].cost.ref[:] = [ .2, .2, 0 ]
+termmodel.differential.costs['pos' ].cost.ref.translation = np.matrix([ .2, .0848, 0.0 ]).T
     
 # --- SOLVER
 
