f4e398d2ae5141b553ae9e6a06730dce301c47e3
Mansard <nmansard@laas.fr>
2019-01-24 09:27:11 +0100

    The two last commits were a temptative for copying the history of solver in log.

--- crocoddyl/logger.py
+++ crocoddyl/logger.py
@@ -1,65 +1,41 @@
-import shooting
+'''
+This class creates an object logger with a callback operator logger(solver)
+that the solver can call at every iteration end to store some data and display the robot motion
+in gepetto-gui.
+In the solver, set up the logger with solver.callback = SolverLogger(), and add the robot-wrapper
+object in argument if you want to use the display functionalities.
+'''
 
+import copy
+def disptraj(robot,xs,dt=0.1,N=-1):
+    '''
+    Display a robot trajectory xs using Gepetto-viewer gui.
+    '''
+    if not hasattr(robot,'viewer'): robot.initDisplay(loadModel=True)
+    import numpy as np
+    a2m = lambda a: np.matrix(a).T
+    import time
+    S = 1 if N<=0 else max(len(xs)/N,1)
+    for i,x in enumerate(xs):
+        if not i % S:
+            robot.display(a2m(x[:robot.nq]))
+            time.sleep(dt)
 
-class SolverAbstract:
-    def __init__(self,shootingProblem):
-        self.problem = shootingProblem
-    
-        self.alphas = [ 10**(-n) for n in range(7) ]
-        self.th_acceptStep = .1
-        self.th_stop = 1e-12
-        
-    def models(self) : return self.problem.runningModels + [self.problem.terminalModel]
-    def datas(self) : return self.problem.runningDatas + [self.problem.terminalData]
-
-    def setCandidate(self,xs=None,us=None,isFeasible=False,copy=True):
-        '''
-        Set a candidate pair xs,us, and define it as feasible (i.e. obtained from
-        rollout) or not.
-        '''
-        pass
-    def calc(self):
-        '''
-        Compute the tangent (LQR) model.
-        Returns cost.
-        '''
-        self.cost = problem.calcDiff(xs,us)
-        return self.cost
-        
-    def computeDirection(self,recalc=True):
-        '''
-        Compute the descent direction dx,dx.
-        Returns the descent direction dx,du and the dual lambdas as lists of T+1, T and T+1 lengths. 
-        '''
-        if recalc: self.calc()
-        return # xs,us,lambdas
-
-    def tryStep(self,stepLength):
-        '''
-        Make a step of length stepLength in the direction self.dxs,self.dus computed 
-        (possibly partially) by computeDirection. Store the result in self.xs_try,self.us_try.
-        Return the cost improvement, i.e. self.cost-self.problem.calc(self.xs_try,self.us_try).
-        '''
-        # self.xs_try,self.us_try = ...
-        # dV = self.cost - ...
-        return # dV
-            
-    def stoppingCriteria(self):
-        '''
-        Return a list of positive parameters whose sum quantifies the algorithm termination.
-        '''
-        return    #[ criterion1, criterion2 ... ]
-    
-    def expectedImprovement(self):
-        '''
-        Return two scalars denoting the quadratic improvement model
-        (i.e. dV = f_0 - f_+ = d1*a + d2*a**2/2)
-        '''
-        return # [ d1, d2 ]
-
-    def solver(self,maxiter=100,init_xs=None,init_us=None):
-        '''
-        Nonlinear solver iterating over the solveQP.
-        Return the optimum xopt,uopt as lists of T+1 and T terms, and a boolean
-        describing the success.
-        '''
+class SolverLogger:
+    def __init__(self,robotwrapper=None):
+        self.steps = []
+        self.iters = []
+        self.costs = []
+        self.regularizations = []
+        self.robotwrapper = robotwrapper
+        self.xs = []
+        self.us = []
+    def __call__(self,solver):
+        self.xs.append(copy.copy(solver.xs))
+        self.us.append(solver.us)
+        self.steps.append( solver.stepLength )
+        self.iters.append( solver.iter )
+        self.costs.append( [ d.cost for d in solver.datas() ] )
+        self.regularizations.append( solver.x_reg )
+        if self.robotwrapper is not None:
+            disptraj(self.robotwrapper,solver.xs,dt=1e-3,N=3)
