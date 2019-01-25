16d4fadc39d290a181caa9bd3a9978b2ae47827a
Carlos Mastalli <carlos.mastalli@iit.it>
2019-01-25 01:39:42 +0100

    [talos-arm] Improved the early implementation of talos-arm example

--- examples/talos_arm.py
+++ examples/talos_arm.py
@@ -1,7 +1,9 @@
 from crocoddyl import StatePinocchio
 from crocoddyl import DifferentialActionModel, IntegratedActionModelEuler
-from crocoddyl import CostModelPosition6D, CostModelState, CostModelControl
-from crocoddyl import ShootingProblem, SolverDDP, SolverLogger
+from crocoddyl import CostModelPosition, CostModelPosition6D
+from crocoddyl import CostModelState, CostModelControl
+from crocoddyl import ShootingProblem, SolverDDP
+from crocoddyl import SolverLogger, SolverPrinter, SolverDisplay
 from crocoddyl import loadTalosArm
 from crocoddyl import plotOCSolution, plotDDPConvergence
 import pinocchio
@@ -25,7 +27,6 @@ terminalModel = IntegratedActionModelEuler(DifferentialActionModel(robot.model))
 # Defining the time duration for running action models and the terminal one
 dt = 1e-3
 runningModel.timeStep = dt
-terminalModel.timeStep = dt
 
 # Note that we need to include a cost model (i.e. set of cost functions) in
 # order to fully define the action model for our optimal control problem.
@@ -34,16 +35,15 @@ terminalModel.timeStep = dt
 # goal cost. First, let's create the common cost functions.
 frameName = 'gripper_left_joint' #gripper_left_fingertip_2_link'
 state = StatePinocchio(robot.model)
-SE3ref = pinocchio.SE3(np.eye(3), np.array([ [.0],[.0],[.4 ]]))
+SE3ref = pinocchio.SE3(np.eye(3), np.array([ [.0],[.0],[.4] ]))
 goalTrackingCost = CostModelPosition6D(robot.model,
-                                     nu=robot.model.nv,
-                                     frame=robot.model.getFrameId(frameName),
-                                     ref=SE3ref)
+                                       nu=robot.model.nv,
+                                       frame=robot.model.getFrameId(frameName),
+                                       ref=SE3ref)
 # goalTrackingCost = CostModelPosition(robot.model,
 #                                      nu=robot.model.nv,
 #                                      frame=robot.model.getFrameId(frameName),
 #                                      ref=np.array([.0,.0,.4]))
-                                    #  ref=np.array([.5,.4,.3]))
 xRegCost = CostModelState(robot.model,
                           state,
                           ref=state.zero(),
@@ -58,38 +58,37 @@ runningCostModel.addCost( name="regu", weight = 1e-7, cost = uRegCost)
 terminalCostModel = terminalModel.differential.costs
 terminalCostModel.addCost( name="pos", weight = 1, cost = goalTrackingCost)
 
-# runningCostModel = runningModel.differential.costs
-# runningCostModel.addCost( name="pos", weight = 10, cost = goalTrackingCost)
-# runningCostModel.addCost( name="regx", weight = 0.1, cost = xRegCost) 
-# runningCostModel.addCost( name="regu", weight = 0.01, cost = uRegCost)
-# terminalCostModel = terminalModel.differential.costs
-# terminalCostModel.addCost( name="pos", weight = 100, cost = goalTrackingCost)
 
-
-T = 50
+# For this optimal control problem, we define 250 knots (or running action
+# models) plus a terminal knot
+T = 250
 q0 = [0.173046, 1., -0.52366, 0., 0., 0.1, -0.005]
 x0 = np.hstack([q0, np.zeros(robot.model.nv)])
 problem = ShootingProblem(x0, [ runningModel ]*T, terminalModel)
 
 # Creating the DDP solver for this OC problem, defining a logger
 ddp = SolverDDP(problem)
-ddp.callback = SolverLogger(robot)
+ddp.callback = [SolverLogger(), SolverPrinter(1), SolverDisplay(robot,4)]
 
 # Solving it with the DDP algorithm
 ddp.solve()
 
-# Plotting the solution
-# plotOCSolution(ddp.callback.xs, ddp.callback.us)
-plotDDPConvergence(ddp.callback.costs,
-                   ddp.callback.control_regs,
-                   ddp.callback.state_regs,
-                   ddp.callback.gm_stops,
-                   ddp.callback.th_stops,
-                   ddp.callback.steps)
 
-# def plotDDPConvergence(J, muLM, muV, gamma, theta, alpha)
+# Plotting the solution and the DDP convergence
+log = ddp.callback[0]
+plotOCSolution(log.xs, log.us)
+plotDDPConvergence(log.costs,log.control_regs,
+                   log.state_regs,log.gm_stops,
+                   log.th_stops,log.steps)
 
+
+# Visualizing the solution in gepetto-viewer
+SolverDisplay(robot)(ddp)
+
+# Printing the reached position
 frame_idx = robot.model.getFrameId(frameName)
-xT = ddp.callback.xs[-1]
+xT = log.xs[-1]
 qT = np.asmatrix(xT[:robot.model.nq]).T
+print
+print "The reached pose by the wrist is"
 print robot.framePlacement(qT, frame_idx)
