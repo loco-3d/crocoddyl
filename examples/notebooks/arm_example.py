7078ec0eb6581d769d5262575d8e9b21586a50b7
Mansard <nmansard@laas.fr>
2019-01-30 03:37:29 +0100

    IVIGIT manipulator jupyter.

---
+++ examples/notebooks/arm_example.py
@@ -0,0 +1,54 @@
+from crocoddyl import *
+import pinocchio
+import numpy as np
+
+robot = loadTalosArm()
+robot.initDisplay(loadModel=True)
+
+robot.q0.flat[:] = [  2,1.5,-2,0,0,0,0 ]
+robot.model.armature[:] = .2
+frameId = robot.model.getFrameId('gripper_left_joint')
+DT = 1e-2
+T  = 25
+
+target = np.array([ 0.4,0  ,.4 ])
+
+robot.viewer.gui.addSphere('world/point',.1,[1,0,0,1])  # radius = .1, RGBA=1001
+robot.viewer.gui.applyConfiguration('world/point', target.tolist()+[0,0,0,1] )  # xyz+quaternion
+robot.viewer.gui.refresh()
+
+# Create the cost model
+costTrack = CostModelFrameTranslation(robot.model,frame=frameId,ref=target)
+costXReg  = CostModelState(robot.model,StatePinocchio(robot.model))
+costUReg  = CostModelControl(robot.model)
+
+# Create the action model with empty costs
+model     = DifferentialActionModel(robot.model)
+termmodel = DifferentialActionModel(robot.model)
+
+# Then let's added the running and terminal cost functions
+model.costs.addCost( name="pos", weight = 1, cost = costTrack)
+model.costs.addCost( name="xreg", weight = 1e-4, cost = costXReg)
+model.costs.addCost( name="ureg", weight = 1e-7, cost = costUReg)
+
+termmodel.costs.addCost( name="pos", weight = 1000, cost = costTrack)
+termmodel.costs.addCost( name="xreg", weight = 1e-4, cost = costXReg)
+termmodel.costs.addCost( name="ureg", weight = 1e-7, cost = costUReg)
+
+# Create the problem
+x0 = np.concatenate([ m2a(robot.q0), np.zeros(robot.model.nv)])
+problem = ShootingProblem(x0, [ IntegratedActionModelEuler(model) ]*T,
+                          IntegratedActionModelEuler(termmodel))
+
+# Creating the DDP solver for this OC problem, defining a logger
+ddp = SolverDDP(problem)
+ddp.callback = [CallbackDDPVerbose() ]
+
+# Solving it with the DDP algorithm
+ddp.solve()
+
+# Visualizing the solution in gepetto-viewer
+for x in ddp.xs: robot.display(a2m(x))
+
+print('Finally reached = ',
+      ddp.datas()[T].differential.costs['pos'].pinocchio.oMf[frameId].translation.T)
