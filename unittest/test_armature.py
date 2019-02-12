d9b547ac1673189638e4fce8ee4370b7a0df2675
Mansard <nmansard@laas.fr>
2019-02-12 10:04:39 +0100

    [test] IVIGIT test armature: an integrative test to investigate #91.

---
+++ unittest/test_armature.py
@@ -0,0 +1,138 @@
+from crocoddyl import StatePinocchio
+from crocoddyl import DifferentialActionModelFloatingInContact
+from crocoddyl import IntegratedActionModelEuler
+from crocoddyl import CostModelSum
+from crocoddyl import CostModelFramePlacement, CostModelFrameVelocity
+from crocoddyl import CostModelState, CostModelControl,CostModelCoM
+from crocoddyl import ActivationModelWeightedQuad
+from crocoddyl import ActuationModelFreeFloating
+from crocoddyl import ContactModel6D, ContactModelMultiple
+from crocoddyl import ShootingProblem
+from crocoddyl import SolverDDP
+from crocoddyl import CallbackDDPLogger, CallbackDDPVerbose, CallbackSolverDisplay
+from crocoddyl import plotOCSolution, plotDDPConvergence
+from crocoddyl import loadTalosLegs
+from crocoddyl import m2a, a2m, ImpulseModelMultiple, ImpulseModel6D, ActionModelImpact
+from crocoddyl.impact import CostModelImpactCoM, CostModelImpactWholeBody
+import pinocchio
+from pinocchio.utils import *
+from numpy.linalg import norm,inv,pinv,eig,svd
+from crocoddyl.diagnostic import displayTrajectory
+disp = lambda xs,dt: displayTrajectory(robot,xs,dt)
+disp.__defaults__ = ( .1, )
+
+robot = loadTalosLegs()
+robot.model.armature[6:] = 1.
+robot.initDisplay(loadModel=True)
+
+rmodel = robot.model
+rdata  = rmodel.createData()
+
+# Setting up the 3d walking problem
+rightFoot = 'right_sole_link'
+leftFoot = 'left_sole_link'
+
+rightId = rmodel.getFrameId(rightFoot)
+leftId  = rmodel.getFrameId(leftFoot)
+
+# Create the initial state
+q0 = robot.q0.copy()
+v0 = zero(rmodel.nv)
+x0 = m2a(np.concatenate([q0,v0]))
+rmodel.defaultState = x0.copy()
+
+def runningModel(contactIds, effectors, com=None, integrationStep = 1e-2):
+    '''
+    Creating the action model for floating-base systems. A walker system 
+    is by default a floating-base system.
+    contactIds is a list of frame Ids of points that should be in contact.
+    effectors is a dict of key frame ids and SE3 values of effector references.
+    '''
+    actModel = ActuationModelFreeFloating(rmodel)
+    State = StatePinocchio(rmodel)
+
+    # Creating a 6D multi-contact model, and then including the supporting foot
+    contactModel = ContactModelMultiple(rmodel)
+    for cid in contactIds:
+       contactModel.addContact('contact%d'%cid,
+                               ContactModel6D(rmodel, cid, ref=None))
+
+    # Creating the cost model for a contact phase
+    costModel = CostModelSum(rmodel, actModel.nu)
+    wx = np.array([0]*6 + [.1]*(rmodel.nv-6) + [10]*rmodel.nv)
+    costModel.addCost('xreg',weight=1e-1,
+                      cost=CostModelState(rmodel,State,ref=rmodel.defaultState,nu=actModel.nu,
+                                          activation=ActivationModelWeightedQuad(wx)))
+    costModel.addCost('ureg',weight=1e-4,
+                      cost=CostModelControl(rmodel, nu=actModel.nu))
+    for fid,ref in effectors.items():
+        costModel.addCost("track%d"%fid, weight=100.,
+                          cost = CostModelFramePlacement(rmodel,fid,ref,actModel.nu))
+
+    if com is not None:
+        costModel.addCost("com", weight=100.,
+                          cost = CostModelCoM(rmodel,ref=com,nu=actModel.nu))
+        
+    # Creating the action model for the KKT dynamics with simpletic Euler
+    # integration scheme
+    dmodel = \
+             DifferentialActionModelFloatingInContact(rmodel,
+                                                      actModel,
+                                                      contactModel,
+                                                      costModel)
+    model = IntegratedActionModelEuler(dmodel)
+    model.timeStep = integrationStep
+    return model
+
+# -------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
+
+q = robot.q0.copy()
+v = zero(rmodel.nv)
+x = m2a(np.concatenate([q,v]))
+
+SE3 = pinocchio.SE3
+pinocchio.forwardKinematics(rmodel,rdata,q0)
+pinocchio.updateFramePlacements(rmodel,rdata)
+right0 = rdata.oMf[rightId].translation
+left0  = rdata.oMf[leftId].translation
+com0 = m2a(pinocchio.centerOfMass(rmodel,rdata,q0))
+
+models = []\
+         +[ runningModel([ rightId, leftId ],{}, integrationStep=5e-2)] *10                     \
+         +[ runningModel([ rightId, leftId ],{},com=com0+[ 0.1,0,0], integrationStep=5e-2)]       \
+         +[ runningModel([ rightId, leftId ],{}, integrationStep=5e-2)] *10                     \
+         +[ runningModel([ rightId, leftId ],{},com=com0+[-0.1,0,0], integrationStep=5e-2)]
+
+pass1 = models[10]
+pass2 = models[21]
+
+pass1.differential.costs['com'].weight = 1000
+pass2.differential.costs['com'].weight = 1000
+
+# --- DDP
+problem = ShootingProblem(initialState=x0,runningModels=models[:-1],terminalModel=models[-1])
+ddp = SolverDDP(problem)
+ddp.callback = [ CallbackDDPLogger(), CallbackDDPVerbose() ]
+ddp.th_stop = 1e-6
+
+ddp.setCandidate()
+m=models[0]
+d=m.createData()
+
+m=m.differential.contact['contact30']
+d=d.differential.contact['contact30']
+#m.calc(d,ddp.xs[0],ddp.us[0])
+m.calc(d,ddp.xs[0])
+
+
+ddp.solve(maxiter=1000,regInit=.1,init_xs=[rmodel.defaultState]*len(ddp.models()))
+
+np.set_printoptions(precision=4, linewidth=200, suppress=True)
+
+# --- PLOT
+import matplotlib.pylab as plt
+plt.ion()
+plt.plot([ d.differential.pinocchio.com[0][0,0] \
+           for d in ddp.datas() ])
