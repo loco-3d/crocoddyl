f7ac03e3184b3bc7ca3eed00be8ee67fd2840252
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-02-14 13:52:17 +0100

    [examples] Fixed grammar error

--- examples/bipedal_walking_from_foot_traj.py
+++ examples/bipedal_walking_from_foot_traj.py
@@ -33,7 +33,7 @@ class SimpleBipedWalkingProblem:
         """ Create a shooting problem for a simple walking.
 
         :param x0: initial state
-        :param stepLength: step lenght
+        :param stepLength: step length
         :param timeStep: step time for each knot
         :param stepKnots: number of knots for step phases
         :param supportKnots: number of knots for double support phases
