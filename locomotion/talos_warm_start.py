2ad74f9582ea6921943f1887fb9c2a6f54f493b0
Rohan Budhiraja <proyan@users.noreply.github.com>
2019-02-20 15:03:38 +0100

    [locomtion] create splines from initial trajectories.

--- locomotion/talos_warm_start.py
+++ locomotion/talos_warm_start.py
@@ -4,10 +4,11 @@ from time import sleep
 from locomote import ContactSequenceHumanoid
 from centroidal_utils import createPhiFromContactSequence, createMultiphaseShootingProblem, createSwingTrajectories
 from crocoddyl import m2a, a2m
-from crocoddyl import ShootingProblem, SolverDDP
+from crocoddyl import ShootingProblem, SolverDDP, StatePinocchio
 from crocoddyl import ActionModelImpact
 from crocoddyl import CallbackDDPVerbose, CallbackSolverDisplay, CallbackDDPLogger, CallbackSolverTimer
 import numpy as np
+import locomote
 np.set_printoptions(linewidth=400, suppress=True)
 robot = conf.robot
 rmodel = robot.model
@@ -20,7 +21,7 @@ cs.loadFromXML(conf.MUSCOD_CS_OUTPUT_FILENAME, conf.CONTACT_SEQUENCE_XML_TAG)
 
 #----------------Define References-------------------------
 
-swing_ref = createSwingTrajectories(rmodel, rdata, conf.X_init, conf.contact_patches, conf.DT)
+swing_ref = createSwingTrajectories(rmodel, rdata, conf.X_init, conf.contact_patches, 0.005)
 phi_c = createPhiFromContactSequence(rmodel, rdata, cs, conf.contact_patches.keys())
 
 #----------------Define Problem----------------------------
@@ -40,41 +41,56 @@ for d in problem.runningDatas:
         d.differential.costs["forces_"+patchname].contact = contactData
 #---------Ugliness over------------------------
 
-
 #-----------Create inital trajectory---------------
 init = lambda t:0
 init.X = []
 init.U = []
-i = 0
-for m in problem.runningModels:
+x_tsid = np.matrix(conf.X_init).T
+conf.ddq_init.append(np.zeros(rmodel.nv))
+dx_tsid = np.vstack([x_tsid[rmodel.nv:,:], np.matrix(conf.ddq_init).T])
+t_tsid = np.linspace(0.,cs.contact_phases[-1].time_trajectory[-1], len(conf.ddq_init)+1)
+x_spl = locomote.CubicHermiteSpline(a2m(t_tsid), x_tsid, dx_tsid)
+
+state = StatePinocchio(rmodel)
+dt = conf.DT
+
+x_eval = lambda t: m2a(x_spl.eval(t)[0])
+for i, (m,d) in enumerate(zip(problem.runningModels, problem.runningDatas)):
   #Impact models have zero timestep, thus they are copying the same state as the beginning of the next action model
   #State and control are defined at the beginning of the time step.
   if isinstance(m, ActionModelImpact):
-    init.X.append(conf.X_init[i+1])
+    init.X.append(x_eval((i+1)*dt))
     init.U.append(np.zeros(m.nu))
-    print "impact at ",i
+    #print "impact at ",i
   else:
-    init.X.append(conf.X_init[i])
-    init.U.append(conf.U_init[i])
-    i+=1
-assert(i==len(conf.U_init))
-assert(i==len(conf.X_init)-1)
+    xp = x_eval(i*dt); xn = x_eval((i+1)*dt)
+    init.X.append(xp)
+    dx = state.diff(xp,xn)
+    acc = dx[rmodel.nv:]/dt
+    u = pinocchio.rnea(rmodel, rdata,a2m(xp[:rmodel.nq]),a2m(xp[rmodel.nq:]), a2m(acc))
+    m.differential.calc(d.differential, init.X[-1])
+    contactJ = d.differential.contact.J
+    f = np.dot(np.linalg.pinv(contactJ.T[:6,:]), u[:6])
+    u -= (np.dot(contactJ.transpose(),f))
+    init.U.append(np.array(u[6:]).squeeze().copy())
 init.X.append(conf.X_init[-1])
 #---------------Display Initial Trajectory--------------
 if conf.DISPLAY:
   robot.initDisplay(loadModel=True)
-if conf.DISPLAY and False:
-  for x in init.X:  
+if conf.DISPLAY:
+  for x in init.X:
     robot.display(a2m(x[:robot.nq]))
     #sleep(0.005)
 #----------------------
 
+
+
 ddp = SolverDDP(problem)
-ddp.callback = [CallbackDDPVerbose(), CallbackSolverTimer()]
-if conf.DISPLAY:
+ddp.callback = [CallbackDDPVerbose()]#, CallbackSolverTimer()]
+if conf.RUNTIME_DISPLAY:
   ddp.callback.append(CallbackSolverDisplay(robot,4))
 ddp.th_stop = 1e-9
-ddp.solve(maxiter=1000,regInit=0.1,init_xs=init.X,init_us=init.U)
+ddp.solve(maxiter=100,regInit=0.1,init_xs=init.X,init_us=init.U)
 #---------------Display Final Trajectory--------------
 if conf.DISPLAY:
   for x in init.X:  
