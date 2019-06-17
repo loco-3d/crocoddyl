14079441f2300e4b08429396848205849509d3a4
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-06-17 14:10:02 +0200

    fix the unnitest packaging

--- unittest/test_state.cpp
+++ unittest/test_state.cpp
@@ -1,14 +1,12 @@
 #include <boost/test/unit_test.hpp>
-
-//#include "example-adder/gepadd.hpp"
+#include "crocoddyl/core/states/state-euclidean.hpp"
 
 BOOST_AUTO_TEST_SUITE ( BOOST_TEST_MODULE )
 
-BOOST_AUTO_TEST_CASE(test_state_0)
+BOOST_AUTO_TEST_CASE(test_boost_check)
 {
-  // BOOST_CHECK(gepetto::example::add(1, 2) == 3);
-  // BOOST_CHECK(gepetto::example::add(5, -1) == 4);
-  // BOOST_CHECK(gepetto::example::add(-3, 1) == -2);
+ BOOST_CHECK( 0 == 0);
+ BOOST_CHECK( 0 != 1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
