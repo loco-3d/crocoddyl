3ae15941ba49c73052d54a2ca9f975e29225f0d5
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-06-17 13:31:39 +0200

    add the test_state.cpp file

---
+++ unittest/test_state.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+
+//#include "example-adder/gepadd.hpp"
+
+BOOST_AUTO_TEST_SUITE ( BOOST_TEST_MODULE )
+
+BOOST_AUTO_TEST_CASE(test_state_0)
+{
+  // BOOST_CHECK(gepetto::example::add(1, 2) == 3);
+  // BOOST_CHECK(gepetto::example::add(5, -1) == 4);
+  // BOOST_CHECK(gepetto::example::add(-3, 1) == -2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
