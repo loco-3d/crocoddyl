bd1ec460fa252a9a4e15e69388d6e1b2e2c78226
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-06-18 10:32:12 +0200

    add headers to file and the test_activation file

--- unittest/test_activation.cpp
+++ unittest/test_activation.cpp
@@ -1,5 +1,5 @@
 /**
- * @file test_state.cpp
+ * @file test_activation.cpp
  * @author Maximilien Naveau (maximilien.naveau@gmail.com)
  * @license License BSD-2-Clause
  * @copyright Copyright (c) 2019, New York University, Max Planck Gesellshaft and LAAS.
@@ -8,14 +8,13 @@
 
 
 #include <boost/test/unit_test.hpp>
-#include "crocoddyl/core/states/state-euclidean.hpp"
+
 
 BOOST_AUTO_TEST_SUITE ( BOOST_TEST_MODULE )
 
-BOOST_AUTO_TEST_CASE(test_boost_check)
+BOOST_AUTO_TEST_CASE(test_activation)
 {
- BOOST_CHECK( 0 == 0);
- BOOST_CHECK( 0 != 1);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
