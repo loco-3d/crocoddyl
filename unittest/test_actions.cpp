a0e087d2e824ee43cb9930115ddd9064b330dc2a
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-06-20 17:07:42 +0200

    add unnittest to the action model lqr, waiting for the action num diff to finish

--- unittest/test_actions.cpp
+++ unittest/test_actions.cpp
@@ -39,14 +39,52 @@ void test_calc_returns_state(crocoddyl::ActionModelAbstract& model)
 
 //____________________________________________________________________________//
 
-void test_calc_returns_a_cost(crocoddyl::ActionModelAbstract& )
+void test_calc_returns_a_cost(crocoddyl::ActionModelAbstract& model)
 {
+  // create the corresponding data object and set the cost to nan
+  std::shared_ptr<crocoddyl::ActionDataAbstract> data = model.createData();
+  data->cost = std::nan("");
+
+  // Getting the cost value computed by calc()
+  Eigen::VectorXd x = model.get_state()->rand();
+  Eigen::VectorXd u = Eigen::VectorXd::Random(model.get_nu());
+  model.calc(data, x, u);
+
+  // Checking that calc returns a cost value
+  BOOST_CHECK(!std::isnan(data->cost));
 }
 
 //____________________________________________________________________________//
 
-void test_partial_derivatives_against_numdiff(crocoddyl::ActionModelAbstract& )
+void test_partial_derivatives_against_numdiff(
+  crocoddyl::ActionModelAbstract& model)
 {
+  // create the corresponding data object and set the cost to nan
+  std::shared_ptr<crocoddyl::ActionDataAbstract> data = model.createData();
+
+  // create the num diff model and data
+  // crocoddyl::ActionModelNumDiff model_num_diff (model);
+  // std::shared_ptr<crocoddyl::ActionDataNumDiff> data_num_diff =
+  //   model_num_diff.createData();
+
+  // Generating random values for the state and control
+  Eigen::VectorXd x = model.get_state()->rand();
+  Eigen::VectorXd u = Eigen::VectorXd::Random(model.get_nu());
+
+  // Computing the action derivatives
+  model.calcDiff(data, x, u);
+  // model_num_diff.calcDiff(data_num_diff, x, u)
+
+  // Checking the partial derivatives against NumDiff
+  // double tol = self.NUMDIFF_MOD * self.MODEL_NUMDIFF.disturbance;
+  // BOOST_CHECK((data->Fx - data_num_diff->Fx).isMuchSmallerThan(1.0, tol));
+  // BOOST_CHECK((data->Fu - data_num_diff->Fu).isMuchSmallerThan(1.0, tol));
+  // BOOST_CHECK((data->Lx - data_num_diff->Lx).isMuchSmallerThan(1.0, tol));
+  // BOOST_CHECK((data->Lu - data_num_diff->Lu).isMuchSmallerThan(1.0, tol));
+  // BOOST_CHECK(data->Lxx - data_num_diff->Lxx).isMuchSmallerThan(1.0, tol));
+  // BOOST_CHECK(data->Lxu - data_num_diff->Lxu).isMuchSmallerThan(1.0, tol));
+  // BOOST_CHECK(data->Luu - data_num_diff->Luu).isMuchSmallerThan(1.0, tol));
+  BOOST_MESSAGE("test_partial_derivatives_against_numdiff is Disabled\n");
 }
 
 //____________________________________________________________________________//
