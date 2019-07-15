a87e28deca4098e9b682bd16d4a944487f6630d9
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-07-15 14:48:50 +0200

    [cleanup] License description is only needed in the header files

--- src/core/states/state-num-diff.cpp
+++ src/core/states/state-num-diff.cpp
@@ -1,10 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-// BSD 3-Clause License
-//
-// Copyright (C) 2018-2019, LAAS-CNRS
-// Copyright note valid unless otherwise stated in individual files.
-// All rights reserved.
-///////////////////////////////////////////////////////////////////////////////
 #include "crocoddyl/core/states/state-num-diff.hpp"
 
 namespace crocoddyl {
