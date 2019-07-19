f69ae2f7c829c13559667f26c59eb86bb4daf459
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-07-19 20:08:14 +0200

    [c++00][wip] Way of removing Rchar of c++. Needs to propage to all bindings classes

--- bindings/python/crocoddyl/core/state-base.hpp
+++ bindings/python/crocoddyl/core/state-base.hpp
@@ -25,16 +25,16 @@ class StateAbstract_wrap : public StateAbstract, public bp::wrapper<StateAbstrac
 
   StateAbstract_wrap(int nx, int ndx) : StateAbstract(nx, ndx), bp::wrapper<StateAbstract>() {}
 
-  Eigen::VectorXd zero() override { return bp::call<Eigen::VectorXd>(this->get_override("zero").ptr()); }
+  Eigen::VectorXd zero() { return bp::call<Eigen::VectorXd>(this->get_override("zero").ptr()); }
 
-  Eigen::VectorXd rand() override { return bp::call<Eigen::VectorXd>(this->get_override("rand").ptr()); }
+  Eigen::VectorXd rand() { return bp::call<Eigen::VectorXd>(this->get_override("rand").ptr()); }
 
   Eigen::VectorXd diff_wrap(const Eigen::Ref<const Eigen::VectorXd>& x0, const Eigen::Ref<const Eigen::VectorXd>& x1) {
     return bp::call<Eigen::VectorXd>(this->get_override("diff").ptr(), (Eigen::VectorXd)x0, (Eigen::VectorXd)x1);
   }
 
   void diff(const Eigen::Ref<const Eigen::VectorXd>& x0, const Eigen::Ref<const Eigen::VectorXd>& x1,
-            Eigen::Ref<Eigen::VectorXd> dxout) override {
+            Eigen::Ref<Eigen::VectorXd> dxout) {
     dxout = diff_wrap(x0, x1);
   }
 
@@ -44,13 +44,12 @@ class StateAbstract_wrap : public StateAbstract, public bp::wrapper<StateAbstrac
   }
 
   void integrate(const Eigen::Ref<const Eigen::VectorXd>& x, const Eigen::Ref<const Eigen::VectorXd>& dx,
-                 Eigen::Ref<Eigen::VectorXd> x1out) override {
+                 Eigen::Ref<Eigen::VectorXd> x1out) {
     x1out = integrate_wrap(x, dx);
   }
 
   void Jdiff(const Eigen::Ref<const Eigen::VectorXd>& x0, const Eigen::Ref<const Eigen::VectorXd>& x1,
-             Eigen::Ref<Eigen::MatrixXd> Jfirst, Eigen::Ref<Eigen::MatrixXd> Jsecond,
-             Jcomponent _firstsecond) override {
+             Eigen::Ref<Eigen::MatrixXd> Jfirst, Eigen::Ref<Eigen::MatrixXd> Jsecond, Jcomponent _firstsecond) {
     std::string firstsecond;
     switch (_firstsecond) {
       case first: {
@@ -96,8 +95,7 @@ class StateAbstract_wrap : public StateAbstract, public bp::wrapper<StateAbstrac
   }
 
   void Jintegrate(const Eigen::Ref<const Eigen::VectorXd>& x0, const Eigen::Ref<const Eigen::VectorXd>& x1,
-                  Eigen::Ref<Eigen::MatrixXd> Jfirst, Eigen::Ref<Eigen::MatrixXd> Jsecond,
-                  Jcomponent _firstsecond) override {
+                  Eigen::Ref<Eigen::MatrixXd> Jfirst, Eigen::Ref<Eigen::MatrixXd> Jsecond, Jcomponent _firstsecond) {
     std::string firstsecond;
     switch (_firstsecond) {
       case first: {
@@ -147,71 +145,64 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(Jdiffs, StateAbstract::Jdiff_wrap, 2, 3)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(Jintegrates, StateAbstract::Jintegrate_wrap, 2, 3)
 
 void exposeStateAbstract() {
-  bp::class_<StateAbstract_wrap, boost::noncopyable>("StateAbstract",
-                                                     R"(Abstract class for the state representation.
-
-        A state is represented by its operators: difference, integrates and their derivatives.
-        The difference operator returns the value of x1 [-] x2 operation. Instead the integrate
-        operator returns the value of x [+] dx. These operators are used to compared two points
-        on the state manifold M or to advance the state given a tangential velocity (Tx M).
-        Therefore the points x, x1 and x2 belongs to the manifold M; and dx or x1 [-] x2 lie
-        on its tangential space.)",
-                                                     bp::init<int, int>(bp::args(" self", " nx", " ndx"),
-                                                                        R"(Initialize the state dimensions.
-
-:param nx: dimension of state configuration vector,
-:param ndx: dimension of state tangent vector)"))
+  bp::class_<StateAbstract_wrap, boost::noncopyable>(
+      "StateAbstract",
+      "Abstract class for the state representation.\n\n"
+      "A state is represented by its operators: difference, integrates and their derivatives.\n"
+      "The difference operator returns the value of x1 [-] x2 operation. Instead the integrate\n"
+      "operator returns the value of x [+] dx. These operators are used to compared two points\n"
+      "on the state manifold M or to advance the state given a tangential velocity (Tx M).\n"
+      "Therefore the points x, x1 and x2 belongs to the manifold M; and dx or x1 [-] x2 lie\n"
+      "on its tangential space.",
+      bp::init<int, int>(bp::args(" self", " nx", " ndx"),
+                         "Initialize the state dimensions.\n\n"
+                         ":param nx: dimension of state configuration vector\n"
+                         ":param ndx: dimension of state tangent vector"))
       .def("zero", pure_virtual(&StateAbstract_wrap::zero), bp::args(" self"),
-           R"(Return a zero reference state.
-
-:return zero reference state)")
+           "Return a zero reference state.\n\n"
+           ":return zero reference state")
       .def("rand", pure_virtual(&StateAbstract_wrap::rand), bp::args(" self"),
-           R"(Return a random reference state.
-
-:return random reference state)")
+           "Return a random reference state.\n\n"
+           ":return random reference state")
       .def("diff", pure_virtual(&StateAbstract_wrap::diff_wrap), bp::args(" self", " x0", " x1"),
-           R"(Operator that differentiates the two state points.
-
-It returns the value of x1 [-] x0 operation. Note tha x0 and x1 are points in the state
-manifold (in M). Instead the operator result lies in the tangent-space of M.
-:param x0: current state (dim state.nx).
-:param x1: next state (dim state.nx).
-:return x1 [-] x0 value (dim state.ndx).)")
+           "Operator that differentiates the two state points.\n\n"
+           "It returns the value of x1 [-] x0 operation. Note tha x0 and x1 are points in the state\n"
+           "manifold (in M). Instead the operator result lies in the tangent-space of M.\n"
+           ":param x0: current state (dim state.nx).\n"
+           ":param x1: next state (dim state.nx).\n"
+           ":return x1 [-] x0 value (dim state.ndx).")
       .def("integrate", pure_virtual(&StateAbstract_wrap::integrate), bp::args(" self", " x", " dx"),
-           R"(Operator that integrates the current state.
-
-It returns the value of x [+] dx operation. x and dx are points in the state.diff(x0,x1) (in M)
-and its tangent, respectively. Note that the operator result lies on M too.
-:param x: current state (dim state.nx).
-:param dx: displacement of the state (dim state.ndx).
-:return x [+] dx value (dim state.nx).)")
+           "Operator that integrates the current state.\n\n"
+           "It returns the value of x [+] dx operation. x and dx are points in the state.diff(x0,x1) (in M)\n"
+           "and its tangent, respectively. Note that the operator result lies on M too.\n"
+           ":param x: current state (dim state.nx).\n"
+           ":param dx: displacement of the state (dim state.ndx).\n"
+           ":return x [+] dx value (dim state.nx).")
       .def("Jdiff", pure_virtual(&StateAbstract_wrap::Jdiff_wrap),
            bp::args(" self", " x0", " x1", " firstsecond = 'both'"),
-           R"(Compute the partial derivatives of difference operator.
-
-The difference operator (x1 [-] x0) is defined by diff(x0, x1). Instead Jdiff
-computes its partial derivatives, i.e. \partial{diff(x0, x1)}{x0} and
-\partial{diff(x0, x1)}{x1}. By default, this function returns the derivatives of the
-first and second argument (i.e. firstsecond='both'). However we can also specific the
-partial derivative for the first and second variables by setting firstsecond='first'
-or firstsecond='second', respectively.
-:param x0: current state (dim state.nx).
-:param x1: next state (dim state.nx).
-:param firstsecond: desired partial derivative
-:return the partial derivative(s) of the diff(x0, x1) function)")
+           "Compute the partial derivatives of difference operator.\n\n"
+           "The difference operator (x1 [-] x0) is defined by diff(x0, x1). Instead Jdiff\n"
+           "computes its partial derivatives, i.e. \\partial{diff(x0, x1)}{x0} and\n"
+           "\\partial{diff(x0, x1)}{x1}. By default, this function returns the derivatives of the\n"
+           "first and second argument (i.e. firstsecond='both'). However we can also specific the\n"
+           "partial derivative for the first and second variables by setting firstsecond='first'\n"
+           "or firstsecond='second', respectively.\n"
+           ":param x0: current state (dim state.nx).\n"
+           ":param x1: next state (dim state.nx).\n"
+           ":param firstsecond: desired partial derivative\n"
+           ":return the partial derivative(s) of the diff(x0, x1) function")
       .def("Jintegrate", pure_virtual(&StateAbstract_wrap::Jintegrate_wrap),
            bp::args(" self", " x", " dx", " firstsecond = 'both'"),
-           R"(Compute the partial derivatives of integrate operator.
-
-The integrate operator (x [+] dx) is defined by integrate(x, dx). Instead Jintegrate
-computes its partial derivatives, i.e. \partial{integrate(x, dx)}{x} and
-\partial{integrate(x, dx)}{dx}. By default, this function returns the derivatives of
-the first and second argument (i.e. firstsecond='both'). However we ask for a specific
-partial derivative by setting firstsecond='first' or firstsecond='second'.
-:param x: current state (dim state.nx).
-:param dx: displacement of the state (dim state.ndx).
-:param firstsecond: desired partial derivative
-:return the partial derivative(s) of the integrate(x, dx) function)")
+           "Compute the partial derivatives of integrate operator.\n\n"
+           "The integrate operator (x [+] dx) is defined by integrate(x, dx). Instead Jintegrate\n"
+           "computes its partial derivatives, i.e. \\partial{integrate(x, dx)}{x} and\n"
+           "\\partial{integrate(x, dx)}{dx}. By default, this function returns the derivatives of\n"
+           "the first and second argument (i.e. firstsecond='both'). However we ask for a specific\n"
+           "partial derivative by setting firstsecond='first' or firstsecond='second'.\n"
+           ":param x: current state (dim state.nx).\n"
+           ":param dx: displacement of the state (dim state.ndx).\n"
+           ":param firstsecond: desired partial derivative\n"
+           ":return the partial derivative(s) of the integrate(x, dx) function")
       .add_property("nx", &StateAbstract_wrap::nx_, "dimension of state configuration vector")
       .add_property("ndx", &StateAbstract_wrap::ndx_, "dimension of state tangent vector")
       .add_property("nq", &StateAbstract_wrap::nq_, "dimension of configuration vector")
