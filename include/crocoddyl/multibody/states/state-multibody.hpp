4551cbe136af390df516ce8dad1f603e54b4008b
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-07-19 11:45:14 +0200

    [state] Added the c++ state pinocchio which is called StateMultibody

--- include/crocoddyl/multibody/states/state-multibody.hpp
+++ include/crocoddyl/multibody/states/state-multibody.hpp
@@ -9,6 +9,37 @@
 #ifndef CROCODDYL_MULTIBODY_STATES_STATE_MULTIBODY_HPP_
 #define CROCODDYL_MULTIBODY_STATES_STATE_MULTIBODY_HPP_
 
-// TODO: StatePinocchio
+#include "crocoddyl/core/state-base.hpp"
+#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/multibody/data.hpp>
+
+namespace crocoddyl {
+
+class StateMultibody : public StateAbstract {
+ public:
+  StateMultibody(pinocchio::Model& model);
+  ~StateMultibody();
+
+  Eigen::VectorXd zero() override;
+  Eigen::VectorXd rand() override;
+  void diff(const Eigen::Ref<const Eigen::VectorXd>& x0, const Eigen::Ref<const Eigen::VectorXd>& x1,
+            Eigen::Ref<Eigen::VectorXd> dxout) override;
+  void integrate(const Eigen::Ref<const Eigen::VectorXd>& x, const Eigen::Ref<const Eigen::VectorXd>& dx,
+                 Eigen::Ref<Eigen::VectorXd> xout) override;
+  void Jdiff(const Eigen::Ref<const Eigen::VectorXd>&, const Eigen::Ref<const Eigen::VectorXd>&,
+             Eigen::Ref<Eigen::MatrixXd> Jfirst, Eigen::Ref<Eigen::MatrixXd> Jsecond,
+             Jcomponent firstsecond = Jcomponent::both) override;
+  void Jintegrate(const Eigen::Ref<const Eigen::VectorXd>&, const Eigen::Ref<const Eigen::VectorXd>&,
+                  Eigen::Ref<Eigen::MatrixXd> Jfirst, Eigen::Ref<Eigen::MatrixXd> Jsecond,
+                  Jcomponent firstsecond = Jcomponent::both) override;
+
+ private:
+  pinocchio::Model model_;
+  Eigen::VectorXd x0_;
+  Eigen::VectorXd dx_;
+  Eigen::MatrixXd Jdq_;
+};
+
+}  // namespace crocoddyl
 
 #endif  // CROCODDYL_MULTIBODY_STATES_STATE_MULTIBODY_HPP_
