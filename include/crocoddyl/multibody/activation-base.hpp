1824eec319176f7018b4cc31a6509efd34d9d8d7
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-07-23 16:01:40 +0200

    [activation] c++ implementation of the activation abstract

--- include/crocoddyl/multibody/activation-base.hpp
+++ include/crocoddyl/multibody/activation-base.hpp
@@ -9,6 +9,47 @@
 #ifndef CROCODDYL_MULTIBODY_ACTIVATION_BASE_HPP_
 #define CROCODDYL_MULTIBODY_ACTIVATION_BASE_HPP_
 
-// TODO: ActivationModelAbstract ActivationDataAbstract
+#include <Eigen/Dense>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+namespace crocoddyl {
+
+struct ActivationDataAbstract;  // forward declaration
+
+class ActivationModelAbstract {
+ public:
+  ActivationModelAbstract();
+  virtual ~ActivationModelAbstract();
+
+  virtual void calc(boost::shared_ptr<ActivationDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& r) = 0;
+  virtual void calcDiff(boost::shared_ptr<ActivationDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& r,
+                        const bool& recalc = true) = 0;
+  virtual boost::shared_ptr<ActivationDataAbstract> createData() = 0;
+
+ protected:
+  unsigned int ncost_;
+
+#ifdef PYTHON_BINDINGS
+ public:
+  void calc_wrap(boost::shared_ptr<ActivationDataAbstract>& data, const Eigen::VectorXd& r) { calc(data, r); }
+
+  void calcDiff_wrap(boost::shared_ptr<ActivationDataAbstract>& data, const Eigen::VectorXd& r, const bool& recalc) {
+    calcDiff(data, r, recalc);
+  }
+  void calcDiff_wrap(boost::shared_ptr<ActivationDataAbstract>& data, const Eigen::VectorXd& r) {
+    calcDiff(data, r, true);
+  }
+#endif
+};
+
+struct ActivationDataAbstract {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  template <typename Model>
+  ActivationDataAbstract(Model* const model) {}
+};
+
+}  // namespace crocoddyl
 
 #endif  // CROCODDYL_MULTIBODY_ACTIVATION_BASE_HPP_
---
+++ src/multibody/activation-base.cpp
@@ -0,0 +1,9 @@
+#include "crocoddyl/multibody/activation-base.hpp"
+
+namespace crocoddyl {
+
+ActivationModelAbstract::ActivationModelAbstract() {}
+
+ActivationModelAbstract::~ActivationModelAbstract() {}
+
+}  // namespace crocoddyl
