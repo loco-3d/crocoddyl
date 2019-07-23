5b1dbd51b12a70a87919edc0cbf4ca0c69f374ba
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-07-23 12:44:25 +0200

    [cost] Missed implementation of cost abstract

---
+++ src/multibody/cost-base.cpp
@@ -0,0 +1,60 @@
+#include "crocoddyl/multibody/cost-base.hpp"
+
+namespace crocoddyl {
+
+CostModelAbstract::CostModelAbstract(const pinocchio::Model& model, const unsigned int& ncost, const unsigned int& nu,
+                                     const bool& with_residuals)
+    : model_(model),
+      nq_(model.nq),
+      nv_(model.nv),
+      nu_(nu),
+      nx_(model.nq + model.nv),
+      ndx_(2 * model.nv),
+      ncost_(ncost),
+      with_residuals_(with_residuals),
+      unone_(Eigen::VectorXd::Zero(nu)) {
+  assert(nq_ != 0);
+  assert(nv_ != 0);
+  assert(nu_ != 0);
+}
+
+CostModelAbstract::CostModelAbstract(const pinocchio::Model& model, const unsigned int& ncost,
+                                     const bool& with_residuals)
+    : model_(model),
+      nq_(model.nq),
+      nv_(model.nv),
+      nu_(model.nv),
+      nx_(model.nq + model.nv),
+      ndx_(2 * model.nv),
+      ncost_(ncost),
+      with_residuals_(with_residuals),
+      unone_(Eigen::VectorXd::Zero(model.nv)) {
+  assert(nq_ != 0);
+  assert(nv_ != 0);
+  assert(nu_ != 0);
+}
+
+CostModelAbstract::~CostModelAbstract() {}
+
+void CostModelAbstract::calc(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x) {
+  calc(data, x, unone_);
+}
+
+void CostModelAbstract::calcDiff(boost::shared_ptr<CostDataAbstract>& data,
+                                 const Eigen::Ref<const Eigen::VectorXd>& x) {
+  calcDiff(data, x, unone_);
+}
+
+unsigned int CostModelAbstract::get_nq() const { return nq_; }
+
+unsigned int CostModelAbstract::get_nv() const { return nv_; }
+
+unsigned int CostModelAbstract::get_nu() const { return nu_; }
+
+unsigned int CostModelAbstract::get_nx() const { return nx_; }
+
+unsigned int CostModelAbstract::get_ndx() const { return ndx_; }
+
+unsigned int CostModelAbstract::get_ncost() const { return ncost_; }
+
+}  // namespace crocoddyl
