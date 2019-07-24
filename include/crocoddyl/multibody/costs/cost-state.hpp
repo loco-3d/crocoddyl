e926f2a7bd94854cad678bc13c5521b256e2575f
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-07-24 16:01:47 +0200

    [cost] c++ implementation of state cost

--- include/crocoddyl/multibody/costs/cost-state.hpp
+++ include/crocoddyl/multibody/costs/cost-state.hpp
@@ -9,6 +9,30 @@
 #ifndef CROCODDYL_MULTIBODY_COSTS_COST_STATE_HPP_
 #define CROCODDYL_MULTIBODY_COSTS_COST_STATE_HPP_
 
+#include "crocoddyl/core/state-base.hpp"
+#include "crocoddyl/multibody/cost-base.hpp"
 // TODO: CostModelControl CostDataControl
+
+namespace crocoddyl {
+
+class CostModelState : public CostModelAbstract {
+ public:
+  CostModelState(pinocchio::Model* const model, StateAbstract* state, ActivationModelAbstract* const activation,
+                 const Eigen::Ref<const Eigen::VectorXd>& xref, const unsigned int& nu);
+  CostModelState(pinocchio::Model* const model, StateAbstract* state, const Eigen::Ref<const Eigen::VectorXd>& xref,
+                 const unsigned int& nu);
+  ~CostModelState();
+
+  void calc(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+                    const Eigen::Ref<const Eigen::VectorXd>& u);
+  void calcDiff(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+                        const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc = true);
+
+ private:
+  StateAbstract* state_;
+  Eigen::VectorXd xref_;
+};
+
+}  // namespace crocoddyl
 
 #endif  // CROCODDYL_MULTIBODY_COSTS_COST_CONTROL_HPP_
---
+++ src/multibody/costs/cost-state.cpp
@@ -0,0 +1,34 @@
+#include "crocoddyl/multibody/costs/cost-state.hpp"
+
+namespace crocoddyl {
+
+CostModelState::CostModelState(pinocchio::Model* const model, StateAbstract* state,
+                               ActivationModelAbstract* const activation,
+                               const Eigen::Ref<const Eigen::VectorXd>& xref, const unsigned int& nu)
+    : CostModelAbstract(model, activation, state->get_ndx(), nu), state_(state), xref_(xref) {}
+
+CostModelState::CostModelState(pinocchio::Model* const model, StateAbstract* state,
+                               const Eigen::Ref<const Eigen::VectorXd>& xref, const unsigned int& nu)
+    : CostModelAbstract(model, state->get_ndx(), nu), state_(state), xref_(xref) {}
+
+CostModelState::~CostModelState() {}
+
+void CostModelState::calc(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+                          const Eigen::Ref<const Eigen::VectorXd>&) {
+  state_->diff(xref_, x, data->r);
+  activation_->calc(data->activation, data->r);
+  data->cost = data->activation->a_value;
+}
+
+void CostModelState::calcDiff(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+                          const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc) {
+  if (recalc) {
+    calc(data, x, u);
+  }
+  state_->Jdiff(xref_, x, data->Rx, data->Rx, second);
+  activation_->calcDiff(data->activation, data->r, recalc);
+  data->Lx = data->Rx.transpose() * data->activation->Ar;
+  data->Lxx = data->Rx.transpose() * data->activation->Arr * data->Rx;
+}
+
+}  // namespace crocoddyl
