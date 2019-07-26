9603832860c4b6091a8b8e7bce3e420a21de0a1b
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-07-26 18:58:43 +0200

    [cost] Added the frame placement cost

--- include/crocoddyl/multibody/costs/frame-placement.hpp
+++ include/crocoddyl/multibody/costs/frame-placement.hpp
@@ -9,8 +9,55 @@
 #ifndef CROCODDYL_MULTIBODY_COSTS_FRAME_PLACEMENT_HPP_
 #define CROCODDYL_MULTIBODY_COSTS_FRAME_PLACEMENT_HPP_
 
-// TODO: CostModelFramePlacement CostDataFramePlacement
-// TODO: CostModelFrameTranslation CostDataFrameTranslation
-// TODO: CostModelFrameRotation CostDataFrameRotation
+#include "crocoddyl/multibody/cost-base.hpp"
+#include <pinocchio/spatial/se3.hpp>
+
+namespace crocoddyl {
+
+struct FramePlacement {
+  FramePlacement(const unsigned int& frame, const pinocchio::SE3& oMf) : frame(frame), oMf(oMf) {}
+  unsigned int frame;
+  pinocchio::SE3 oMf;
+};
+
+class CostModelFramePlacement : public CostModelAbstract {
+ public:
+  CostModelFramePlacement(pinocchio::Model* const model, ActivationModelAbstract* const activation,
+                          const FramePlacement& Fref, const unsigned int& nu);
+  CostModelFramePlacement(pinocchio::Model* const model, ActivationModelAbstract* const activation,
+                          const FramePlacement& Fref);
+  CostModelFramePlacement(pinocchio::Model* const model, const FramePlacement& Fref, const unsigned int& nu);
+  CostModelFramePlacement(pinocchio::Model* const model, const FramePlacement& Fref);
+  ~CostModelFramePlacement();
+
+  void calc(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+            const Eigen::Ref<const Eigen::VectorXd>& u);
+  void calcDiff(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+                const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc = true);
+  boost::shared_ptr<CostDataAbstract> createData(pinocchio::Data* const data);
+
+  const FramePlacement& get_Mref() const;
+
+ private:
+  FramePlacement Mref_;
+};
+
+
+struct CostDataFramePlacement : public CostDataAbstract {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  template <typename Model>
+  CostDataFramePlacement(Model* const model, pinocchio::Data* const data)
+      : CostDataAbstract(model, data) {}
+
+  pinocchio::Motion::Vector6 r;
+  pinocchio::SE3 rMf;
+  pinocchio::SE3::Matrix6 J;
+  pinocchio::SE3::Matrix6 oJf;
+  pinocchio::SE3::Matrix6 rJf;
+  pinocchio::SE3::Matrix6 fJf;
+};
+
+}  // namespace crocoddyl
 
 #endif  // CROCODDYL_MULTIBODY_COSTS_FRAME_PLACEMENT_HPP_
---
+++ src/multibody/costs/frame-placement.cpp
@@ -0,0 +1,69 @@
+#include "crocoddyl/multibody/costs/frame-placement.hpp"
+#include "pinocchio/algorithm/frames.hpp"
+
+namespace crocoddyl {
+
+CostModelFramePlacement::CostModelFramePlacement(pinocchio::Model* const model,
+                                                 ActivationModelAbstract* const activation, const FramePlacement& Mref,
+                                                 const unsigned int& nu)
+    : CostModelAbstract(model, activation, nu), Mref_(Mref) {
+  assert(activation->get_nr() == 6 && "CostModelFramePlacement: activation::nr is not equals to 6");
+}
+
+CostModelFramePlacement::CostModelFramePlacement(pinocchio::Model* const model,
+                                                 ActivationModelAbstract* const activation, const FramePlacement& Mref)
+    : CostModelAbstract(model, activation), Mref_(Mref) {
+  assert(activation->get_nr() == 6 && "CostModelFramePlacement: activation::nr is not equals to 6");
+}
+
+CostModelFramePlacement::CostModelFramePlacement(pinocchio::Model* const model, const FramePlacement& Mref,
+                                                 const unsigned int& nu)
+    : CostModelAbstract(model, 6, nu), Mref_(Mref) {}
+
+CostModelFramePlacement::CostModelFramePlacement(pinocchio::Model* const model, const FramePlacement& Mref)
+    : CostModelAbstract(model, 6), Mref_(Mref) {}
+
+CostModelFramePlacement::~CostModelFramePlacement() {}
+
+void CostModelFramePlacement::calc(boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>&,
+                                   const Eigen::Ref<const Eigen::VectorXd>&) {
+  CostDataFramePlacement* d = static_cast<CostDataFramePlacement*>(data.get());
+
+  // Compute the frame placement w.r.t. the reference frame
+  d->rMf = Mref_.oMf.inverse() * d->pinocchio->oMf[Mref_.frame];
+  d->r = pinocchio::log6(d->rMf);
+
+  // Compute the cost
+  activation_->calc(d->activation, data->r);
+  d->cost = d->activation->a_value;
+}
+
+void CostModelFramePlacement::calcDiff(boost::shared_ptr<CostDataAbstract>& data,
+                                         const Eigen::Ref<const Eigen::VectorXd>& x,
+                                         const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc) {
+  if (recalc) {
+    calc(data, x, u);
+  }
+  // Update the frame placements
+  CostDataFramePlacement* d = static_cast<CostDataFramePlacement*>(data.get());
+  pinocchio::updateFramePlacements(*pinocchio_, *d->pinocchio);
+
+  // Compute the frame Jacobian at the error point
+  pinocchio::Jlog6(d->rMf, d->rJf);
+  pinocchio::getFrameJacobian(*pinocchio_, *d->pinocchio, Mref_.frame, pinocchio::LOCAL, d->fJf);
+  d->J = d->rJf * d->fJf;
+
+  // Compute the derivatives of the frame placement
+  activation_->calcDiff(d->activation, d->r, recalc);
+  d->Rx.leftCols(nv_) = d->J;
+  d->Lx.head(nv_) = d->J.transpose() * d->activation->Ar;
+  d->Lxx.topLeftCorner(nv_, nv_) = d->J.transpose() * d->activation->Arr * d->J;
+}
+
+boost::shared_ptr<CostDataAbstract> CostModelFramePlacement::createData(pinocchio::Data* const data) {
+  return boost::make_shared<CostDataFramePlacement>(this, data);
+}
+
+const FramePlacement& CostModelFramePlacement::get_Mref() const { return Mref_; }
+
+}  // namespace crocoddyl
