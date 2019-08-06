c40df8ba1136c03f80fb95784e370e526bbddb85
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-06 18:31:47 +0200

    [activation] Added asserts of the r dimension

--- src/core/activations/quadratic.cpp
+++ src/core/activations/quadratic.cpp
@@ -8,11 +8,13 @@ ActivationModelQuad::~ActivationModelQuad() {}
 
 void ActivationModelQuad::calc(const boost::shared_ptr<ActivationDataAbstract>& data,
                                const Eigen::Ref<const Eigen::VectorXd>& r) {
+  assert(r.size() == nr_ && "ActivationModelQuad::calc: r has wrong dimension");
   data->a_value = 0.5 * r.transpose() * r;
 }
 
 void ActivationModelQuad::calcDiff(const boost::shared_ptr<ActivationDataAbstract>& data,
                                    const Eigen::Ref<const Eigen::VectorXd>& r, const bool& recalc) {
+  assert(r.size() == nr_ && "ActivationModelQuad::calcDiff: r has wrong dimension");
   if (recalc) {
     calc(data, r);
   }
