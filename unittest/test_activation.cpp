e382335cce8d68da47b47593a7344b8172e2b610
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-08-07 19:40:28 +0200

    fix the mpi copyright

--- unittest/test_activation.cpp
+++ unittest/test_activation.cpp
@@ -2,7 +2,7 @@
  * @file test_activation.cpp
  * @author Maximilien Naveau (maximilien.naveau@gmail.com)
  * @license License BSD-2-Clause
- * @copyright Copyright (c) 2019, New York University, Max Planck Gesellshaft and LAAS.
+ * @copyright Copyright (c) 2019, New York University, Max Planck Gesellschaft and LAAS.
  * @date 2019-06-18
  */
 
