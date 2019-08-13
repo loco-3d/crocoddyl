4bc2872c69e0e3eb0e005a8b78c2685dcd5f3be9
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-13 17:12:15 +0200

    [contact] Added c++ implementation of multiple contact

--- include/crocoddyl/multibody/contacts/multiple-contacts.hpp
+++ include/crocoddyl/multibody/contacts/multiple-contacts.hpp
@@ -9,6 +9,60 @@
 #ifndef CROCODDYL_MULTIBODY_CONTACTS_MULTIPLE_CONTACTS_HPP_
 #define CROCODDYL_MULTIBODY_CONTACTS_MULTIPLE_CONTACTS_HPP_
 
-// TODO: ContactModelMultiple ContactDataMultiple
+#include <string>
+#include <map>
+#include <utility>
+#include "crocoddyl/multibody/contact-base.hpp"
+
+namespace crocoddyl {
+
+struct ContactItem {
+  ContactItem() {}
+  ContactItem(const std::string& name, ContactModelAbstract* contact) : name(name), contact(contact) {}
+
+  std::string name;
+  ContactModelAbstract* contact;
+};
+
+class ContactModelMultiple : public ContactModelAbstract {
+ public:
+  typedef std::map<std::string, ContactItem> ContactModelContainer;
+  typedef std::map<std::string, boost::shared_ptr<ContactDataAbstract> > ContactDataContainer;
+
+  ContactModelMultiple(StateMultibody& state);
+  ~ContactModelMultiple();
+
+  void addContact(const std::string& name, ContactModelAbstract* const contact);
+  void removeContact(const std::string& name);
+
+  void calc(const boost::shared_ptr<ContactDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x);
+  void calcDiff(const boost::shared_ptr<ContactDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+                const bool& recalc = true);
+  boost::shared_ptr<ContactDataAbstract> createData(pinocchio::Data* const data);
+
+  const ContactModelContainer& get_contacts() const;
+  const unsigned int& get_nc() const;
+
+ private:
+  ContactModelContainer contacts_;
+  unsigned int nc_;
+};
+
+struct ContactDataMultiple : ContactDataAbstract {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  template <typename Model>
+  ContactDataMultiple(Model* const model, pinocchio::Data* const data) : ContactDataAbstract(model, data) {
+    for (ContactModelMultiple::ContactModelContainer::const_iterator it = model->get_contacts().begin();
+         it != model->get_contacts().end(); ++it) {
+      const ContactItem& item = it->second;
+      contacts.insert(std::make_pair(item.name, item.contact->createData(data)));
+    }
+  }
+
+  ContactModelMultiple::ContactDataContainer contacts;
+};
+
+}  // namespace crocoddyl
 
 #endif  // CROCODDYL_MULTIBODY_CONTACTS_MULTIPLE_CONTACTS_HPP_
