b9bc63f1e341795c30c5fd08c1e41eea860ad209
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-14 18:47:13 +0200

    [cleanup] Use capitals only for objects that need to be defined externally

--- unittest/bindings/test_costs.py
+++ unittest/bindings/test_costs.py
@@ -68,11 +68,11 @@ class CostModelSumTestCase(unittest.TestCase):
         self.x = self.ROBOT_STATE.rand()
         self.u = np.matrix(np.random.rand(self.ROBOT_MODEL.nv)).T
 
-        self.COST_SUM = crocoddyl.CostModelSum(self.ROBOT_STATE)
-        self.COST_SUM.addCost('myCost', self.COST, 1.)
+        self.cost_sum = crocoddyl.CostModelSum(self.ROBOT_STATE)
+        self.cost_sum.addCost('myCost', self.COST, 1.)
 
         self.data = self.COST.createData(self.robot_data)
-        self.data_SUM = self.COST_SUM.createData(self.robot_data)
+        self.data_sum = self.cost_sum.createData(self.robot_data)
 
         nq = self.ROBOT_MODEL.nq
         pinocchio.forwardKinematics(self.ROBOT_MODEL, self.robot_data, self.x[:nq], self.x[nq:])
@@ -80,40 +80,40 @@ class CostModelSumTestCase(unittest.TestCase):
         pinocchio.updateFramePlacements(self.ROBOT_MODEL, self.robot_data)
 
     def test_dimensions(self):
-        self.assertEqual(self.COST.State.nx, self.COST_SUM.State.nx, "Wrong nx.")
-        self.assertEqual(self.COST.State.ndx, self.COST_SUM.State.ndx, "Wrong ndx.")
-        self.assertEqual(self.COST.nu, self.COST_SUM.nu, "Wrong nu.")
-        self.assertEqual(self.COST.State.nq, self.COST_SUM.State.nq, "Wrong nq.")
-        self.assertEqual(self.COST.State.nv, self.COST_SUM.State.nv, "Wrong nv.")
-        self.assertEqual(self.COST.activation.nr, self.COST_SUM.activation.nr, "Wrong nr.")
+        self.assertEqual(self.COST.State.nx, self.cost_sum.State.nx, "Wrong nx.")
+        self.assertEqual(self.COST.State.ndx, self.cost_sum.State.ndx, "Wrong ndx.")
+        self.assertEqual(self.COST.nu, self.cost_sum.nu, "Wrong nu.")
+        self.assertEqual(self.COST.State.nq, self.cost_sum.State.nq, "Wrong nq.")
+        self.assertEqual(self.COST.State.nv, self.cost_sum.State.nv, "Wrong nv.")
+        self.assertEqual(self.COST.activation.nr, self.cost_sum.activation.nr, "Wrong nr.")
 
     def test_calc(self):
         # Run calc for both action models
         self.COST.calc(self.data, self.x, self.u)
-        self.COST_SUM.calc(self.data_SUM, self.x, self.u)
+        self.cost_sum.calc(self.data_sum, self.x, self.u)
         # Checking the cost value and its residual
-        self.assertAlmostEqual(self.data.cost, self.data_SUM.cost, 10, "Wrong cost value.")
-        self.assertTrue(np.allclose(self.data.costResiduals, self.data_SUM.costResiduals, atol=1e-9),
+        self.assertAlmostEqual(self.data.cost, self.data_sum.cost, 10, "Wrong cost value.")
+        self.assertTrue(np.allclose(self.data.costResiduals, self.data_sum.costResiduals, atol=1e-9),
                         "Wrong cost residuals.")
 
     def test_calcDiff(self):
         # Run calc for both action models
         self.COST.calcDiff(self.data, self.x, self.u)
-        self.COST_SUM.calcDiff(self.data_SUM, self.x, self.u)
+        self.cost_sum.calcDiff(self.data_sum, self.x, self.u)
         # Checking the cost value and its residual
-        self.assertAlmostEqual(self.data.cost, self.data_SUM.cost, 10, "Wrong cost value.")
-        self.assertTrue(np.allclose(self.data.costResiduals, self.data_SUM.costResiduals, atol=1e-9),
+        self.assertAlmostEqual(self.data.cost, self.data_sum.cost, 10, "Wrong cost value.")
+        self.assertTrue(np.allclose(self.data.costResiduals, self.data_sum.costResiduals, atol=1e-9),
                         "Wrong cost residuals.")
         # Checking the Jacobians and Hessians of the cost
-        self.assertTrue(np.allclose(self.data.Lx, self.data_SUM.Lx, atol=1e-9), "Wrong Lx.")
-        self.assertTrue(np.allclose(self.data.Lu, self.data_SUM.Lu, atol=1e-9), "Wrong Lu.")
-        self.assertTrue(np.allclose(self.data.Lxx, self.data_SUM.Lxx, atol=1e-9), "Wrong Lxx.")
-        self.assertTrue(np.allclose(self.data.Lxu, self.data_SUM.Lxu, atol=1e-9), "Wrong Lxu.")
-        self.assertTrue(np.allclose(self.data.Luu, self.data_SUM.Luu, atol=1e-9), "Wrong Luu.")
+        self.assertTrue(np.allclose(self.data.Lx, self.data_sum.Lx, atol=1e-9), "Wrong Lx.")
+        self.assertTrue(np.allclose(self.data.Lu, self.data_sum.Lu, atol=1e-9), "Wrong Lu.")
+        self.assertTrue(np.allclose(self.data.Lxx, self.data_sum.Lxx, atol=1e-9), "Wrong Lxx.")
+        self.assertTrue(np.allclose(self.data.Lxu, self.data_sum.Lxu, atol=1e-9), "Wrong Lxu.")
+        self.assertTrue(np.allclose(self.data.Luu, self.data_sum.Luu, atol=1e-9), "Wrong Luu.")
 
     def test_removeCost(self):
-        self.COST_SUM.removeCost("myCost")
-        self.assertEqual(len(self.COST_SUM.costs), 0, "The number of cost items should be zero")
+        self.cost_sum.removeCost("myCost")
+        self.assertEqual(len(self.cost_sum.costs), 0, "The number of cost items should be zero")
 
 
 class StateCostTest(CostModelAbstractTestCase):
