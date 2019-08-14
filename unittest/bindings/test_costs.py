316f1d09c6b9448812f5970bb47857d20bfb3552
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-14 18:41:32 +0200

    [cleanup][unittest] Do not make fancy Python construction of classes

--- unittest/bindings/test_costs.py
+++ unittest/bindings/test_costs.py
@@ -7,136 +7,109 @@ import sys
 
 
 class CostModelAbstractTestCase(unittest.TestCase):
+    ROBOT_MODEL = None
+    ROBOT_STATE = None
     COST = None
     COST_DER = None
 
     def setUp(self):
-        self.ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
-        self.ROBOT_DATA = self.ROBOT_MODEL.createData()
-        self.STATE = crocoddyl.StateMultibody(self.ROBOT_MODEL)
-        self.x = self.STATE.rand()
+        self.robot_data = self.ROBOT_MODEL.createData()
+        self.x = self.ROBOT_STATE.rand()
         self.u = np.matrix(np.random.rand(self.ROBOT_MODEL.nv)).T
 
-        if self.COST is crocoddyl.CostModelFramePlacement:
-            Mref = crocoddyl.FramePlacement(self.ROBOT_MODEL.getFrameId('rleg5_joint'), pinocchio.SE3.Random())
-            self.cost = self.COST(self.STATE, Mref)
-            self.costDer = self.COST_DER(self.STATE, Mref=Mref)
-        elif self.COST is crocoddyl.CostModelFrameTranslation:
-            xref = crocoddyl.FrameTranslation(self.ROBOT_MODEL.getFrameId('rleg5_joint'),
-                                              np.matrix(np.random.rand(3)).T)
-            self.cost = self.COST(self.STATE, xref)
-            self.costDer = self.COST_DER(self.STATE, xref=xref)
-        elif self.COST is crocoddyl.CostModelControl:
-            self.cost = self.COST(self.STATE)
-            self.costDer = self.COST_DER(self.STATE)
-        elif self.COST is crocoddyl.CostModelState:
-            self.cost = self.COST(self.STATE)
-            self.costDer = self.COST_DER(self.STATE)
-        self.DATA = self.cost.createData(self.ROBOT_DATA)
-        self.DATA_DER = self.costDer.createData(self.ROBOT_DATA)
+        self.data = self.COST.createData(self.robot_data)
+        self.data_der = self.COST_DER.createData(self.robot_data)
 
         nq = self.ROBOT_MODEL.nq
-        pinocchio.forwardKinematics(self.ROBOT_MODEL, self.ROBOT_DATA, self.x[:nq], self.x[nq:])
-        pinocchio.computeJointJacobians(self.ROBOT_MODEL, self.ROBOT_DATA, self.x[:nq])
-        pinocchio.updateFramePlacements(self.ROBOT_MODEL, self.ROBOT_DATA)
+        pinocchio.forwardKinematics(self.ROBOT_MODEL, self.robot_data, self.x[:nq], self.x[nq:])
+        pinocchio.computeJointJacobians(self.ROBOT_MODEL, self.robot_data, self.x[:nq])
+        pinocchio.updateFramePlacements(self.ROBOT_MODEL, self.robot_data)
 
     def test_dimensions(self):
-        self.assertEqual(self.cost.State.nx, self.costDer.State.nx, "Wrong nx.")
-        self.assertEqual(self.cost.State.ndx, self.costDer.State.ndx, "Wrong ndx.")
-        self.assertEqual(self.cost.nu, self.costDer.nu, "Wrong nu.")
-        self.assertEqual(self.cost.State.nq, self.costDer.State.nq, "Wrong nq.")
-        self.assertEqual(self.cost.State.nv, self.costDer.State.nv, "Wrong nv.")
-        self.assertEqual(self.cost.activation.nr, self.costDer.activation.nr, "Wrong nr.")
+        self.assertEqual(self.COST.State.nx, self.COST_DER.State.nx, "Wrong nx.")
+        self.assertEqual(self.COST.State.ndx, self.COST_DER.State.ndx, "Wrong ndx.")
+        self.assertEqual(self.COST.nu, self.COST_DER.nu, "Wrong nu.")
+        self.assertEqual(self.COST.State.nq, self.COST_DER.State.nq, "Wrong nq.")
+        self.assertEqual(self.COST.State.nv, self.COST_DER.State.nv, "Wrong nv.")
+        self.assertEqual(self.COST.activation.nr, self.COST_DER.activation.nr, "Wrong nr.")
 
     def test_calc(self):
         # Run calc for both action models
-        self.cost.calc(self.DATA, self.x, self.u)
-        self.costDer.calc(self.DATA_DER, self.x, self.u)
+        self.COST.calc(self.data, self.x, self.u)
+        self.COST_DER.calc(self.data_der, self.x, self.u)
         # Checking the cost value and its residual
-        self.assertAlmostEqual(self.DATA.cost, self.DATA_DER.cost, 10, "Wrong cost value.")
-        self.assertTrue(np.allclose(self.DATA.costResiduals, self.DATA_DER.costResiduals, atol=1e-9),
+        self.assertAlmostEqual(self.data.cost, self.data_der.cost, 10, "Wrong cost value.")
+        self.assertTrue(np.allclose(self.data.costResiduals, self.data_der.costResiduals, atol=1e-9),
                         "Wrong cost residuals.")
 
     def test_calcDiff(self):
         # Run calc for both action models
-        self.cost.calcDiff(self.DATA, self.x, self.u)
-        self.costDer.calcDiff(self.DATA_DER, self.x, self.u)
+        self.COST.calcDiff(self.data, self.x, self.u)
+        self.COST_DER.calcDiff(self.data_der, self.x, self.u)
         # Checking the cost value and its residual
-        self.assertAlmostEqual(self.DATA.cost, self.DATA_DER.cost, 10, "Wrong cost value.")
-        self.assertTrue(np.allclose(self.DATA.costResiduals, self.DATA_DER.costResiduals, atol=1e-9),
+        self.assertAlmostEqual(self.data.cost, self.data_der.cost, 10, "Wrong cost value.")
+        self.assertTrue(np.allclose(self.data.costResiduals, self.data_der.costResiduals, atol=1e-9),
                         "Wrong cost residuals.")
         # Checking the Jacobians and Hessians of the cost
-        self.assertTrue(np.allclose(self.DATA.Lx, self.DATA_DER.Lx, atol=1e-9), "Wrong Lx.")
-        self.assertTrue(np.allclose(self.DATA.Lu, self.DATA_DER.Lu, atol=1e-9), "Wrong Lu.")
-        self.assertTrue(np.allclose(self.DATA.Lxx, self.DATA_DER.Lxx, atol=1e-9), "Wrong Lxx.")
-        self.assertTrue(np.allclose(self.DATA.Lxu, self.DATA_DER.Lxu, atol=1e-9), "Wrong Lxu.")
-        self.assertTrue(np.allclose(self.DATA.Luu, self.DATA_DER.Luu, atol=1e-9), "Wrong Luu.")
+        self.assertTrue(np.allclose(self.data.Lx, self.data_der.Lx, atol=1e-9), "Wrong Lx.")
+        self.assertTrue(np.allclose(self.data.Lu, self.data_der.Lu, atol=1e-9), "Wrong Lu.")
+        self.assertTrue(np.allclose(self.data.Lxx, self.data_der.Lxx, atol=1e-9), "Wrong Lxx.")
+        self.assertTrue(np.allclose(self.data.Lxu, self.data_der.Lxu, atol=1e-9), "Wrong Lxu.")
+        self.assertTrue(np.allclose(self.data.Luu, self.data_der.Luu, atol=1e-9), "Wrong Luu.")
 
 
 class CostModelSumTestCase(unittest.TestCase):
+    ROBOT_MODEL = None
+    ROBOT_STATE = None
     COST = None
 
     def setUp(self):
-        self.ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
-        self.ROBOT_DATA = self.ROBOT_MODEL.createData()
-        self.STATE = crocoddyl.StateMultibody(self.ROBOT_MODEL)
-        self.x = self.STATE.rand()
+        self.robot_data = self.ROBOT_MODEL.createData()
+        self.x = self.ROBOT_STATE.rand()
         self.u = np.matrix(np.random.rand(self.ROBOT_MODEL.nv)).T
 
-        self.COST_SUM = crocoddyl.CostModelSum(self.STATE)
+        self.COST_SUM = crocoddyl.CostModelSum(self.ROBOT_STATE)
+        self.COST_SUM.addCost('myCost', self.COST, 1.)
 
-        if self.COST is crocoddyl.CostModelFramePlacement:
-            Mref = crocoddyl.FramePlacement(self.ROBOT_MODEL.getFrameId('rleg5_joint'), pinocchio.SE3.Random())
-            self.cost = self.COST(self.STATE, Mref)
-        elif self.COST is crocoddyl.CostModelFrameTranslation:
-            xref = crocoddyl.FrameTranslation(self.ROBOT_MODEL.getFrameId('rleg5_joint'),
-                                              np.matrix(np.random.rand(3)).T)
-            self.cost = self.COST(self.STATE, xref)
-        elif self.COST is crocoddyl.CostModelControl:
-            self.cost = self.COST(self.STATE)
-        elif self.COST is crocoddyl.CostModelState:
-            self.cost = self.COST(self.STATE)
-        self.COST_SUM.addCost('myCost', self.cost, 1.)
-
-        self.DATA = self.cost.createData(self.ROBOT_DATA)
-        self.DATA_SUM = self.COST_SUM.createData(self.ROBOT_DATA)
+        self.data = self.COST.createData(self.robot_data)
+        self.data_SUM = self.COST_SUM.createData(self.robot_data)
 
         nq = self.ROBOT_MODEL.nq
-        pinocchio.forwardKinematics(self.ROBOT_MODEL, self.ROBOT_DATA, self.x[:nq], self.x[nq:])
-        pinocchio.computeJointJacobians(self.ROBOT_MODEL, self.ROBOT_DATA, self.x[:nq])
-        pinocchio.updateFramePlacements(self.ROBOT_MODEL, self.ROBOT_DATA)
+        pinocchio.forwardKinematics(self.ROBOT_MODEL, self.robot_data, self.x[:nq], self.x[nq:])
+        pinocchio.computeJointJacobians(self.ROBOT_MODEL, self.robot_data, self.x[:nq])
+        pinocchio.updateFramePlacements(self.ROBOT_MODEL, self.robot_data)
 
     def test_dimensions(self):
-        self.assertEqual(self.cost.State.nx, self.COST_SUM.State.nx, "Wrong nx.")
-        self.assertEqual(self.cost.State.ndx, self.COST_SUM.State.ndx, "Wrong ndx.")
-        self.assertEqual(self.cost.nu, self.COST_SUM.nu, "Wrong nu.")
-        self.assertEqual(self.cost.State.nq, self.COST_SUM.State.nq, "Wrong nq.")
-        self.assertEqual(self.cost.State.nv, self.COST_SUM.State.nv, "Wrong nv.")
-        self.assertEqual(self.cost.activation.nr, self.COST_SUM.activation.nr, "Wrong nr.")
+        self.assertEqual(self.COST.State.nx, self.COST_SUM.State.nx, "Wrong nx.")
+        self.assertEqual(self.COST.State.ndx, self.COST_SUM.State.ndx, "Wrong ndx.")
+        self.assertEqual(self.COST.nu, self.COST_SUM.nu, "Wrong nu.")
+        self.assertEqual(self.COST.State.nq, self.COST_SUM.State.nq, "Wrong nq.")
+        self.assertEqual(self.COST.State.nv, self.COST_SUM.State.nv, "Wrong nv.")
+        self.assertEqual(self.COST.activation.nr, self.COST_SUM.activation.nr, "Wrong nr.")
 
     def test_calc(self):
         # Run calc for both action models
-        self.cost.calc(self.DATA, self.x, self.u)
-        self.COST_SUM.calc(self.DATA_SUM, self.x, self.u)
+        self.COST.calc(self.data, self.x, self.u)
+        self.COST_SUM.calc(self.data_SUM, self.x, self.u)
         # Checking the cost value and its residual
-        self.assertAlmostEqual(self.DATA.cost, self.DATA_SUM.cost, 10, "Wrong cost value.")
-        self.assertTrue(np.allclose(self.DATA.costResiduals, self.DATA_SUM.costResiduals, atol=1e-9),
+        self.assertAlmostEqual(self.data.cost, self.data_SUM.cost, 10, "Wrong cost value.")
+        self.assertTrue(np.allclose(self.data.costResiduals, self.data_SUM.costResiduals, atol=1e-9),
                         "Wrong cost residuals.")
 
     def test_calcDiff(self):
         # Run calc for both action models
-        self.cost.calcDiff(self.DATA, self.x, self.u)
-        self.COST_SUM.calcDiff(self.DATA_SUM, self.x, self.u)
+        self.COST.calcDiff(self.data, self.x, self.u)
+        self.COST_SUM.calcDiff(self.data_SUM, self.x, self.u)
         # Checking the cost value and its residual
-        self.assertAlmostEqual(self.DATA.cost, self.DATA_SUM.cost, 10, "Wrong cost value.")
-        self.assertTrue(np.allclose(self.DATA.costResiduals, self.DATA_SUM.costResiduals, atol=1e-9),
+        self.assertAlmostEqual(self.data.cost, self.data_SUM.cost, 10, "Wrong cost value.")
+        self.assertTrue(np.allclose(self.data.costResiduals, self.data_SUM.costResiduals, atol=1e-9),
                         "Wrong cost residuals.")
         # Checking the Jacobians and Hessians of the cost
-        self.assertTrue(np.allclose(self.DATA.Lx, self.DATA_SUM.Lx, atol=1e-9), "Wrong Lx.")
-        self.assertTrue(np.allclose(self.DATA.Lu, self.DATA_SUM.Lu, atol=1e-9), "Wrong Lu.")
-        self.assertTrue(np.allclose(self.DATA.Lxx, self.DATA_SUM.Lxx, atol=1e-9), "Wrong Lxx.")
-        self.assertTrue(np.allclose(self.DATA.Lxu, self.DATA_SUM.Lxu, atol=1e-9), "Wrong Lxu.")
-        self.assertTrue(np.allclose(self.DATA.Luu, self.DATA_SUM.Luu, atol=1e-9), "Wrong Luu.")
+        self.assertTrue(np.allclose(self.data.Lx, self.data_SUM.Lx, atol=1e-9), "Wrong Lx.")
+        self.assertTrue(np.allclose(self.data.Lu, self.data_SUM.Lu, atol=1e-9), "Wrong Lu.")
+        self.assertTrue(np.allclose(self.data.Lxx, self.data_SUM.Lxx, atol=1e-9), "Wrong Lxx.")
+        self.assertTrue(np.allclose(self.data.Lxu, self.data_SUM.Lxu, atol=1e-9), "Wrong Lxu.")
+        self.assertTrue(np.allclose(self.data.Luu, self.data_SUM.Luu, atol=1e-9), "Wrong Luu.")
 
     def test_removeCost(self):
         self.COST_SUM.removeCost("myCost")
@@ -144,39 +117,67 @@ class CostModelSumTestCase(unittest.TestCase):
 
 
 class StateCostTest(CostModelAbstractTestCase):
-    COST = crocoddyl.CostModelState
-    COST_DER = utils.StateCostDerived
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    COST = crocoddyl.CostModelState(ROBOT_STATE)
+    COST_DER = utils.StateCostDerived(ROBOT_STATE)
 
 
 class StateCostSumTest(CostModelSumTestCase):
-    COST = crocoddyl.CostModelState
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    COST = crocoddyl.CostModelState(ROBOT_STATE)
 
 
 class ControlCostTest(CostModelAbstractTestCase):
-    COST = crocoddyl.CostModelControl
-    COST_DER = utils.ControlCostDerived
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    COST = crocoddyl.CostModelControl(ROBOT_STATE)
+    COST_DER = utils.ControlCostDerived(ROBOT_STATE)
 
 
 class ControlCostSumTest(CostModelSumTestCase):
-    COST = crocoddyl.CostModelControl
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    COST = crocoddyl.CostModelControl(ROBOT_STATE)
 
 
 class FramePlacementCostTest(CostModelAbstractTestCase):
-    COST = crocoddyl.CostModelFramePlacement
-    COST_DER = utils.FramePlacementCostDerived
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    Mref = crocoddyl.FramePlacement(ROBOT_MODEL.getFrameId('rleg5_joint'), pinocchio.SE3.Random())
+    COST = crocoddyl.CostModelFramePlacement(ROBOT_STATE, Mref)
+    COST_DER = utils.FramePlacementCostDerived(ROBOT_STATE, Mref=Mref)
 
 
 class FramePlacementCostSumTest(CostModelSumTestCase):
-    COST = crocoddyl.CostModelFramePlacement
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    Mref = crocoddyl.FramePlacement(ROBOT_MODEL.getFrameId('rleg5_joint'), pinocchio.SE3.Random())
+    COST = crocoddyl.CostModelFramePlacement(ROBOT_STATE, Mref)
 
 
 class FrameTranslationCostTest(CostModelAbstractTestCase):
-    COST = crocoddyl.CostModelFrameTranslation
-    COST_DER = utils.FrameTranslationCostDerived
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    xref = crocoddyl.FrameTranslation(ROBOT_MODEL.getFrameId('rleg5_joint'), np.matrix(np.random.rand(3)).T)
+    COST = crocoddyl.CostModelFrameTranslation(ROBOT_STATE, xref)
+    COST_DER = utils.FrameTranslationCostDerived(ROBOT_STATE, xref=xref)
 
 
 class FrameTranslationCostSumTest(CostModelSumTestCase):
-    COST = crocoddyl.CostModelFrameTranslation
+    ROBOT_MODEL = pinocchio.buildSampleModelHumanoidRandom()
+    ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+    xref = crocoddyl.FrameTranslation(ROBOT_MODEL.getFrameId('rleg5_joint'), np.matrix(np.random.rand(3)).T)
+    COST = crocoddyl.CostModelFrameTranslation(ROBOT_STATE, xref)
 
 
 if __name__ == '__main__':
