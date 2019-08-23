7a48a46e1a245265ac9fe79e66cd8fe6650c7e97
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-23 20:12:02 +0200

    [diagostic] Added white background with floor

--- bindings/python/crocoddyl/__init__.py
+++ bindings/python/crocoddyl/__init__.py
@@ -2,6 +2,20 @@ from .libcrocoddyl_pywrap import *
 from .libcrocoddyl_pywrap import __version__
 
 
+def setGepettoViewerBackground(robot):
+    if not hasattr(robot, 'viewer'):
+        # Spawn robot model
+        robot.initDisplay(loadModel=True)
+        # Set white background and floor
+        window_id = robot.viewer.gui.getWindowID('python-pinocchio')
+        robot.viewer.gui.setBackgroundColor1(window_id, [1., 1., 1., 1.])
+        robot.viewer.gui.setBackgroundColor2(window_id, [1., 1., 1., 1.])
+        robot.viewer.gui.addFloor('hpp-gui/floor')
+        robot.viewer.gui.setScale('hpp-gui/floor', [0.5, 0.5, 0.5])
+        robot.viewer.gui.setColor('hpp-gui/floor', [0.7, 0.7, 0.7, 1.])
+        robot.viewer.gui.setLightingMode('hpp-gui/floor', 'OFF')
+
+
 def displayTrajectory(robot, xs, dt=0.1, rate=-1, cameraTF=None):
     """  Display a robot trajectory xs using Gepetto-viewer gui.
 
@@ -11,8 +25,7 @@ def displayTrajectory(robot, xs, dt=0.1, rate=-1, cameraTF=None):
     :param rate: visualization rate
     :param cameraTF: camera transform
     """
-    if not hasattr(robot, 'viewer'):
-        robot.initDisplay(loadModel=True)
+    setGepettoViewerBackground(robot)
     if cameraTF is not None:
         robot.viewer.gui.setCameraTransform(0, cameraTF)
     import numpy as np
