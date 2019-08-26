c45e50fddf42c28e257d51fe2594750e07cb0084
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-26 20:24:18 +0200

    [bindings] Added the Python bindings of the NumDiff action classes

--- bindings/python/crocoddyl/core/numdiff/diff-action.hpp
+++ bindings/python/crocoddyl/core/numdiff/diff-action.hpp
@@ -6,47 +6,50 @@
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef BINDINGS_PYTHON_CROCODDYL_CORE_NUMDIFF_ACTION_HPP_
-#define BINDINGS_PYTHON_CROCODDYL_CORE_NUMDIFF_ACTION_HPP_
+#ifndef BINDINGS_PYTHON_CROCODDYL_CORE_NUMDIFF_DIFF_ACTION_HPP_
+#define BINDINGS_PYTHON_CROCODDYL_CORE_NUMDIFF_DIFF_ACTION_HPP_
 
-#include "crocoddyl/core/numdiff/action.hpp"
+#include "crocoddyl/core/numdiff/diff-action.hpp"
 
 namespace crocoddyl {
 namespace python {
 
 namespace bp = boost::python;
 
-void exposeActionNumDiff() {
-  bp::class_<ActionModelNumDiff, bp::bases<ActionModelAbstract> >(
-      "ActionModelNumDiff", "Abstract class for computing calcDiff by using numerical differentiation.\n\n",
-      bp::init<ActionModelAbstract&, bp::optional<bool> >(
+void exposeDifferentialActionNumDiff() {
+  bp::class_<DifferentialActionModelNumDiff, bp::bases<DifferentialActionModelAbstract> >(
+      "DifferentialActionModelNumDiff",
+      "Abstract class for computing calcDiff by using numerical differentiation.\n\n",
+      bp::init<DifferentialActionModelAbstract&, bp::optional<bool> >(
           bp::args(" self", " model", " gaussApprox=False"),
           "Initialize the action model NumDiff.\n\n"
           ":param model: action model where we compute the derivatives through NumDiff,\n"
           ":param gaussApprox: compute the Hessian using Gauss approximation")[bp::with_custodian_and_ward<1, 2>()])
-      .def("calc", &ActionModelNumDiff::calc, bp::args(" self", " data", " x", " u"),
+      .def("calc", &DifferentialActionModelNumDiff::calc, bp::args(" self", " data", " x", " u"),
            "Compute the next state and cost value.\n\n"
            "The system evolution is described in model.\n"
            ":param data: NumDiff action data\n"
            ":param x: time-discrete state vector\n"
            ":param u: time-discrete control input")
-      .def("calcDiff", &ActionModelNumDiff::calcDiff, bp::args(" self", " data", " x", " u", " recalc=True"),
+      .def("calcDiff", &DifferentialActionModelNumDiff::calcDiff,
+           bp::args(" self", " data", " x", " u", " recalc=True"),
            "Compute the derivatives of the dynamics and cost functions.\n\n"
            "It computes the Jacobian and Hessian using numerical differentiation.\n"
            ":param data: NumDiff action data\n"
            ":param x: time-discrete state vector\n"
            ":param u: time-discrete control input\n"
            ":param recalc: If true, it updates the state evolution and the cost value.")
-      .def("createData", &ActionModelNumDiff::createData, bp::args(" self"),
+      .def("createData", &DifferentialActionModelNumDiff::createData, bp::args(" self"),
            "Create the action data.\n\n"
            "Each action model (AM) has its own data that needs to be allocated.\n"
            "This function returns the allocated data for a predefined AM.\n"
            ":return AM data.")
-      .add_property("model", bp::make_function(&ActionModelNumDiff::get_model, bp::return_internal_reference<>()),
+      .add_property("model",
+                    bp::make_function(&DifferentialActionModelNumDiff::get_model, bp::return_internal_reference<>()),
                     "action model");
 }
 
 }  // namespace python
 }  // namespace crocoddyl
 
-#endif  // BINDINGS_PYTHON_CROCODDYL_CORE_NUMDIFF_ACTION_HPP_
+#endif  // BINDINGS_PYTHON_CROCODDYL_CORE_NUMDIFF_DIFF_ACTION_HPP_
---
+++ src/core/numdiff/diff-action.cpp
@@ -0,0 +1,113 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2019, LAAS-CNRS, New York University, Max Planck Gesellshaft
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#include "crocoddyl/core/numdiff/diff-action.hpp"
+
+namespace crocoddyl {
+
+DifferentialActionModelNumDiff::DifferentialActionModelNumDiff(DifferentialActionModelAbstract& model,
+                                                               bool with_gauss_approx)
+    : DifferentialActionModelAbstract(model.get_state(), model.get_nu(), model.get_nr()), model_(model) {
+  with_gauss_approx_ = with_gauss_approx;
+  disturbance_ = std::sqrt(2.0 * std::numeric_limits<double>::epsilon());
+  assert((!with_gauss_approx_ || nr_ > 1) && "No Gauss approximation possible with nr = 1");
+
+  dx_.resize(state_.get_ndx());
+  dx_.setZero();
+  du_.resize(model.get_nu());
+  du_.setZero();
+  tmp_x_.resize(state_.get_nx());
+  tmp_x_.setZero();
+}
+
+DifferentialActionModelNumDiff::~DifferentialActionModelNumDiff() {}
+
+void DifferentialActionModelNumDiff::calc(const boost::shared_ptr<DifferentialActionDataAbstract>& data,
+                                          const Eigen::Ref<const Eigen::VectorXd>& x,
+                                          const Eigen::Ref<const Eigen::VectorXd>& u) {
+  assert(x.size() == state_.get_nx() && "x has wrong dimension");
+  assert(u.size() == nu_ && "u has wrong dimension");
+  model_.calc(data, x, u);
+}
+
+void DifferentialActionModelNumDiff::calcDiff(const boost::shared_ptr<DifferentialActionDataAbstract>& data,
+                                              const Eigen::Ref<const Eigen::VectorXd>& x,
+                                              const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc) {
+  assert(x.size() == state_.get_nx() && "x has wrong dimension");
+  assert(u.size() == nu_ && "u has wrong dimension");
+  boost::shared_ptr<DifferentialActionDataNumDiff> data_num_diff =
+      boost::static_pointer_cast<DifferentialActionDataNumDiff>(data);
+
+  if (recalc) {
+    model_.calc(data_num_diff->data_0, x, u);
+  }
+  Eigen::VectorXd& xn0 = data_num_diff->data_0->xout;
+  double& c0 = data_num_diff->data_0->cost;
+
+  assertStableStateFD(x);
+
+  // Computing the d action(x,u) / dx
+  dx_.setZero();
+  for (unsigned ix = 0; ix < state_.get_ndx(); ++ix) {
+    dx_(ix) = disturbance_;
+    model_.get_state().integrate(x, dx_, tmp_x_);
+    calc(data_num_diff->data_x[ix], tmp_x_, u);
+    // data->Fx
+    Eigen::VectorXd& xn = data_num_diff->data_x[ix]->xout;
+    double& c = data_num_diff->data_x[ix]->cost;
+    model_.get_state().diff(xn0, xn, data_num_diff->Fx.col(ix));
+    // data->Lx
+    data_num_diff->Lx(ix) = (c - c0) / disturbance_;
+
+    // data->Rx
+    if (model_.get_nr() > 1) {
+      // TODO(mnaveau): manage the gaussian approximation
+      // data_num_diff->Rx.col(ix) = data_num_diff->data_x[ix]->cost_residual -
+      //                             data_num_diff->data_0[ix]->cost_residual;
+    }
+    dx_(ix) = 0.0;
+  }
+  data_num_diff->Fx /= disturbance_;
+
+  // Computing the d action(x,u) / du
+  du_.setZero();
+  for (unsigned iu = 0; iu < model_.get_nu(); ++iu) {
+    du_(iu) = disturbance_;
+    calc(data_num_diff->data_u[iu], x, u + du_);
+    // data->Fu
+    Eigen::VectorXd& xn = data_num_diff->data_u[iu]->xout;
+    double& c = data_num_diff->data_u[iu]->cost;
+    model_.get_state().diff(xn0, xn, data_num_diff->Fu.col(iu));
+    // data->Lu
+    data_num_diff->Lu(iu) = (c - c0) / disturbance_;
+    // data->Ru
+    if (model_.get_nr() > 1) {
+      // TODO(mnaveau): manage the gaussian approximation
+      // data_num_diff->Ru.col(iu) = data_num_diff->data_u[iu]->cost_residual -
+      //                             data_num_diff->data_0[iu]->cost_residual;
+    }
+    du_(iu) = 0.0;
+  }
+  data_num_diff->Fu /= disturbance_;
+
+  if (with_gauss_approx_) {
+    data_num_diff->Lxx = data_num_diff->Rx.transpose() * data_num_diff->Rx;
+    data_num_diff->Lxu = data_num_diff->Rx.transpose() * data_num_diff->Ru;
+    data_num_diff->Luu = data_num_diff->Ru.transpose() * data_num_diff->Ru;
+  }
+}
+
+void DifferentialActionModelNumDiff::assertStableStateFD(const Eigen::Ref<const Eigen::VectorXd>& /** x */) {
+  // TODO(cmastalli): First we need to do it AMNumDiff and then to replicate it.
+}
+
+boost::shared_ptr<DifferentialActionDataAbstract> DifferentialActionModelNumDiff::createData() {
+  return boost::make_shared<DifferentialActionDataNumDiff>(this);
+}
+
+}  // namespace crocoddyl
