38670f1ffdf356a1a9decd2e8988aaf5f452eeda
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-29 12:52:26 +0200

    [example] Adapted the bipedal example

--- examples/bipedal_walking_from_foot_traj.py
+++ examples/bipedal_walking_from_foot_traj.py
@@ -2,12 +2,8 @@ import sys
 
 import numpy as np
 import pinocchio
-from crocoddyl import (ActivationModelWeightedQuad, ActuationModelFreeFloating, CallbackDDPLogger, CallbackDDPVerbose,
-                       CallbackSolverDisplay, ContactModel6D, ContactModelMultiple, CostModelCoM, CostModelControl,
-                       CostModelFramePlacement, CostModelFrameVelocity, CostModelState, CostModelSum,
-                       DifferentialActionModelFloatingInContact, IntegratedActionModelEuler, ShootingProblem,
-                       SolverDDP, StatePinocchio, a2m, displayTrajectory, loadTalosLegs, m2a)
-from pinocchio.utils import zero
+import crocoddyl
+import example_robot_data
 
 WITHDISPLAY = 'disp' in sys.argv
 WITHPLOT = 'plot' in sys.argv
@@ -79,29 +75,23 @@ def plotSolution(rmodel, xs, us, figIndex=1, show=True):
         plt.show()
 
 
-class TaskSE3:
-    def __init__(self, oXf, frameId):
-        self.oXf = oXf
-        self.frameId = frameId
-
-
 class SimpleBipedGaitProblem:
     """ Defines a simple 3d locomotion problem
     """
     def __init__(self, rmodel, rightFoot, leftFoot):
         self.rmodel = rmodel
         self.rdata = rmodel.createData()
-        self.state = StatePinocchio(self.rmodel)
+        self.state = crocoddyl.StateMultibody(self.rmodel)
+        self.actuation = crocoddyl.ActuationModelFloatingBase(self.state)
         # Getting the frame id for all the legs
         self.rfId = self.rmodel.getFrameId(rightFoot)
         self.lfId = self.rmodel.getFrameId(leftFoot)
         # Defining default state
         q0 = self.rmodel.referenceConfigurations["half_sitting"]
-        self.rmodel.defaultState = \
-            np.concatenate([m2a(q0), np.zeros(self.rmodel.nv)])
+        self.rmodel.defaultState = np.concatenate([q0, np.zeros((self.rmodel.nv, 1))])
         self.firstStep = True
         # Remove the armature
-        self.rmodel.armature[6:] = 1.
+        # self.rmodel.armature[6:] = 1.
 
     def createWalkingProblem(self, x0, stepLength, stepHeight, timeStep, stepKnots, supportKnots):
         """ Create a shooting problem for a simple walking gait.
@@ -115,12 +105,12 @@ class SimpleBipedGaitProblem:
         :return shooting problem
         """
         # Compute the current foot positions
-        q0 = a2m(x0[:self.rmodel.nq])
+        q0 = x0[:self.state.nq]
         pinocchio.forwardKinematics(self.rmodel, self.rdata, q0)
         pinocchio.updateFramePlacements(self.rmodel, self.rdata)
         rfPos0 = self.rdata.oMf[self.rfId].translation
         lfPos0 = self.rdata.oMf[self.lfId].translation
-        comRef = m2a(rfPos0 + lfPos0) / 2
+        comRef = (rfPos0 + lfPos0) / 2
         comRef[2] = 0.6185
 
         # Defining the action models along the time instances
@@ -142,7 +132,7 @@ class SimpleBipedGaitProblem:
         loco3dModel += doubleSupport + rStep
         loco3dModel += doubleSupport + lStep
 
-        problem = ShootingProblem(x0, loco3dModel, loco3dModel[-1])
+        problem = crocoddyl.ShootingProblem(x0, loco3dModel, loco3dModel[-1])
         return problem
 
     def createFootstepModels(self, comPos0, feetPos0, stepLength, stepHeight, timeStep, numKnots, supportFootIds,
@@ -174,16 +164,16 @@ class SimpleBipedGaitProblem:
                 # swing-leg motion.
                 phKnots = numKnots / 2
                 if k < phKnots:
-                    dp = a2m([[stepLength * (k + 1) / numKnots, 0., stepHeight * k / phKnots]])
+                    dp = np.matrix([[stepLength * (k + 1) / numKnots, 0., stepHeight * k / phKnots]]).T
                 elif k == phKnots:
-                    dp = a2m([[stepLength * (k + 1) / numKnots, 0., stepHeight]])
+                    dp = np.matrix([[stepLength * (k + 1) / numKnots, 0., stepHeight]]).T
                 else:
-                    dp = a2m([[stepLength * (k + 1) / numKnots, 0., stepHeight * (1 - float(k - phKnots) / phKnots)]])
+                    dp = np.matrix([[stepLength * (k + 1) / numKnots, 0., stepHeight * (1 - float(k - phKnots) / phKnots)]]).T
                 tref = np.asmatrix(p + dp)
 
-                swingFootTask += [TaskSE3(pinocchio.SE3(np.eye(3), tref), i)]
+                swingFootTask += [crocoddyl.FramePlacement(i, pinocchio.SE3(np.eye(3), tref))]
 
-            comTask = np.array([stepLength * (k + 1) / numKnots, 0., 0.]) * comPercentage + comPos0
+            comTask = np.matrix([stepLength * (k + 1) / numKnots, 0., 0.]).T * comPercentage + comPos0
             footSwingModel += [
                 self.createSwingFootModel(timeStep, supportFootIds, comTask=comTask, swingFootTask=swingFootTask)
             ]
@@ -193,9 +183,9 @@ class SimpleBipedGaitProblem:
             self.createFootSwitchModel(supportFootIds, swingFootTask)
 
         # Updating the current foot position for next step
-        comPos0 += np.array([stepLength * comPercentage, 0., 0.])
+        comPos0 += np.matrix([stepLength * comPercentage, 0., 0.]).T
         for p in feetPos0:
-            p += a2m([[stepLength, 0., 0.]])
+            p += np.matrix([[stepLength, 0., 0.]]).T
         return footSwingModel + [footSwitchModel]
 
     def createSwingFootModel(self, timeStep, supportFootIds, comTask=None, swingFootTask=None):
@@ -207,79 +197,97 @@ class SimpleBipedGaitProblem:
         :param swingFootTask: swinging foot task
         :return action model for a swing foot phase
         """
-        # Creating the action model for floating-base systems. A walker system
-        # is by default a floating-base system
-        actModel = ActuationModelFreeFloating(self.rmodel)
-
         # Creating a 6D multi-contact model, and then including the supporting
         # foot
-        contactModel = ContactModelMultiple(self.rmodel)
+        contactModel = crocoddyl.ContactModelMultiple(self.state, self.actuation.nu)
         for i in supportFootIds:
+            Mref = crocoddyl.FramePlacement(i, pinocchio.SE3.Identity())
             supportContactModel = \
-                ContactModel6D(self.rmodel, i, ref=pinocchio.SE3.Identity(),
-                               gains=[0., 0.])
+                crocoddyl.ContactModel6D(self.state, Mref,self.actuation.nu, np.matrix([0., 0.]).T)
             contactModel.addContact('contact_' + str(i), supportContactModel)
 
         # Creating the cost model for a contact phase
-        costModel = CostModelSum(self.rmodel, actModel.nu)
+        costModel = crocoddyl.CostModelSum(self.state, self.actuation.nu)
         if isinstance(comTask, np.ndarray):
-            comTrack = CostModelCoM(self.rmodel, comTask, actModel.nu)
+            comTrack = crocoddyl.CostModelCoMPosition(self.state, comTask, self.actuation.nu)
             costModel.addCost("comTrack", comTrack, 1e4)
         if swingFootTask is not None:
             for i in swingFootTask:
                 footTrack = \
-                    CostModelFramePlacement(self.rmodel,
-                                            i.frameId,
-                                            i.oXf,
-                                            actModel.nu)
+                    crocoddyl.CostModelFramePlacement(self.state,
+                                            i,
+                                            self.actuation.nu)
                 costModel.addCost("footTrack_" + str(i), footTrack, 1e4)
 
-        stateWeights = np.array([0] * 3 + [500.] * 3 + [0.01] * (self.rmodel.nv - 6) + [10] * self.rmodel.nv)
-        stateReg = CostModelState(self.rmodel,
-                                  self.state,
-                                  self.rmodel.defaultState,
-                                  actModel.nu,
-                                  activation=ActivationModelWeightedQuad(stateWeights**2))
-        ctrlReg = CostModelControl(self.rmodel, actModel.nu)
+        stateWeights = np.array([0] * 3 + [500.] * 3 + [0.01] * (self.state.nv - 6) + [10] * self.state.nv)
+        stateReg = crocoddyl.CostModelState(self.state,
+                                            crocoddyl.ActivationModelWeightedQuad(np.matrix(stateWeights**2).T),
+                                            self.rmodel.defaultState,
+                                            self.actuation.nu)
+        ctrlReg = crocoddyl.CostModelControl(self.state, self.actuation.nu)
         costModel.addCost("stateReg", stateReg, 1e-1)
         costModel.addCost("ctrlReg", ctrlReg, 1e-3)
 
         # Creating the action model for the KKT dynamics with simpletic Euler
         # integration scheme
-        dmodel = DifferentialActionModelFloatingInContact(self.rmodel, actModel, contactModel, costModel)
-        model = IntegratedActionModelEuler(dmodel)
-        model.timeStep = timeStep
+        dmodel = crocoddyl.DifferentialActionModelContactFwdDynamics(self.state, self.actuation, contactModel, costModel)
+        model = crocoddyl.IntegratedActionModelEuler(dmodel, timeStep)
         return model
 
-    def createFootSwitchModel(self, supportFootId, swingFootTask):
+    def createFootSwitchModel(self, supportFootIds, swingFootTask):
         """ Action model for a foot switch phase.
 
         :param supportFootIds: Ids of the constrained feet
         :param swingFootTask: swinging foot task
         :return action model for a foot switch phase
         """
-        model = self.createSwingFootModel(0., supportFootId, swingFootTask=swingFootTask)
+        # Creating a 6D multi-contact model, and then including the supporting
+        # foot
+        contactModel = crocoddyl.ContactModelMultiple(self.state, self.actuation.nu)
+        for i in supportFootIds:
+            Mref = crocoddyl.FramePlacement(i, pinocchio.SE3.Identity())
+            supportContactModel = \
+                crocoddyl.ContactModel6D(self.state, Mref,self.actuation.nu, np.matrix([0., 0.]).T)
+            contactModel.addContact('contact_' + str(i), supportContactModel)
 
-        for i in swingFootTask:
-            impactFootVelCost = \
-                CostModelFrameVelocity(self.rmodel, i.frameId)
-            model.differential.costs.addCost('impactVel_' + str(i), impactFootVelCost, 1e4)
-            model.differential.costs['impactVel_' + str(i)].weight = 1e6
-            model.differential.costs['footTrack_' + str(i)].weight = 1e8
-        model.differential.costs['stateReg'].weight = 1e1
-        model.differential.costs['ctrlReg'].weight = 1e-3
+        # Creating the cost model for a contact phase
+        costModel = crocoddyl.CostModelSum(self.state, self.actuation.nu)
+        if swingFootTask is not None:
+            for i in swingFootTask:
+                footTrack = \
+                    crocoddyl.CostModelFramePlacement(self.state,
+                                            i,
+                                            self.actuation.nu)
+                costModel.addCost("footTrack_" + str(i), footTrack, 1e8)
+                footVel = crocoddyl.FrameMotion(i.frame, pinocchio.Motion.Zero())
+                impactFootVelCost = crocoddyl.CostModelFrameVelocity(self.state, footVel, self.actuation.nu)
+                costModel.addCost('impactVel_' + str(i.frame), impactFootVelCost, 1e6)
+
+        stateWeights = np.array([0] * 3 + [500.] * 3 + [0.01] * (self.state.nv - 6) + [10] * self.state.nv)
+        stateReg = crocoddyl.CostModelState(self.state,
+                                            crocoddyl.ActivationModelWeightedQuad(np.matrix(stateWeights**2).T),
+                                            self.rmodel.defaultState,
+                                            self.actuation.nu)
+        ctrlReg = crocoddyl.CostModelControl(self.state, self.actuation.nu)
+        costModel.addCost("stateReg", stateReg, 1e1)
+        costModel.addCost("ctrlReg", ctrlReg, 1e-3)
+
+        # Creating the action model for the KKT dynamics with simpletic Euler
+        # integration scheme
+        dmodel = crocoddyl.DifferentialActionModelContactFwdDynamics(self.state, self.actuation, contactModel, costModel)
+        model = crocoddyl.IntegratedActionModelEuler(dmodel, 0.)
         return model
 
 
 # Creating the lower-body part of Talos
-talos_legs = loadTalosLegs()
+talos_legs = example_robot_data.loadTalosLegs()
 rmodel = talos_legs.model
-rdata = rmodel.createData()
+
 
 # Defining the initial state of the robot
 q0 = rmodel.referenceConfigurations['half_sitting'].copy()
-v0 = zero(rmodel.nv)
-x0 = m2a(np.concatenate([q0, v0]))
+v0 = pinocchio.utils.zero(rmodel.nv)
+x0 = np.concatenate([q0, v0])
 
 # Setting up the 3d walking problem
 rightFoot = 'right_sole_link'
@@ -303,25 +311,23 @@ for i, phase in enumerate(GAITPHASES):
     for key, value in phase.items():
         if key == 'walking':
             # Creating a walking problem
-            ddp[i] = SolverDDP(
+            ddp[i] = crocoddyl.SolverDDP(
                 gait.createWalkingProblem(x0, value['stepLength'], value['stepHeight'], value['timeStep'],
                                           value['stepKnots'], value['supportKnots']))
 
     # Added the callback functions
     print('*** SOLVE ' + key + ' ***')
-    ddp[i].callback = [CallbackDDPLogger(), CallbackDDPVerbose()]
     if WITHDISPLAY:
-        ddp[i].callback.append(CallbackSolverDisplay(talos_legs, 4, 1, cameraTF))
+        ddp[i].setCallbacks([crocoddyl.CallbackVerbose(), crocoddyl.CallbackSolverDisplay(talos_legs, 4, 4, cameraTF)])
+    else:
+        ddp[i].setCallbacks([crocoddyl.CallbackVerbose()])
+
 
     # Solving the problem with the DDP solver
     ddp[i].th_stop = 1e-9
-    ddp[i].solve(maxiter=1000,
-                 regInit=.1,
-                 init_xs=[rmodel.defaultState] * len(ddp[i].models()),
-                 init_us=[
-                     m.differential.quasiStatic(d.differential, rmodel.defaultState)
-                     for m, d in zip(ddp[i].models(), ddp[i].datas())[:-1]
-                 ])
+    xs = [rmodel.defaultState] * len(ddp[i].models())
+    us = [m.quasicStatic(d, rmodel.defaultState) for m, d in list(zip(ddp[i].models(), ddp[i].datas()))[:-1]]
+    ddp[i].solve(xs, us, 1000, False, 0.1)
 
     # Defining the final state as initial one for the next phase
     x0 = ddp[i].xs[-1]
@@ -329,7 +335,7 @@ for i, phase in enumerate(GAITPHASES):
 # Display the entire motion
 if WITHDISPLAY:
     for i, phase in enumerate(GAITPHASES):
-        displayTrajectory(talos_legs, ddp[i].xs, ddp[i].models()[0].timeStep)
+        crocoddyl.displayTrajectory(talos_legs, ddp[i].xs, ddp[i].models()[0].dt)
 
 # Plotting the entire motion
 if WITHPLOT:
