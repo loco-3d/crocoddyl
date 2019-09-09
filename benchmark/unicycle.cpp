b32bc8ab48ff6a9c09ddcc02a92e09930b845fed
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-09-09 10:26:36 +0200

    [cleanup] Added copyright in benchmark cpp code

--- benchmark/unicycle.cpp
+++ benchmark/unicycle.cpp
@@ -1,3 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2019, LAAS-CNRS
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
 #include "crocoddyl/core/actions/unicycle.hpp"
 #include "crocoddyl/core/utils/callbacks.hpp"
 #include "crocoddyl/core/solvers/ddp.hpp"
