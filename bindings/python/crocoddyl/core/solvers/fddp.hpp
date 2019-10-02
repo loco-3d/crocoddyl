5c54382e495965b02fd436a18af5c060de4945a2
Wolfgang Merkt <w.merkt@gmail.com>
2019-10-02 19:32:32 +0100

    FDDP bindings: re-add overload to fix unit test

--- bindings/python/crocoddyl/core/solvers/fddp.hpp
+++ bindings/python/crocoddyl/core/solvers/fddp.hpp
@@ -16,6 +16,9 @@ namespace python {
 
 namespace bp = boost::python;
 
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SolverFDDP_solves, SolverFDDP::solve, 0, 5)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SolverFDDP_computeDirections, SolverDDP::computeDirection, 0, 1)
+
 void exposeSolverFDDP() {
   bp::class_<SolverFDDP, bp::bases<SolverDDP> >(
       "SolverFDDP",
@@ -29,6 +32,30 @@ void exposeSolverFDDP() {
       bp::init<ShootingProblem&>(bp::args(" self", " problem"),
                                  "Initialize the vector dimension.\n\n"
                                  ":param problem: shooting problem.")[bp::with_custodian_and_ward<1, 2>()])
+      .def("solve", &SolverFDDP::solve,
+           SolverFDDP_solves(
+               bp::args(" self", " init_xs=[]", " init_us=[]", " maxiter=100", " isFeasible=False", " regInit=None"),
+               "Compute the optimal trajectory xopt, uopt as lists of T+1 and T terms.\n\n"
+               "From an initial guess init_xs,init_us (feasible or not), iterate\n"
+               "over computeDirection and tryStep until stoppingCriteria is below\n"
+               "threshold. It also describes the globalization strategy used\n"
+               "during the numerical optimization.\n"
+               ":param init_xs: initial guess for state trajectory with T+1 elements.\n"
+               ":param init_us: initial guess for control trajectory with T elements.\n"
+               ":param maxiter: maximun allowed number of iterations.\n"
+               ":param isFeasible: true if the init_xs are obtained from integrating the init_us (rollout).\n"
+               ":param regInit: initial guess for the regularization value. Very low values are typical\n"
+               "                used with very good guess points (init_xs, init_us).\n"
+               ":returns the optimal trajectory xopt, uopt and a boolean that describes if convergence was reached."))
+      .def("computeDirection", &SolverFDDP::computeDirection,
+           SolverFDDP_computeDirections(
+               bp::args(" self", " recalc=True"),
+               "Compute the search direction (dx, du) for the current guess (xs, us).\n\n"
+               "You must call setCandidate first in order to define the current\n"
+               "guess. A current guess defines a state and control trajectory\n"
+               "(xs, us) of T+1 and T elements, respectively.\n"
+               ":params recalc: true for recalculating the derivatives at current state and control.\n"
+               ":returns the search direction dx, du and the dual lambdas as lists of T+1, T and T+1 lengths."))
       .def("expectedImprovement", &SolverFDDP::expectedImprovement,
            bp::return_value_policy<bp::copy_const_reference>(), bp::args(" self"),
            "Return two scalars denoting the quadratic improvement model\n\n"
