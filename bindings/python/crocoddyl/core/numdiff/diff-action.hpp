d37326bf637c8b24cf27e654ab27e64c2ab4e456
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-10-11 19:27:21 +0200

    [python] Exposed data for diff-action numdiff

--- bindings/python/crocoddyl/core/numdiff/diff-action.hpp
+++ bindings/python/crocoddyl/core/numdiff/diff-action.hpp
@@ -68,6 +68,41 @@ void exposeDifferentialActionNumDiff() {
                     bp::make_function(&DifferentialActionModelNumDiff::get_with_gauss_approx,
                                       bp::return_value_policy<bp::return_by_value>()),
                     "Gauss approximation for computing the Hessians");
+
+  bp::register_ptr_to_python<boost::shared_ptr<DifferentialActionDataNumDiff> >();
+
+  bp::class_<DifferentialActionDataNumDiff, bp::bases<DifferentialActionDataAbstract> >(
+      "DifferentialActionDataNumDiff", "Numerical differentiation diff-action data.",
+      bp::init<DifferentialActionModelNumDiff*>(bp::args(" self", " model"),
+                                                "Create numerical differentiation diff-action data.\n\n"
+                                                ":param model: numdiff diff-action model"))
+      .add_property(
+          "Rx", bp::make_getter(&DifferentialActionDataNumDiff::Rx, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&DifferentialActionDataNumDiff::Rx), "Jacobian of the cost residual.")
+      .add_property(
+          "Ru", bp::make_getter(&DifferentialActionDataNumDiff::Ru, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&DifferentialActionDataNumDiff::Ru), "Jacobian of the cost residual.")
+      .add_property(
+          "dx", bp::make_getter(&DifferentialActionDataNumDiff::dx, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&DifferentialActionDataNumDiff::dx), "state disturbance.")
+      .add_property(
+          "du", bp::make_getter(&DifferentialActionDataNumDiff::du, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&DifferentialActionDataNumDiff::du), "control disturbance.")
+      .add_property(
+          "xp", bp::make_getter(&DifferentialActionDataNumDiff::du, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&DifferentialActionDataNumDiff::du), "rate state after disturbance.")
+      .add_property(
+          "data_0",
+          bp::make_getter(&DifferentialActionDataNumDiff::data_0, bp::return_value_policy<bp::return_by_value>()),
+          "data that contains the final results")
+      .add_property(
+          "data_x",
+          bp::make_getter(&DifferentialActionDataNumDiff::data_x, bp::return_value_policy<bp::return_by_value>()),
+          "temporary data associated with the state variation")
+      .add_property(
+          "data_u",
+          bp::make_getter(&DifferentialActionDataNumDiff::data_u, bp::return_value_policy<bp::return_by_value>()),
+          "temporary data associated with the control variation");
 }
 
 }  // namespace python
