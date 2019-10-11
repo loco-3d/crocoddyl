25bb88a60ffe199b735553bfbb0e7829b936df25
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-10-11 18:34:59 +0200

    [python] Exposed the Euler integrator data

--- bindings/python/crocoddyl/core/integrator/euler.hpp
+++ bindings/python/crocoddyl/core/integrator/euler.hpp
@@ -74,7 +74,33 @@ void exposeIntegratedActionEuler() {
       "IntegratedActionDataEuler", "Sympletic Euler integrator data.",
       bp::init<IntegratedActionModelEuler*>(bp::args(" self", " model"),
                                             "Create sympletic Euler integrator data.\n\n"
-                                            ":param model: sympletic Euler integrator model"));
+                                            ":param model: sympletic Euler integrator model"))
+      .add_property(
+          "differential",
+          bp::make_getter(&IntegratedActionDataEuler::differential, bp::return_value_policy<bp::return_by_value>()),
+          "differential action data")
+      .add_property(
+          "dx", bp::make_getter(&IntegratedActionDataEuler::ddx_dx, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&IntegratedActionDataEuler::ddx_dx), "state rate.")
+      .add_property(
+          "ddx_dx",
+          bp::make_getter(&IntegratedActionDataEuler::ddx_dx, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&IntegratedActionDataEuler::ddx_dx), "Jacobian of the state rate with respect to the state.")
+      .add_property(
+          "ddx_du",
+          bp::make_getter(&IntegratedActionDataEuler::ddx_du, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&IntegratedActionDataEuler::ddx_du),
+          "Jacobian of the state rate with respect to the control.")
+      .add_property(
+          "dxnext_dx",
+          bp::make_getter(&IntegratedActionDataEuler::dxnext_dx, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&IntegratedActionDataEuler::dxnext_dx),
+          "Jacobian of the next state with respect to the state.")
+      .add_property(
+          "dxnext_ddx",
+          bp::make_getter(&IntegratedActionDataEuler::dxnext_ddx, bp::return_value_policy<bp::return_by_value>()),
+          bp::make_setter(&IntegratedActionDataEuler::dxnext_ddx),
+          "Jacobian of the next state with respect to the state rate.");
 }
 
 }  // namespace python
