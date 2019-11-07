7d904d0c7497700d6bcc611270211e296bbee9f8
Rohan Budhiraja <proyan@users.noreply.github.com>
2019-11-07 09:26:50 +0100

    [bindings][contact force] expose costdata.contactdata element

--- bindings/python/crocoddyl/multibody/costs/contact-force.hpp
+++ bindings/python/crocoddyl/multibody/costs/contact-force.hpp
@@ -97,7 +97,9 @@ void exposeCostContactForce() {
           ":param data: Pinocchio data")[bp::with_custodian_and_ward<1, 2, bp::with_custodian_and_ward<1, 3> >()])
       .add_property("Arr_Ru",
                     bp::make_getter(&CostDataContactForce::Arr_Ru, bp::return_value_policy<bp::return_by_value>()),
-                    "Intermediate product of Arr (2nd deriv of Activation) with Ru (deriv of residue)");
+                    "Intermediate product of Arr (2nd deriv of Activation) with Ru (deriv of residue)")
+    .add_property("contact", bp::make_getter(&CostDataContactForce::contact_, bp::return_value_policy<bp::return_by_value>()),
+                  bp::make_setter(&CostDataContactForce::contact_), "contact data associated with the current cost");
 }
 
 }  // namespace python
