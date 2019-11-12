f030bd869594d8d5741b5d9d7439ebaccc3bbfb0
Wolfgang Merkt <w.merkt@gmail.com>
2019-11-12 19:43:19 +0000

    [CI] Travis: formatting check

---
+++ .travis/build-and-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+# Exit if in format testing mode
+if [ $CHECK_CLANG_FORMAT ]; then exit 0; fi
+
+mkdir _build ; cd _build ; cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+make -j2
+export PYTHONPATH=$(pwd)/bindings/python:$PYTHONPATH
+make test
