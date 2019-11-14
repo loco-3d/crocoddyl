9156c7deaee912f6dc4b88b42139abdc458dcf96
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-11-14 16:32:07 +0000

    [cleanup] Removed empty space, formatted code

--- unittest/python/crocoddyl/cost.py
+++ unittest/python/crocoddyl/cost.py
@@ -317,7 +317,6 @@ class CostModelFrameVelocityLinear(CostModelPinocchio):
     end-effector. It assumes that updateFramePlacement and computeForwardKinematicsDerivatives
     have been runned.
     """
-
     def __init__(self, pinocchioModel, frame, ref=None, nu=None, activation=None):
         self.CostDataType = CostDataFrameVelocityLinear
         CostModelPinocchio.__init__(self, pinocchioModel, ncost=3)
