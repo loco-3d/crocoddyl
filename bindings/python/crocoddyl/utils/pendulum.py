c3d562e328569875762e51c8d4f91252cc02ea90
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-11-14 15:22:37 +0000

    [examples] Added display and plot options in the pendulum example

--- bindings/python/crocoddyl/utils/pendulum.py
+++ bindings/python/crocoddyl/utils/pendulum.py
@@ -1,6 +1,6 @@
-import numpy as np
-import pinocchio
 import crocoddyl
+import pinocchio
+import numpy as np
 
 
 class CostModelDoublePendulum(crocoddyl.CostModelAbstract):
--- examples/double_pendulum.py
+++ examples/double_pendulum.py
@@ -1,54 +1,71 @@
+import os
+import sys
+
 import crocoddyl
 import pinocchio
 import numpy as np
 import example_robot_data
 from crocoddyl.utils.pendulum import CostModelDoublePendulum, ActuationModelDoublePendulum
 
+WITHDISPLAY = 'display' in sys.argv or 'CROCODDYL_DISPLAY' in os.environ
+WITHPLOT = 'plot' in sys.argv or 'CROCODDYL_PLOT' in os.environ
+
 crocoddyl.switchToNumpyMatrix()
 
+# Loading the double pendulum model
 robot = example_robot_data.loadDoublePendulum()
 robot_model = robot.model
-
-robot.initViewer(loadModel=True)
-
-q0 = np.array([3.14, 0])
-robot.q0.flat = q0
-robot.framesForwardKinematics(robot.q0)
-robot.display(robot.q0)
-
-robot.viewer.gui.refresh()
 
 state = crocoddyl.StateMultibody(robot_model)
 actModel = ActuationModelDoublePendulum(state, actLink=1)
 
+weights = np.array([1, 1, 1, 1] + [0.1] * 2)
 runningCostModel = crocoddyl.CostModelSum(state, actModel.nu)
 terminalCostModel = crocoddyl.CostModelSum(state, actModel.nu)
-
-weights = np.array([1, 1, 1, 1] + [0.1] * 2)
-
 xRegCost = crocoddyl.CostModelState(state, crocoddyl.ActivationModelQuad(state.ndx), state.zero(), actModel.nu)
 uRegCost = crocoddyl.CostModelControl(state, crocoddyl.ActivationModelQuad(2), actModel.nu)
 xPendCost = CostModelDoublePendulum(state, crocoddyl.ActivationModelWeightedQuad(np.matrix(weights).T), actModel.nu)
 
 dt = 1e-2
 
-runningCostModel.addCost("regu", uRegCost, 1e-6 / dt)
-runningCostModel.addCost("pend", xPendCost, 1e-3 / dt)
-terminalCostModel.addCost("ori2", xPendCost, 1e4)
+runningCostModel.addCost("uReg", uRegCost, 1e-6 / dt)
+runningCostModel.addCost("xGoal", xPendCost, 1e-3 / dt)
+terminalCostModel.addCost("xGoal", xPendCost, 1e4)
 
 runningModel = crocoddyl.IntegratedActionModelEuler(
     crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actModel, runningCostModel), dt)
 terminalModel = crocoddyl.IntegratedActionModelEuler(
     crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actModel, terminalCostModel), dt)
 
+# Creating the shooting problem and the FDDP solver
 T = 100
 x0 = [3.14, 0, 0., 0.]
 problem = crocoddyl.ShootingProblem(np.matrix(x0).T, [runningModel] * T, terminalModel)
-
 fddp = crocoddyl.SolverFDDP(problem)
 
-fddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose()])
+cameraTF = [1.4, 0., 0.2, 0.5, 0.5, 0.5, 0.5]
+if WITHDISPLAY and WITHPLOT:
+    fddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose(), crocoddyl.CallbackDisplay(robot, 4, 4, cameraTF, False)])
+elif WITHDISPLAY:
+    fddp.setCallbacks([crocoddyl.CallbackVerbose(), crocoddyl.CallbackDisplay(robot, 4, 4, cameraTF, False)])
+else:
+    fddp.setCallbacks([crocoddyl.CallbackVerbose()])
 
+# Solving the problem with the FDDP solver
 fddp.solve()
 
-crocoddyl.displayTrajectory(robot, fddp.xs, dt)
+# Plotting the entire motion
+if WITHPLOT:
+    log = fddp.getCallbacks()[0]
+    crocoddyl.plotOCSolution(log.xs, log.us, figIndex=1, show=False)
+    crocoddyl.plotConvergence(log.costs,
+                              log.control_regs,
+                              log.state_regs,
+                              log.gm_stops,
+                              log.th_stops,
+                              log.steps,
+                              figIndex=2)
+
+# Display the entire motion
+if WITHDISPLAY:
+    crocoddyl.displayTrajectory(robot, fddp.xs, dt, False)
