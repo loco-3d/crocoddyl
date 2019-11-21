fa198b3197310da16be1e54378c08d1f15ffea3d
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-11-21 11:00:30 +0000

    [frame] Created functions to print the internal values of frame objects in c++ and Python

--- bindings/python/crocoddyl/utils.hpp
+++ bindings/python/crocoddyl/utils.hpp
@@ -171,4 +171,21 @@ struct dict_to_map {
 }  // namespace python
 }  // namespace crocoddyl
 
+namespace crocoddyl {
+namespace python {
+namespace bp = boost::python;
+
+///
+/// \brief Set the Python method __str__ and __repr__ to use the overloading operator<<.
+///
+template <class C>
+struct PrintableVisitor : public bp::def_visitor<PrintableVisitor<C> > {
+  template <class PyClass>
+  void visit(PyClass& cl) const {
+    cl.def(bp::self_ns::str(bp::self_ns::self)).def(bp::self_ns::repr(bp::self_ns::self));
+  }
+};
+}  // namespace python
+}  // namespace crocoddyl
+
 #endif  // BINDINGS_PYTHON_CROCODDYL_UTILS_HPP_
--- include/crocoddyl/multibody/frames.hpp
+++ include/crocoddyl/multibody/frames.hpp
@@ -21,30 +21,55 @@ typedef std::size_t FrameIndex;
 
 struct FrameTranslation {
   FrameTranslation(const FrameIndex& frame, const Eigen::Vector3d& oxf) : frame(frame), oxf(oxf) {}
+  friend std::ostream& operator<<(std::ostream& os, const FrameTranslation& X) {
+    os << "      frame: " << X.frame << std::endl << "translation: " << std::endl << X.oxf.transpose() << std::endl;
+    return os;
+  }
+
   FrameIndex frame;
   Eigen::Vector3d oxf;
 };
 
 struct FrameRotation {
   FrameRotation(const FrameIndex& frame, const Eigen::Matrix3d& oRf) : frame(frame), oRf(oRf) {}
+  friend std::ostream& operator<<(std::ostream& os, const FrameRotation& X) {
+    os << "   frame: " << X.frame << std::endl << "rotation: " << std::endl << X.oRf << std::endl;
+    return os;
+  }
+
   FrameIndex frame;
   Eigen::Matrix3d oRf;
 };
 
 struct FramePlacement {
   FramePlacement(const FrameIndex& frame, const pinocchio::SE3& oMf) : frame(frame), oMf(oMf) {}
+  friend std::ostream& operator<<(std::ostream& os, const FramePlacement& X) {
+    os << "    frame: " << X.frame << std::endl << "placement: " << std::endl << X.oMf << std::endl;
+    return os;
+  }
+
   FrameIndex frame;
   pinocchio::SE3 oMf;
 };
 
 struct FrameMotion {
   FrameMotion(const FrameIndex& frame, const pinocchio::Motion& oMf) : frame(frame), oMf(oMf) {}
+  friend std::ostream& operator<<(std::ostream& os, const FrameMotion& X) {
+    os << " frame: " << X.frame << std::endl << "motion: " << std::endl << X.oMf << std::endl;
+    return os;
+  }
+
   FrameIndex frame;
   pinocchio::Motion oMf;
 };
 
 struct FrameForce {
-  FrameForce(const FrameIndex& frame, const pinocchio::Force& oFf) :  frame(frame), oFf(oFf) {}
+  FrameForce(const FrameIndex& frame, const pinocchio::Force& oFf) : frame(frame), oFf(oFf) {}
+  friend std::ostream& operator<<(std::ostream& os, const FrameForce& X) {
+    os << "frame: " << X.frame << std::endl << "force: " << std::endl << X.oFf << std::endl;
+    return os;
+  }
+
   FrameIndex frame;
   pinocchio::Force oFf;
 };
