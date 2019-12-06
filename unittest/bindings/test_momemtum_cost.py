de6bfc53479993a160515fa26449f201f9e16b81
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-12-06 14:01:22 +0000

    [cleanup] Cleaned up the unittest code

--- unittest/bindings/test_momemtum_cost.py
+++ unittest/bindings/test_momemtum_cost.py
@@ -13,12 +13,7 @@ from crocoddyl.utils import a2m, m2a
 from pinocchio.utils import rand
 from test_utils import NUMDIFF_MODIFIER, assertNumDiff
 
-
-def absmax(A):
-    return np.max(abs(A))
-
-
-# Loading Talos arm with FF TODO use a bided or quadruped
+# Loading Talos arm with FF TODO use a biped or quadruped
 # -----------------------------------------------------------------------------
 robot = loadANYmal()
 robot.model.armature[6:] = 1.
