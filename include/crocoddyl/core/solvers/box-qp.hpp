9e05331b826a2dd9592249cb46a305eb8d3c4e4c
Carlos Mastalli <carlos.mastalli@gmail.com>
2019-12-12 16:01:23 +0000

    [solver] Rewrote the box-qp which has problem in the active set computation
    This QP solver is also faster than previous one. I reduced a half the
    computation time

--- include/crocoddyl/core/solvers/box-qp.hpp
+++ include/crocoddyl/core/solvers/box-qp.hpp
@@ -21,9 +21,9 @@
 namespace crocoddyl {
 
 struct BoxQPSolution {
-  BoxQPSolution(const Eigen::MatrixXd& Hff_inv_in, const Eigen::VectorXd& x_in, const std::vector<size_t>& free_idx_in,
-                const std::vector<size_t>& clamped_idx_in)
-      : Hff_inv(Hff_inv_in), x(x_in), free_idx(free_idx_in), clamped_idx(clamped_idx_in) {}
+  BoxQPSolution(const Eigen::MatrixXd& Hff_inv, const Eigen::VectorXd& x, const std::vector<size_t>& free_idx,
+                const std::vector<size_t>& clamped_idx)
+      : Hff_inv(Hff_inv), x(x), free_idx(free_idx), clamped_idx(clamped_idx) {}
 
   Eigen::MatrixXd Hff_inv;
   Eigen::MatrixXd x;
@@ -34,116 +34,144 @@ struct BoxQPSolution {
 // Based on Yuval Tassa's BoxQP
 // Cf. https://www.mathworks.com/matlabcentral/fileexchange/52069-ilqg-ddp-trajectory-optimization
 inline BoxQPSolution BoxQP(const Eigen::MatrixXd& H, const Eigen::VectorXd& q, const Eigen::VectorXd& lb,
-                           const Eigen::VectorXd& ub, const Eigen::VectorXd& xinit, const int maxiter = 100,
+                           const Eigen::VectorXd& ub, const Eigen::VectorXd& xinit, const std::size_t maxiter = 100,
                            const double th_acceptstep = 0.1, const double th_grad = 1e-9, const double reg = 1e-9) {
-  if (maxiter < 0) {
-    throw_pretty("Max iterations needs to be positive.");
+  const std::size_t nx = xinit.size();
+  if (static_cast<std::size_t>(H.rows()) != nx || static_cast<std::size_t>(H.cols()) != nx) {
+    throw_pretty("Invalid argument: "
+                 << "H has wrong dimension (it should be " + std::to_string(nx) + "," + std::to_string(nx) + ")");
+  }
+  if (static_cast<std::size_t>(q.size()) != nx) {
+    throw_pretty("Invalid argument: "
+                 << "q has wrong dimension (it should be " + std::to_string(nx) + ")");
+  }
+  if (static_cast<std::size_t>(lb.size()) != nx) {
+    throw_pretty("Invalid argument: "
+                 << "lb has wrong dimension (it should be " + std::to_string(nx) + ")");
+  }
+  if (static_cast<std::size_t>(ub.size()) != nx) {
+    throw_pretty("Invalid argument: "
+                 << "ub has wrong dimension (it should be " + std::to_string(nx) + ")");
   }
 
-  int it = 0;
-  Eigen::VectorXd delta_xf(xinit.size()), x = xinit;
+  std::vector<double> alphas;
+  const std::size_t& n_alphas = 10;
+  alphas.resize(n_alphas);
+  for (std::size_t n = 0; n < n_alphas; ++n) {
+    alphas[n] = 1. / pow(2., static_cast<double>(n));
+  }
+
+  // We need to enforce feasible warm-starting of the algorithm
+  Eigen::VectorXd x(nx), xnew(nx), g(nx);
+  for (std::size_t i = 0; i < nx; ++i) {
+    x(i) = std::max(std::min(xinit(i), ub(i)), lb(i));
+  }
+
+  // Start the numerical iterations
+  Eigen::VectorXd qf, xf, xc, dx, dxf;
+  Eigen::MatrixXd Hff, Hff_inv, Hfc;
+  Eigen::LLT<Eigen::MatrixXd> Hff_inv_llt;
   std::vector<size_t> clamped_idx, free_idx;
-  Eigen::VectorXd grad = q + H * xinit;
-  Eigen::MatrixXd Hff, Hfc, Hff_inv(H.rows(), H.cols());
-  Eigen::LLT<Eigen::MatrixXd> Hff_inv_llt = Eigen::LLT<Eigen::MatrixXd>(H.rows());
-
-  Hff_inv_llt.compute(Eigen::MatrixXd::Identity(H.rows(), H.cols()) * reg + H);
-  Hff_inv_llt.solveInPlace(Hff_inv);
-
-  if (grad.lpNorm<Eigen::Infinity>() <= th_grad) {
-    return BoxQPSolution(Hff_inv, xinit, free_idx, clamped_idx);
-  }
-
-  while (grad.lpNorm<Eigen::Infinity>() > th_grad && it < maxiter) {
-    ++it;
-    grad.noalias() = q + H * x;
+  for (std::size_t k = 0; k < maxiter; ++k) {
     clamped_idx.clear();
     free_idx.clear();
-
-    for (int i = 0; i < grad.size(); ++i) {
-      if ((x(i) == lb(i) && grad(i) > 0) || (x(i) == ub(i) && grad(i) < 0)) {
-        clamped_idx.push_back(i);
+    // Compute the gradient
+    g = q;
+    g.noalias() += H * x;
+    for (std::size_t j = 0; j < nx; ++j) {
+      const double& gj = g(j);
+      const double& xj = x(j);
+      const double& lbj = lb(j);
+      const double& ubj = ub(j);
+      if ((xj == lbj && gj > 0.) || (xj == ubj && gj < 0.)) {
+        clamped_idx.push_back(j);
       } else {
-        free_idx.push_back(i);
+        free_idx.push_back(j);
       }
     }
 
-    if (free_idx.size() == 0) {
+    // Check convergence
+    const std::size_t& nc = clamped_idx.size();
+    const std::size_t& nf = free_idx.size();
+    if (g.lpNorm<Eigen::Infinity>() <= th_grad || nf == 0) {
+      if (k == 0) { // compute the inverse of the free Hessian
+        Hff.resize(nf, nf);
+        for (std::size_t i = 0; i < nf; ++i) {
+          for (std::size_t j = 0; j < nf; ++j) {
+            Hff(i, j) = H(free_idx[i], free_idx[j]);
+          }
+        }
+        Hff_inv_llt = Eigen::LLT<Eigen::MatrixXd>(nf);
+        Hff_inv_llt.compute(Eigen::MatrixXd::Identity(nf, nf) * reg + Hff);
+        Eigen::ComputationInfo info = Hff_inv_llt.info();
+        if (info != Eigen::Success) {
+          throw_pretty("backward_error");
+        }
+        Hff_inv.setIdentity(nf, nf);
+        Hff_inv_llt.solveInPlace(Hff_inv);
+      }
       return BoxQPSolution(Hff_inv, x, free_idx, clamped_idx);
     }
 
-    Hff.resize(free_idx.size(), free_idx.size());
-    Hfc.resize(free_idx.size(), clamped_idx.size());
-
-    if (clamped_idx.size() == 0) {
-      Hff = H;
-    } else {
-      for (size_t i = 0; i < free_idx.size(); ++i) {
-        for (size_t j = 0; j < free_idx.size(); ++j) {
-          Hff(i, j) = H(free_idx[i], free_idx[j]);
-        }
+    // Compute the search direaction as Newton step along the free space
+    qf.resize(nf);
+    xf.resize(nf);
+    xc.resize(nc);
+    dx.resize(nx);
+    dxf.resize(nf);
+    Hff.resize(nf, nf);
+    Hfc.resize(nf, nc);
+    for (std::size_t i = 0; i < nf; ++i) {
+      qf(i) = q(free_idx[i]);
+      xf(i) = x(free_idx[i]);
+      for (std::size_t j = 0; j < nf; ++j) {
+        Hff(i, j) = H(free_idx[i], free_idx[j]);
       }
-
-      for (size_t i = 0; i < free_idx.size(); ++i) {
-        for (size_t j = 0; j < clamped_idx.size(); ++j) {
-          Hfc(i, j) = H(free_idx[i], clamped_idx[j]);
-        }
+      for (std::size_t j = 0; j < nc; ++j) {
+        xc(j) = x(clamped_idx[j]);
+        Hfc(i, j) = H(free_idx[i], clamped_idx[j]);
       }
     }
-
-    // NOTE: Array indexing not supported in current eigen version
-    Eigen::VectorXd q_free(free_idx.size()), x_free(free_idx.size()), x_clamped(clamped_idx.size());
-    for (size_t i = 0; i < free_idx.size(); ++i) {
-      q_free(i) = q(free_idx[i]);
-      x_free(i) = x(free_idx[i]);
+    Hff_inv_llt = Eigen::LLT<Eigen::MatrixXd>(nf);
+    Hff_inv_llt.compute(Hff);
+    Eigen::ComputationInfo info = Hff_inv_llt.info();
+    if (info != Eigen::Success) {
+      std::cout << "hey" << std::endl;
+      throw_pretty("backward_error");
+    }
+    Hff_inv.setIdentity(nf, nf);
+    Hff_inv_llt.solveInPlace(Hff_inv);
+    dxf = -qf;
+    if (nc != 0) {
+      dxf.noalias() -= Hfc * xc;
+    }
+    Hff_inv_llt.solveInPlace(dxf);
+    dxf -= xf;
+    dx.setZero();
+    for (std::size_t i = 0; i < nf; ++i) {
+      dx(free_idx[i]) = dxf(i);
     }
 
-    for (size_t j = 0; j < clamped_idx.size(); ++j) {
-      x_clamped(j) = x(clamped_idx[j]);
+    // There is not improvement anymore
+    if (dx.lpNorm<Eigen::Infinity>() < th_grad) {
+      return BoxQPSolution(Hff_inv, x, free_idx, clamped_idx);
     }
 
-    // The dimension of Hff has changed - reinitialise LLT
-    // Hff_inv_llt = Eigen::LLT<Eigen::MatrixXd>(Hff.rows());
-    // Hff_inv_llt.compute(Eigen::MatrixXd::Identity(Hff.rows(), Hff.cols()) * reg + Hff);
-    // Hff_inv_llt.solveInPlace(Hff_inv);
-    // TODO: Use Cholesky, however, often unstable without adapting reg.
-    Hff_inv = (Eigen::MatrixXd::Identity(Hff.rows(), Hff.cols()) * reg + Hff).inverse();
+    // Try different step lengths
+    double fold = 0.5 * x.dot(H * x) + q.dot(x);
+    for (std::vector<double>::const_iterator it = alphas.begin(); it != alphas.end(); ++it) {
+      double steplength = *it;
 
-    if (clamped_idx.size() == 0) {
-      // std::cout << "Hff_inv=" << Hff_inv.rows() << "x" << Hff_inv.cols() << ", q_free=" << q_free.size() << ",
-      // x_free=" << x_free.size() << std::endl;
-      delta_xf.noalias() = -Hff_inv * (q_free)-x_free;
-    } else {
-      delta_xf.noalias() = -Hff_inv * (q_free + Hfc * x_clamped) - x_free;
-    }
-
-    double f_old = (0.5 * x.transpose() * H * x + q.transpose() * x)(0);
-    static const Eigen::VectorXd alpha_space = Eigen::VectorXd::LinSpaced(10, 1.0, 0.1);
-
-    bool armijo_reached = false;
-    Eigen::VectorXd x_new(x.size()), x_diff(x.size());
-    for (int ai = 0; ai < alpha_space.rows(); ++ai) {
-      x_new = x;
-      for (size_t i = 0; i < free_idx.size(); ++i) {
-        x_new(free_idx[i]) = std::max(std::min(x(free_idx[i]) + alpha_space[ai] * delta_xf(i), ub(i)), lb(i));
+      for (std::size_t i = 0; i < nx; ++i) {
+        xnew(i) = std::max(std::min(x(i) + steplength * dx(i), ub(i)), lb(i));
       }
-
-      double f_new = (0.5 * x_new.transpose() * H * x_new + q.transpose() * x_new)(0);
-      x_diff.noalias() = x - x_new;
-
-      // armijo criterion>
-      double armijo_coef = (f_old - f_new) / (grad.transpose() * x_diff + 1e-5);
-      if (armijo_coef > th_acceptstep) {
-        armijo_reached = true;
-        x = x_new;
+      double fnew = 0.5 * xnew.dot(H * xnew) + q.dot(xnew);
+      if (fold - fnew > th_acceptstep * g.dot(x - xnew)) {
+        x = xnew;
         break;
       }
     }
-
-    // break if no step made
-    if (!armijo_reached) break;
   }
-
   return BoxQPSolution(Hff_inv, x, free_idx, clamped_idx);
 }
 
