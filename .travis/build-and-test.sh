8acdc46d80fe9f6bcd1f0d8655a2ce3925d4f3c3
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-01-08 13:12:43 +0000

    [ci] Need to defined the c++11 in the cmake

--- .travis/build-and-test.sh
+++ .travis/build-and-test.sh
@@ -4,6 +4,13 @@ set -xe
 # Exit if in format testing mode
 if [ $CHECK_CLANG_FORMAT ]; then exit 0; fi
 
-mkdir _build ; cd _build ; cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+mkdir _build ; cd _build
+if [ -z "$CMAKE_CXX_STANDARD" ]
+then
+cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD
+else
+cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD
+fi
+
 make -j1
 make test
