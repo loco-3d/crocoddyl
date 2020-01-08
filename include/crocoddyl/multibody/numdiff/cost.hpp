d4cc210639c708507e65ece327b6c0419f564904
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2020-01-08 18:14:27 +0100

    [num diff] add the num diff of the costs

--- include/crocoddyl/multibody/numdiff/cost.hpp
+++ include/crocoddyl/multibody/numdiff/cost.hpp
@@ -12,24 +12,56 @@
 
 #include <boost/function.hpp>
 #include "crocoddyl/multibody/cost-base.hpp"
+#include "pinocchio/algorithm/kinematics.hpp"
+#include "pinocchio/algorithm/jacobian.hpp"
+#include "pinocchio/algorithm/frames.hpp"
+#include "pinocchio/algorithm/center-of-mass.hpp"
+#include "pinocchio/algorithm/centroidal.hpp"
+#include "pinocchio/algorithm/centroidal-derivatives.hpp"
 
 namespace crocoddyl {
 
-struct CostNumDiffData;  // forward declaration
+struct CostDataNumDiff;  // forward declaration
 
 // Simple renaming that ease the code writing.
-typedef boost::function<void(const pinocchio::Model&, pinocchio::Data&, const Eigen::VectorXd&, const Eigen::VectorXd&)> ReevaluationFunction;
+typedef boost::function<void(const Eigen::VectorXd&)> ReevaluationFunction;
 
-class CostNumDiffModel: CostModelAbstract {
+/**
+ * @brief Compute all the pinocchio data needed for the numerical
+ * differentation. We use the address of the object to avoid a copy from the
+ * "boost::bind".
+ * 
+ * @param model is the rigid body robot model.
+ * @param data contains the results of the computations.
+ * @param x is the state vector.
+ */
+void updateAllPinocchio(pinocchio::Model* const model,
+                        pinocchio::Data* data,
+                        const Eigen::VectorXd& x)
+{
+  const Eigen::VectorXd& q = x.segment(0, model->nq);
+  const Eigen::VectorXd& v = x.segment(model->nq, model->nv);
+  Eigen::VectorXd a = Eigen::VectorXd::Zero(model->nv);
+  Eigen::Matrix<double, 6, Eigen::Dynamic> tmp;
+  tmp.resize(6, model->nv);
+  pinocchio::forwardKinematics(*model, *data, q);
+  pinocchio::computeJointJacobians(*model, *data, q);
+  pinocchio::updateFramePlacements(*model, *data);
+  pinocchio::jacobianCenterOfMass(*model, *data, q);
+  pinocchio::computeCentroidalMomentum(*model, *data, q, v);
+  pinocchio::computeCentroidalDynamicsDerivatives(
+      *model, *data, q, v, a, tmp, tmp, tmp, tmp);
+}
 
+class CostNumDiffModel : public CostModelAbstract {
  public:
   /**
    * @brief Construct a new CostNumDiffModel object from a CostModelAbstract.
-   * 
-   * @param model 
+   *
+   * @param model
    */
   CostNumDiffModel(const boost::shared_ptr<CostModelAbstract>& model);
-  
+
   /**
    * @brief Default destructor of the CostNumDiffModel object
    */
@@ -38,20 +70,20 @@ class CostNumDiffModel: CostModelAbstract {
   /**
    * @brief @copydoc ActionModelAbstract::calc()
    */
-  void calc(const boost::shared_ptr<CostNumDiffData>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
-                    const Eigen::Ref<const Eigen::VectorXd>& u);
+  void calc(const boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+            const Eigen::Ref<const Eigen::VectorXd>& u);
 
   /**
    * @brief @copydoc ActionModelAbstract::calcDiff()
    */
-  void calcDiff(const boost::shared_ptr<CostNumDiffData>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
-                        const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc = true);
+  void calcDiff(const boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+                const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc = true);
 
   /**
    * @brief Create a Data object
-   * 
+   *
    * @param data is the DataCollector used by the original model.
-   * @return boost::shared_ptr<CostModelAbstract> 
+   * @return boost::shared_ptr<CostModelAbstract>
    */
   boost::shared_ptr<CostDataAbstract> createData(DataCollectorAbstract* const data);
 
@@ -88,45 +120,67 @@ class CostNumDiffModel: CostModelAbstract {
    * @brief Register functions that take a pinocchio model, a pinocchio
    * data, a state and a control. These function are called during the
    * evaluation of the gradient and hessian.
-   * 
+   *
    * @param reevals are the registered functions.
    */
   void set_reevals(const std::vector<ReevaluationFunction>& reevals);
 
  protected:
-  boost::shared_ptr<CostModelAbstract> model_; /*< Model of the cost. */
-  double disturbance_; /*< Numerical disturbance used in the numerical differentiation. */
-  std::vector<ReevaluationFunction> reevals_; /*< Functions that needs execution before calc or calcDiff. */
+  /** @brief Model of the cost. */
+  boost::shared_ptr<CostModelAbstract> model_;
+
+  /** @brief Numerical disturbance used in the numerical differentiation. */
+  double disturbance_;
+
+  /** @brief Functions that needs execution before calc or calcDiff. */
+  std::vector<ReevaluationFunction> reevals_;
+
+ private:
+  /**
+   * @brief Make sure that when we finite difference the Cost Model, the user
+   * does not face unknown behaviour because of the finite differencing of a
+   * quaternion around pi. This behaviour might occur if CostModelState and
+   * FloatingInContact differential model are used together.
+   *
+   * For full discussions see issue
+   * https://gepgitlab.laas.fr/loco-3d/crocoddyl/issues/139
+   *
+   * @param x is the state at which the check is performed.
+   */
+  virtual void assertStableStateFD(const Eigen::Ref<const Eigen::VectorXd>& /*x*/){};
 };
 
-struct CostNumDiffData : public CostDataAbstract {
+struct CostDataNumDiff : public CostDataAbstract {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   template <typename Model>
-  explicit CostNumDiffData(Model* const model, DataCollectorAbstract* const data)
-      : CostDataAbstract(model, data),
-        dx(model->get_model()->get_state()->get_ndx()),
-        du(model->get_model()->get_nu()),
-        xp(model->get_model()->get_state()->get_nx()) {
+  explicit CostDataNumDiff(Model* const model, DataCollectorAbstract* const shared_data)
+      : CostDataAbstract(model, shared_data),
+        dx(model->get_state()->get_ndx()),
+        xp(model->get_state()->get_nx()),
+        du(model->get_nu()),
+        up(model->get_nu()) {
     dx.setZero();
-    du.setZero();
     xp.setZero();
-
+    du.setZero();
+    up.setZero();
+    
     const std::size_t& ndx = model->get_model()->get_state()->get_ndx();
     const std::size_t& nu = model->get_model()->get_nu();
-    data_0 = model->get_model()->createData();
+    data_0 = model->get_model()->createData(shared_data);
     for (std::size_t i = 0; i < ndx; ++i) {
-      data_x.push_back(model->get_model()->createData());
+      data_x.push_back(model->get_model()->createData(shared_data));
     }
     for (std::size_t i = 0; i < nu; ++i) {
-      data_u.push_back(model->get_model()->createData());
+      data_u.push_back(model->get_model()->createData(shared_data));
     }
   }
 
-  virtual ~CostNumDiffData() {}
+  virtual ~CostDataNumDiff() {}
   Eigen::VectorXd dx;  //!< State disturbance.
-  Eigen::VectorXd du;  //!< Control disturbance.
   Eigen::VectorXd xp;  //!< The integrated state from the disturbance on one DoF "\f$ \int x dx_i \f$".
+  Eigen::VectorXd du;  //!< Control disturbance.
+  Eigen::VectorXd up;  //!< The integrated control from the disturbance on one DoF "\f$ \int u du_i = u + du \f$".
   boost::shared_ptr<CostDataAbstract> data_0;  //!< The data at the approximation point.
   std::vector<boost::shared_ptr<CostDataAbstract> >
       data_x;  //!< The temporary data associated with the state variation.
