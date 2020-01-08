34bdda2e4d79c3295a1cba7dbc46d3d62bc4f9fa
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-01-08 13:26:14 +0000

    [ci] Run examples in Release mode

--- .travis/build-and-test.sh
+++ .travis/build-and-test.sh
@@ -7,10 +7,26 @@ if [ $CHECK_CLANG_FORMAT ]; then exit 0; fi
 mkdir _build ; cd _build
 if [ -z "$CMAKE_CXX_STANDARD" ]
 then
-cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD
+  cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD
 else
-cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD
+  cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD
 fi
 
 make -j1
 make test
+
+# Run the examples in Release mode
+if [ "$CMAKE_BUILD_TYPE" == "Release" ]
+then
+  # Examples
+  make -s examples-double_pendulum
+  make -s examples-quadrotor
+  make -s examples-quadrotor_ubound
+  make -s examples-arm_manipulation
+  make -s examples-bipedal_walk
+  make -s examples-bipedal_walk_ubound
+  make -s examples-quadrupedal_gaits
+  make -s examples-quadrupedal_walk_ubound
+  make -s examples-humanoid_manipulation
+  make -s examples-humanoid_manipulation_ubound
+fi
