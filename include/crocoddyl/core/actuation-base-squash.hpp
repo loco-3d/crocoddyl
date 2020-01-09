6a701373ad2569bd660731be9d236c0d18bb9744
PepMS <jmarti@iri.upc.edu>
2020-01-09 16:12:06 +0100

    formatting

--- include/crocoddyl/core/actuation-base-squash.hpp
+++ include/crocoddyl/core/actuation-base-squash.hpp
@@ -5,22 +5,21 @@
 namespace crocoddyl {
 
 class ActuationModelSquashingAbstract : public ActuationModelAbstract {
-    public:
-        ActuationModelSquashingAbstract(boost::shared_ptr<StateAbstract> state, const std::size_t& nu);
-        virtual ~ActuationModelSquashingAbstract();
+ public:
+  ActuationModelSquashingAbstract(boost::shared_ptr<StateAbstract> state, const std::size_t& nu);
+  virtual ~ActuationModelSquashingAbstract();
 
-        virtual void calc(const boost::shared_ptr<ActuationDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
+  virtual void calc(const boost::shared_ptr<ActuationDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
                     const Eigen::Ref<const Eigen::VectorXd>& u) = 0;
-        virtual void calcDiff(const boost::shared_ptr<ActuationDataAbstract>& data,
+  virtual void calcDiff(const boost::shared_ptr<ActuationDataAbstract>& data,
                         const Eigen::Ref<const Eigen::VectorXd>& x, const Eigen::Ref<const Eigen::VectorXd>& u,
                         const bool& recalc = true) = 0;
 
-        virtual void calcSquash(const Eigen::Ref<const Eigen::VectorXd>& u) = 0;
-        virtual void calcSquashDiff(const Eigen::Ref<const Eigen::VectorXd>& u) = 0;
-    
-    protected:
-        Eigen::VectorXd v_; // Squashing function output
-        Eigen::MatrixXd dv_du_; // Squashing function Jacobian wrt control input u
+  virtual void calcSquash(const Eigen::Ref<const Eigen::VectorXd>& u) = 0;
+  virtual void calcSquashDiff(const Eigen::Ref<const Eigen::VectorXd>& u) = 0;
 
+ protected:
+  Eigen::VectorXd v_;      // Squashing function output
+  Eigen::MatrixXd dv_du_;  // Squashing function Jacobian wrt control input u
 };
-}
+}  // namespace crocoddyl
--- src/core/actuation-base-squash.cpp
+++ src/core/actuation-base-squash.cpp
@@ -2,7 +2,9 @@
 #include "crocoddyl/core/actuation-base-squash.hpp"
 
 namespace crocoddyl {
-    ActuationModelSquashingAbstract::ActuationModelSquashingAbstract(boost::shared_ptr<StateAbstract> state, const std::size_t& nu) : ActuationModelAbstract(state, nu) {}
+ActuationModelSquashingAbstract::ActuationModelSquashingAbstract(boost::shared_ptr<StateAbstract> state,
+                                                                 const std::size_t& nu)
+    : ActuationModelAbstract(state, nu) {}
 
-    ActuationModelSquashingAbstract::~ActuationModelSquashingAbstract() {}
-}
+ActuationModelSquashingAbstract::~ActuationModelSquashingAbstract() {}
+}  // namespace crocoddyl
