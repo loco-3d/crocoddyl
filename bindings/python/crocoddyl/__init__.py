40bb1e9909a7bbfc1fec859bfea98128754e9715
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-01-23 12:39:16 +0000

    [diagnostic] Set ALWAYS_ON visibility of forces, trajectories and cones

--- bindings/python/crocoddyl/__init__.py
+++ bindings/python/crocoddyl/__init__.py
@@ -140,6 +140,7 @@ class GepettoDisplay:
             forceName = self.forceGroup + "/" + key
             self.robot.viewer.gui.addArrow(forceName, self.forceRadius, self.forceLength, self.forceColor)
             self.robot.viewer.gui.setFloatProperty(forceName, "Alpha", 1.)
+        self.robot.viewer.gui.setVisibility(self.forceGroup, "ALWAYS_ON_TOP")
 
     def addFrictionCones(self):
         for key in self.activeContacts:
@@ -150,6 +151,7 @@ class GepettoDisplay:
             frameName = self.frameTrajGroup + "/" + key
             self.robot.viewer.gui.addCurve(frameName, [np.array([0., 0., 0.]).tolist()] * 2, self.frameTrajColor[key])
             self.robot.viewer.gui.setCurveLineWidth(frameName, self.frameTrajLineWidth)
+            self.robot.viewer.gui.setVisibility(frameName, "ALWAYS_ON_TOP")
 
     def getForceTrajectoryFromSolver(self, solver):
         fs = []
@@ -231,6 +233,8 @@ class GepettoDisplay:
                 l = self.robot.viewer.gui.addLine(lineGroup + "/" + str(k), [0., 0., 0.],
                                                   m_generatrices[:3, k].T.tolist()[0], self.frictionConeColor2)
         self.robot.viewer.gui.setScale(coneGroup, [scale, scale, scale])
+        self.robot.viewer.gui.setVisibility(meshGroup, "ALWAYS_ON_TOP")
+        self.robot.viewer.gui.setVisibility(lineGroup, "ALWAYS_ON_TOP")
 
     def setConeMu(self, coneName, mu):
         current_mu = self.frictionMu[coneName]
