b665fb92055a216c0860873434d3d772e80d1578
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-02-11 11:30:08 +0100

    [templatizing] introduce mathbase to store scalar based matrix classes

---
+++ include/crocoddyl/core/mathbase.hpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2020 LAAS-CNRS
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef CROCODDYL_CORE_MATHBASE_HPP_
+#define CROCODDYL_CORE_MATHBASE_HPP_
+
+#include <Eigen/Core>
+
+namespace crocoddyl {
+
+template <typename _Scalar>
+struct MathBaseTpl {
+public:
+
+  typedef _Scalar Scalar;
+  typedef Eigen::Matrix<Scalar, 2, 1> Vector2s;
+  typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> VectorXs;
+  typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> MatrixXs;
+
+};
+
+}
+
+#endif
