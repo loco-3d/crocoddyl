ad65cdc6f474a08c220bbb9103bcce5211910176
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-02-18 11:48:09 +0100

    [scalar template] diff-action-base

--- include/crocoddyl/core/diff-action-base.hxx
+++ include/crocoddyl/core/diff-action-base.hxx
@@ -7,49 +7,60 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "crocoddyl/core/utils/exception.hpp"
-#include "crocoddyl/core/diff-action-base.hpp"
 
 namespace crocoddyl {
 
-DifferentialActionModelAbstract::DifferentialActionModelAbstract(boost::shared_ptr<StateAbstract> state,
+template <typename Scalar>
+DifferentialActionModelAbstractTpl<Scalar>::DifferentialActionModelAbstractTpl(boost::shared_ptr<StateAbstract> state,
                                                                  const std::size_t& nu, const std::size_t& nr)
     : nu_(nu),
       nr_(nr),
       state_(state),
-      unone_(Eigen::VectorXd::Zero(nu)),
-      u_lb_(Eigen::VectorXd::Constant(nu, -std::numeric_limits<double>::infinity())),
-      u_ub_(Eigen::VectorXd::Constant(nu, std::numeric_limits<double>::infinity())),
+      unone_(VectorXs::Zero(nu)),
+      u_lb_(VectorXs::Constant(nu, -std::numeric_limits<double>::infinity())),
+      u_ub_(VectorXs::Constant(nu, std::numeric_limits<double>::infinity())),
       has_control_limits_(false) {}
 
-DifferentialActionModelAbstract::~DifferentialActionModelAbstract() {}
+template <typename Scalar>
+DifferentialActionModelAbstractTpl<Scalar>::~DifferentialActionModelAbstractTpl() {}
 
-void DifferentialActionModelAbstract::calc(const boost::shared_ptr<DifferentialActionDataAbstract>& data,
-                                           const Eigen::Ref<const Eigen::VectorXd>& x) {
+template <typename Scalar>
+void DifferentialActionModelAbstractTpl<Scalar>::calc(const boost::shared_ptr<DifferentialActionDataAbstract>& data,
+                                           const Eigen::Ref<const VectorXs>& x) {
   calc(data, x, unone_);
 }
 
-void DifferentialActionModelAbstract::calcDiff(const boost::shared_ptr<DifferentialActionDataAbstract>& data,
-                                               const Eigen::Ref<const Eigen::VectorXd>& x) {
+template <typename Scalar>
+void DifferentialActionModelAbstractTpl<Scalar>::calcDiff(const boost::shared_ptr<DifferentialActionDataAbstract>& data,
+                                               const Eigen::Ref<const VectorXs>& x) {
   calcDiff(data, x, unone_);
 }
 
-boost::shared_ptr<DifferentialActionDataAbstract> DifferentialActionModelAbstract::createData() {
+template <typename Scalar>
+boost::shared_ptr<DifferentialActionDataAbstractTpl<Scalar> > DifferentialActionModelAbstractTpl<Scalar>::createData() {
   return boost::make_shared<DifferentialActionDataAbstract>(this);
 }
 
-const std::size_t& DifferentialActionModelAbstract::get_nu() const { return nu_; }
+template <typename Scalar>
+const std::size_t& DifferentialActionModelAbstractTpl<Scalar>::get_nu() const { return nu_; }
 
-const std::size_t& DifferentialActionModelAbstract::get_nr() const { return nr_; }
+template <typename Scalar>
+const std::size_t& DifferentialActionModelAbstractTpl<Scalar>::get_nr() const { return nr_; }
 
-const boost::shared_ptr<StateAbstract>& DifferentialActionModelAbstract::get_state() const { return state_; }
+template <typename Scalar>
+const boost::shared_ptr<StateAbstractTpl<Scalar> >& DifferentialActionModelAbstractTpl<Scalar>::get_state() const { return state_; }
 
-const Eigen::VectorXd& DifferentialActionModelAbstract::get_u_lb() const { return u_lb_; }
+template <typename Scalar>
+const typename MathBaseTpl<Scalar>::VectorXs& DifferentialActionModelAbstractTpl<Scalar>::get_u_lb() const { return u_lb_; }
 
-const Eigen::VectorXd& DifferentialActionModelAbstract::get_u_ub() const { return u_ub_; }
+template <typename Scalar>
+const typename MathBaseTpl<Scalar>::VectorXs& DifferentialActionModelAbstractTpl<Scalar>::get_u_ub() const { return u_ub_; }
 
-bool const& DifferentialActionModelAbstract::get_has_control_limits() const { return has_control_limits_; }
+template <typename Scalar>
+bool const& DifferentialActionModelAbstractTpl<Scalar>::get_has_control_limits() const { return has_control_limits_; }
 
-void DifferentialActionModelAbstract::set_u_lb(const Eigen::VectorXd& u_lb) {
+template <typename Scalar>
+void DifferentialActionModelAbstractTpl<Scalar>::set_u_lb(const VectorXs& u_lb) {
   if (static_cast<std::size_t>(u_lb.size()) != nu_) {
     throw_pretty("Invalid argument: "
                  << "lower bound has wrong dimension (it should be " + std::to_string(nu_) + ")");
@@ -58,7 +69,8 @@ void DifferentialActionModelAbstract::set_u_lb(const Eigen::VectorXd& u_lb) {
   update_has_control_limits();
 }
 
-void DifferentialActionModelAbstract::set_u_ub(const Eigen::VectorXd& u_ub) {
+template <typename Scalar>
+void DifferentialActionModelAbstractTpl<Scalar>::set_u_ub(const VectorXs& u_ub) {
   if (static_cast<std::size_t>(u_ub.size()) != nu_) {
     throw_pretty("Invalid argument: "
                  << "upper bound has wrong dimension (it should be " + std::to_string(nu_) + ")");
@@ -67,7 +79,8 @@ void DifferentialActionModelAbstract::set_u_ub(const Eigen::VectorXd& u_ub) {
   update_has_control_limits();
 }
 
-void DifferentialActionModelAbstract::update_has_control_limits() {
+template <typename Scalar>
+void DifferentialActionModelAbstractTpl<Scalar>::update_has_control_limits() {
   has_control_limits_ = isfinite(u_lb_.array()).any() && isfinite(u_ub_.array()).any();
 }
 
