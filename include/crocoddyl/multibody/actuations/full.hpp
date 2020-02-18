4e056aaecca3bf865689474e80c6f8c7cbe4516c
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-02-18 11:27:34 +0100

    [scalar template] actuation

--- include/crocoddyl/multibody/actuations/full.hpp
+++ include/crocoddyl/multibody/actuations/full.hpp
@@ -14,17 +14,59 @@
 
 namespace crocoddyl {
 
-class ActuationModelFull : public ActuationModelAbstract {
- public:
-  explicit ActuationModelFull(boost::shared_ptr<StateMultibody> state);
-  ~ActuationModelFull();
-
-  void calc(const boost::shared_ptr<ActuationDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
-            const Eigen::Ref<const Eigen::VectorXd>& u);
-  void calcDiff(const boost::shared_ptr<ActuationDataAbstract>& data, const Eigen::Ref<const Eigen::VectorXd>& x,
-                const Eigen::Ref<const Eigen::VectorXd>& u);
-  boost::shared_ptr<ActuationDataAbstract> createData();
+template<typename _Scalar>
+class ActuationModelFullTpl : public ActuationModelAbstractTpl<_Scalar> {
+public:
+  typedef _Scalar Scalar;
+  typedef MathBaseTpl<Scalar> MathBase;
+  typedef ActuationModelAbstractTpl<Scalar> Base;
+  typedef StateMultibodyTpl<Scalar> StateMultibody;
+  typedef ActuationDataAbstractTpl<Scalar> ActuationDataAbstract;
+  typedef typename MathBase::VectorXs VectorXs;
+  typedef typename MathBase::MatrixXs MatrixXs;
+  explicit ActuationModelFullTpl(boost::shared_ptr<StateMultibody> state)
+    : Base(state, state->get_nv()) {
+    pinocchio::JointModelFreeFlyerTpl<Scalar> ff_joint;
+    if (state->get_pinocchio().joints[1].shortname() == ff_joint.shortname()) {
+      throw_pretty("Invalid argument: "
+                   << "the first joint cannot be free-flyer");
+  }
 };
+  
+  ~ActuationModelFullTpl() {};
+
+  void calc(const boost::shared_ptr<ActuationDataAbstract>& data,
+            const Eigen::Ref<const VectorXs>& /*x*/,
+            const Eigen::Ref<const VectorXs>& u) {
+    if (static_cast<std::size_t>(u.size()) != nu_) {
+      throw_pretty("Invalid argument: "
+                   << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
+    }
+    data->tau = u;
+  };
+  
+  void calcDiff(const boost::shared_ptr<ActuationDataAbstract>& data,
+                const Eigen::Ref<const VectorXs>& /*x*/,
+                const Eigen::Ref<const VectorXs>& /*u*/) {
+    // The derivatives has constant values which were set in createData.
+    assert_pretty(data->dtau_dx == MatrixXs::Zero(state_->get_nv(), state_->get_ndx()),
+                  "dtau_dx has wrong value");
+    assert_pretty(data->dtau_du == MatrixXs::Identity(state_->get_nv(), nu_), "dtau_du has wrong value");
+};
+  
+  boost::shared_ptr<ActuationDataAbstract> createData() {
+    boost::shared_ptr<ActuationDataAbstract> data = boost::make_shared<ActuationDataAbstract>(this);
+    data->dtau_du.diagonal().fill(1);
+    return data;
+  };
+
+protected:
+  using Base::nu_;
+  using Base::state_;
+
+};
+
+typedef ActuationModelFullTpl<double> ActuationModelFull;
 
 }  // namespace crocoddyl
 
