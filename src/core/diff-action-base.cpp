8d4038ff402d35ef9bf805b1f2590e80cb52cf84
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-02-18 12:34:49 +0100

    [scalar template] diff-action-base

---
+++ src/core/diff-action-base.cpp
@@ -0,0 +1,9 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2020, LAAS-CNRS, The University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#include "crocoddyl/core/diff-action-base.hpp"
