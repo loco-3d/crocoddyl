9b3dfc415251fb26bb721744af8751de6eb66890
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-02-21 15:49:20 +0100

    update copyright notics

--- src/core/numdiff/diff-action.cpp
+++ src/core/numdiff/diff-action.cpp
@@ -1,7 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2018-2020, LAAS-CNRS, University of Edinburgh
+// Copyright (C) 2018-2020, LAAS-CNRS, University of Edinburgh, New York University,
+// Max Planck Gesellschaft 
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
