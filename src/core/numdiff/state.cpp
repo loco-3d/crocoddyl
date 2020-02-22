654db8c104d8956668f14243298ca484e72b7d61
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-02-22 10:47:34 +0100

    clang format

--- src/core/numdiff/state.cpp
+++ src/core/numdiff/state.cpp
@@ -2,7 +2,7 @@
 // BSD 3-Clause License
 //
 // Copyright (C) 2018-2020, LAAS-CNRS, University of Edinburgh, New York University,
-// Max Planck Gesellschaft 
+// Max Planck Gesellschaft
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
