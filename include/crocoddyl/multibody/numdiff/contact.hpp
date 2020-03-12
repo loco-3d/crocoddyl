b6705d56743494c64847498d6bb9f51e78ac5228
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-03-12 13:57:48 +0000

    [contact] Added numdiff of contacts

---
+++ include/crocoddyl/multibody/numdiff/contact.hpp
@@ -0,0 +1,160 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2020, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef CROCODDYL_MULTIBODY_NUMDIFF_CONTACT_HPP_
+#define CROCODDYL_MULTIBODY_NUMDIFF_CONTACT_HPP_
+
+#include <boost/function.hpp>
+#include "crocoddyl/multibody/fwd.hpp"
+#include "crocoddyl/multibody/contact-base.hpp"
+
+namespace crocoddyl {
+
+template <typename _Scalar>
+class ContactModelNumDiffTpl : public ContactModelAbstractTpl<_Scalar> {
+ public:
+  typedef _Scalar Scalar;
+  typedef ContactDataAbstractTpl<Scalar> ContactDataAbstract;
+  typedef ContactModelAbstractTpl<Scalar> Base;
+  typedef MathBaseTpl<Scalar> MathBase;
+  typedef typename MathBaseTpl<Scalar>::VectorXs VectorXs;
+  typedef boost::function<void(const typename MathBaseTpl<Scalar>::VectorXs&)> ReevaluationFunction;
+
+  /**
+   * @brief Construct a new ContactModelNumDiff object from a ContactModelAbstract.
+   *
+   * @param model
+   */
+  explicit ContactModelNumDiffTpl(const boost::shared_ptr<Base>& model);
+
+  /**
+   * @brief Default destructor of the ContactModelNumDiff object
+   */
+  virtual ~ContactModelNumDiffTpl();
+
+  /**
+   * @brief @copydoc ActionModelAbstract::calc()
+   */
+  void calc(const boost::shared_ptr<ContactDataAbstract>& data, const Eigen::Ref<const VectorXs>& x);
+
+  /**
+   * @brief @copydoc ActionModelAbstract::calcDiff()
+   */
+  void calcDiff(const boost::shared_ptr<ContactDataAbstract>& data, const Eigen::Ref<const VectorXs>& x);
+
+  /**
+   * @brief Create a Data object
+   *
+   * @param data is the DataCollector used by the original model.
+   * @return boost::shared_ptr<ContactModelAbstract>
+   */
+  boost::shared_ptr<ContactDataAbstract> createData(pinocchio::DataTpl<Scalar>* const data);
+
+  /**
+   * @brief Get the model_ object
+   *
+   * @return ContactModelAbstract&
+   */
+  const boost::shared_ptr<Base>& get_model() const;
+
+  /**
+   * @brief Get the disturbance_ object
+   *
+   * @return const Scalar&
+   */
+  const Scalar& get_disturbance() const;
+
+  /**
+   * @brief Set the disturbance_ object
+   *
+   * @param disturbance is the value used to find the numerical derivative
+   */
+  void set_disturbance(const Scalar& disturbance);
+
+  /**
+   * @brief Register functions that take a pinocchio model, a pinocchio
+   * data, a state and a control. These function are called during the
+   * evaluation of the gradient and hessian.
+   *
+   * @param reevals are the registered functions.
+   */
+  void set_reevals(const std::vector<ReevaluationFunction>& reevals);
+
+ protected:
+  using Base::nc_;
+  using Base::nu_;
+  using Base::state_;
+
+  /** @brief Model of the Contact. */
+  boost::shared_ptr<Base> model_;
+
+  /** @brief Numerical disturbance used in the numerical differentiation. */
+  Scalar disturbance_;
+
+  /** @brief Functions that needs execution before calc or calcDiff. */
+  std::vector<ReevaluationFunction> reevals_;
+
+ private:
+  /**
+   * @brief Make sure that when we finite difference the Cost Model, the user
+   * does not face unknown behaviour because of the finite differencing of a
+   * quaternion around pi. This behaviour might occur if CostModelState and
+   * FloatingInContact differential model are used together.
+   *
+   * For full discussions see issue
+   * https://gepgitlab.laas.fr/loco-3d/crocoddyl/issues/139
+   *
+   * @param x is the state at which the check is performed.
+   */
+  void assertStableStateFD(const Eigen::Ref<const VectorXs>& /*x*/);
+};
+
+template <typename _Scalar>
+struct ContactDataNumDiffTpl : public ContactDataAbstractTpl<_Scalar> {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  typedef _Scalar Scalar;
+  typedef MathBaseTpl<Scalar> MathBase;
+  typedef ContactDataAbstractTpl<Scalar> Base;
+  typedef typename MathBaseTpl<Scalar>::VectorXs VectorXs;
+
+  template <template <typename Scalar> class Model>
+  explicit ContactDataNumDiffTpl(Model<Scalar>* const model, pinocchio::DataTpl<Scalar>* const data)
+      : Base(model, data),
+        dx(model->get_state()->get_ndx()),
+        xp(model->get_state()->get_nx()) {
+    dx.setZero();
+    xp.setZero();
+
+    const std::size_t& ndx = model->get_model()->get_state()->get_ndx();
+    data_0 = model->get_model()->createData(data);
+    for (std::size_t i = 0; i < ndx; ++i) {
+      data_x.push_back(model->get_model()->createData(data));
+    }
+  }
+
+  virtual ~ContactDataNumDiffTpl() {}
+
+  using Base::pinocchio;
+  using Base::a0;
+  using Base::da0_dx;
+
+  VectorXs dx;  //!< State disturbance.
+  VectorXs xp;  //!< The integrated state from the disturbance on one DoF "\f$ \int x dx_i \f$".
+  boost::shared_ptr<Base> data_0;                //!< The data at the approximation point.
+  std::vector<boost::shared_ptr<Base> > data_x;  //!< The temporary data associated with the state variation.
+};
+
+}  // namespace crocoddyl
+
+/* --- Details -------------------------------------------------------------- */
+/* --- Details -------------------------------------------------------------- */
+/* --- Details -------------------------------------------------------------- */
+#include "crocoddyl/multibody/numdiff/contact.hxx"
+
+#endif  // CROCODDYL_MULTIBODY_NUMDIFF_CONTACT_HPP_
---
+++ include/crocoddyl/multibody/numdiff/contact.hxx
@@ -0,0 +1,89 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2020, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#include "crocoddyl/core/utils/exception.hpp"
+#include "crocoddyl/multibody/numdiff/contact.hpp"
+
+namespace crocoddyl {
+
+template <typename Scalar>
+ContactModelNumDiffTpl<Scalar>::ContactModelNumDiffTpl(const boost::shared_ptr<Base>& model)
+    : Base(model->get_state(), model->get_nc(), model->get_nu()), model_(model) {
+  disturbance_ = std::sqrt(2.0 * std::numeric_limits<Scalar>::epsilon());
+}
+
+template <typename Scalar>
+ContactModelNumDiffTpl<Scalar>::~ContactModelNumDiffTpl() {}
+
+template <typename Scalar>
+void ContactModelNumDiffTpl<Scalar>::calc(const boost::shared_ptr<ContactDataAbstract>& data,
+                                          const Eigen::Ref<const VectorXs>& x) {
+  boost::shared_ptr<ContactDataNumDiffTpl<Scalar> > data_nd =
+      boost::static_pointer_cast<ContactDataNumDiffTpl<Scalar> >(data);
+  model_->calc(data_nd->data_0, x);
+  data_nd->a0 = data_nd->data_0->a0;
+}
+
+template <typename Scalar>
+void ContactModelNumDiffTpl<Scalar>::calcDiff(const boost::shared_ptr<ContactDataAbstract>& data,
+                                              const Eigen::Ref<const VectorXs>& x) {
+  boost::shared_ptr<ContactDataNumDiffTpl<Scalar> > data_nd =
+      boost::static_pointer_cast<ContactDataNumDiffTpl<Scalar> >(data);
+
+  const VectorXs& a0 = data_nd->a0;
+
+  assertStableStateFD(x);
+
+  // Computing the d contact(x,u) / dx
+  data_nd->dx.setZero();
+  for (std::size_t ix = 0; ix < state_->get_ndx(); ++ix) {
+    // x + dx
+    data_nd->dx(ix) = disturbance_;
+    model_->get_state()->integrate(x, data_nd->dx, data_nd->xp);
+    // call the update function on the pinocchio data
+    for (size_t i = 0; i < reevals_.size(); ++i) {
+      reevals_[i](data_nd->xp);
+    }
+    // contact(x+dx, u)
+    model_->calc(data_nd->data_x[ix], data_nd->xp);
+    data_nd->da0_dx(ix) = (data_nd->data_x[ix]->a0 - a0) / disturbance_;
+  }
+}
+
+template <typename Scalar>
+boost::shared_ptr<ContactDataAbstractTpl<Scalar> > ContactModelNumDiffTpl<Scalar>::createData(
+    pinocchio::DataTpl<Scalar>* const data) {
+  return boost::make_shared<ContactDataNumDiffTpl<Scalar> >(this, data);
+}
+
+template <typename Scalar>
+const boost::shared_ptr<ContactModelAbstractTpl<Scalar> >& ContactModelNumDiffTpl<Scalar>::get_model() const {
+  return model_;
+}
+
+template <typename Scalar>
+const Scalar& ContactModelNumDiffTpl<Scalar>::get_disturbance() const {
+  return disturbance_;
+}
+
+template <typename Scalar>
+void ContactModelNumDiffTpl<Scalar>::set_disturbance(const Scalar& disturbance) {
+  disturbance_ = disturbance;
+}
+
+template <typename Scalar>
+void ContactModelNumDiffTpl<Scalar>::set_reevals(const std::vector<ReevaluationFunction>& reevals) {
+  reevals_ = reevals;
+}
+
+template <typename Scalar>
+void ContactModelNumDiffTpl<Scalar>::assertStableStateFD(const Eigen::Ref<const VectorXs>& /*x*/) {
+  // do nothing in the general case
+}
+
+}  // namespace crocoddyl
