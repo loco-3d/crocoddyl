216c89e9349774e446ed4556c637b26963bb8cdb
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-03-20 15:05:42 +0000

    [quadruped] Removed the dependency of solver.models() in quadrupedal plotter

--- examples/quadrupedal_walk_ubound.py
+++ examples/quadrupedal_walk_ubound.py
@@ -57,8 +57,8 @@ elif WITHPLOT:
 else:
     boxddp.setCallbacks([crocoddyl.CallbackVerbose()])
 
-xs = [robot_model.defaultState] * len(boxddp.models())
-us = [m.quasiStatic(d, robot_model.defaultState) for m, d in list(zip(boxddp.models(), boxddp.datas()))[:-1]]
+xs = [robot_model.defaultState] * (boxddp.problem.T + 1)
+us = [m.quasiStatic(d, robot_model.defaultState) for m, d in list(zip(boxddp.problem.runningModels, boxddp.problem.runningDatas))]
 
 # Solve the DDP problem
 boxddp.solve(xs, us, 100, False, 0.1)
