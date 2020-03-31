b72d07a724386bf9370425b45879d73b957f3896
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-03-31 18:37:23 +0100

    [cleanup] Implemented the quasiStatic_x in hxx file

--- include/crocoddyl/core/action-base.hpp
+++ include/crocoddyl/core/action-base.hpp
@@ -47,12 +47,7 @@ class ActionModelAbstractTpl {
   void quasiStatic(const boost::shared_ptr<ActionDataAbstract>& data, Eigen::Ref<VectorXs> u,
                    const Eigen::Ref<const VectorXs>& x, const std::size_t& maxiter = 100, const Scalar& tol = 1e-9);
   VectorXs quasiStatic_x(const boost::shared_ptr<ActionDataAbstract>& data, const VectorXs& x,
-                         const std::size_t& maxiter = 100, const Scalar& tol = 1e-9) {
-    VectorXs u(nu_);
-    u.setZero();
-    quasiStatic(data, u, x, maxiter, tol);
-    return u;
-  }
+                         const std::size_t& maxiter = 100, const Scalar& tol = 1e-9);
 
   const std::size_t& get_nu() const;
   const std::size_t& get_nr() const;
