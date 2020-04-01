e90b07d1ca39996f03dfdce6b0d2304eb2780996
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-04-01 18:53:27 +0100

    [actuation] Added actuation numdiff class

---
+++ include/crocoddyl/core/numdiff/actuation.hpp
@@ -0,0 +1,152 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2020, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef CROCODDYL_CORE_NUMDIFF_ACTUATION_HPP_
+#define CROCODDYL_CORE_NUMDIFF_ACTUATION_HPP_
+
+#include <vector>
+#include <iostream>
+
+#include "crocoddyl/core/fwd.hpp"
+#include "crocoddyl/core/actuation-base.hpp"
+
+namespace crocoddyl {
+
+template <typename _Scalar>
+class ActuationModelNumDiffTpl : public ActuationModelAbstractTpl<_Scalar> {
+ public:
+  typedef _Scalar Scalar;
+  typedef MathBaseTpl<Scalar> MathBase;
+  typedef ActuationModelAbstractTpl<Scalar> Base;
+  typedef ActuationDataAbstractTpl<Scalar> ActuationDataAbstract;
+  typedef typename MathBase::VectorXs VectorXs;
+  typedef typename MathBase::MatrixXs MatrixXs;
+
+  /**
+   * @brief Construct a new ActuationModelNumDiff object
+   *
+   * @param model
+   */
+  explicit ActuationModelNumDiffTpl(boost::shared_ptr<Base> model);
+
+  /**
+   * @brief Destroy the ActuationModelNumDiff object
+   */
+  ~ActuationModelNumDiffTpl();
+
+  /**
+   * @brief @copydoc Base::calc()
+   */
+  virtual void calc(const boost::shared_ptr<ActuationDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
+                    const Eigen::Ref<const VectorXs>& u);
+
+  /**
+   * @brief @copydoc Base::calcDiff()
+   */
+  virtual void calcDiff(const boost::shared_ptr<ActuationDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
+                        const Eigen::Ref<const VectorXs>& u);
+
+  /**
+   * @brief Create a Data object from the given model.
+   *
+   * @return boost::shared_ptr<ActuationDataAbstract>
+   */
+  virtual boost::shared_ptr<ActuationDataAbstract> createData();
+
+  /**
+   * @brief Get the model_ object
+   *
+   * @return Base&
+   */
+  const boost::shared_ptr<Base>& get_model() const;
+
+  /**
+   * @brief Get the disturbance_ object
+   *
+   * @return const Scalar&
+   */
+  const Scalar& get_disturbance() const;
+
+  /**
+   * @brief Set the disturbance_ object
+   *
+   * @param disturbance is the value used to find the numerical derivative
+   */
+  void set_disturbance(const Scalar& disturbance);
+
+ private:
+  /**
+   * @brief This is the model to compute the finite differentiation from
+   */
+  boost::shared_ptr<Base> model_;
+
+  /**
+   * @brief This is the numerical disturbance value used during the numerical
+   * differentiation
+   */
+  Scalar disturbance_;
+
+ protected:
+  using Base::nu_;
+};
+
+template <typename _Scalar>
+struct ActuationDataNumDiffTpl : public ActuationDataAbstractTpl<_Scalar> {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  typedef _Scalar Scalar;
+  typedef MathBaseTpl<Scalar> MathBase;
+  typedef typename MathBase::VectorXs VectorXs;
+  typedef ActuationDataAbstractTpl<Scalar> Base;
+
+  /**
+   * @brief Construct a new ActuationDataNumDiff object
+   *
+   * @tparam Model is the type of the ActuationModel.
+   * @param model is the object to compute the numerical differentiation from.
+   */
+  template <template <typename Scalar> class Model>
+  explicit ActuationDataNumDiffTpl(Model<Scalar>* const model)
+      : Base(model),
+        dx(model->get_model()->get_state()->get_ndx()),
+        du(model->get_model()->get_nu()),
+        xp(model->get_model()->get_state()->get_nx()) {
+    dx.setZero();
+    du.setZero();
+    xp.setZero();
+    const std::size_t& ndx = model->get_model()->get_state()->get_ndx();
+    const std::size_t& nu = model->get_model()->get_nu();
+    data_0 = model->get_model()->createData();
+    for (std::size_t i = 0; i < ndx; ++i) {
+      data_x.push_back(model->get_model()->createData());
+    }
+    for (std::size_t i = 0; i < nu; ++i) {
+      data_u.push_back(model->get_model()->createData());
+    }
+  }
+
+  VectorXs dx;                     //!< State disturbance
+  VectorXs du;                     //!< Control disturbance
+  VectorXs xp;                     //!< The integrated state from the disturbance on one DoF "\f$ \int x dx_i \f$"
+  boost::shared_ptr<Base> data_0;  //!< The data that contains the final results
+  std::vector<boost::shared_ptr<Base> > data_x;  //!< The temporary data associated with the state variation
+  std::vector<boost::shared_ptr<Base> > data_u;  //!< The temporary data associated with the control variation
+
+  using Base::dtau_du;
+  using Base::dtau_dx;
+  using Base::tau;
+};
+
+}  // namespace crocoddyl
+
+/* --- Details -------------------------------------------------------------- */
+/* --- Details -------------------------------------------------------------- */
+/* --- Details -------------------------------------------------------------- */
+#include "crocoddyl/core/numdiff/actuation.hxx"
+
+#endif  // CROCODDYL_CORE_NUMDIFF_ACTUATION_HPP_
---
+++ include/crocoddyl/core/numdiff/actuation.hxx
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2020, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#include "crocoddyl/core/utils/exception.hpp"
+#include "crocoddyl/core/numdiff/actuation.hpp"
+
+namespace crocoddyl {
+
+template <typename Scalar>
+ActuationModelNumDiffTpl<Scalar>::ActuationModelNumDiffTpl(boost::shared_ptr<Base> model)
+    : Base(model->get_state(), model->get_nu(), model->get_nr()), model_(model) {
+  disturbance_ = std::sqrt(2.0 * std::numeric_limits<Scalar>::epsilon());
+}
+
+template <typename Scalar>
+ActuationModelNumDiffTpl<Scalar>::~ActuationModelNumDiffTpl() {}
+
+template <typename Scalar>
+void ActuationModelNumDiffTpl<Scalar>::calc(const boost::shared_ptr<ActuationDataAbstract>& data,
+                                            const Eigen::Ref<const VectorXs>& x, const Eigen::Ref<const VectorXs>& u) {
+  if (static_cast<std::size_t>(x.size()) != model_->get_state()->get_nx()) {
+    throw_pretty("Invalid argument: "
+                 << "x has wrong dimension (it should be " + std::to_string(model_->get_state()->get_nx()) + ")");
+  }
+  if (static_cast<std::size_t>(u.size()) != nu_) {
+    throw_pretty("Invalid argument: "
+                 << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
+  }
+  boost::shared_ptr<ActuationDataNumDiffTpl<Scalar> > data_nd =
+      boost::static_pointer_cast<ActuationDataNumDiffTpl<Scalar> >(data);
+  model_->calc(data_nd->data_0, x, u);
+  data->tau = data_nd->data_0->tau;
+}
+
+template <typename Scalar>
+void ActuationModelNumDiffTpl<Scalar>::calcDiff(const boost::shared_ptr<ActuationDataAbstract>& data,
+                                                const Eigen::Ref<const VectorXs>& x,
+                                                const Eigen::Ref<const VectorXs>& u) {
+  if (static_cast<std::size_t>(x.size()) != model_->get_state()->get_nx()) {
+    throw_pretty("Invalid argument: "
+                 << "x has wrong dimension (it should be " + std::to_string(model_->get_state()->get_nx()) + ")");
+  }
+  if (static_cast<std::size_t>(u.size()) != nu_) {
+    throw_pretty("Invalid argument: "
+                 << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
+  }
+  boost::shared_ptr<ActuationDataNumDiffTpl<Scalar> > data_nd =
+      boost::static_pointer_cast<ActuationDataNumDiffTpl<Scalar> >(data);
+
+  const VectorXs& tau0 = data_nd->data_0->tau;
+
+  // Computing the d Actuation(x,u) / dx
+  data_nd->dx.setZero();
+  for (std::size_t ix = 0; ix < model_->get_state()->get_ndx(); ++ix) {
+    data_nd->dx(ix) = disturbance_;
+    model_->get_state()->integrate(x, data_nd->dx, data_nd->xp);
+    model_->calc(data_nd->data_x[ix], data_nd->xp, u);
+    data_nd->dtau_dx.col(ix) = (data_nd->data_x[ix]->tau0 - tau0) / disturbance_;
+    data_nd->dx(ix) = 0.0;
+  }
+
+  // Computing the d Actuation(x,u) / du
+  data_nd->du.setZero();
+  for (unsigned iu = 0; iu < model_->get_nu(); ++iu) {
+    data_nd->du(iu) = disturbance_;
+    model_->calc(data_nd->data_u[iu], x, u + data_nd->du);
+    data_nd->dtau_du.col(iu) = (data_nd->data_u[iu]->tau0 - tau0) / disturbance_;
+    data_nd->du(iu) = 0.0;
+  }
+}
+
+template <typename Scalar>
+boost::shared_ptr<ActuationDataAbstractTpl<Scalar> > ActuationModelNumDiffTpl<Scalar>::createData() {
+  return boost::make_shared<ActuationDataNumDiffTpl<Scalar> >(this);
+}
+
+template <typename Scalar>
+const boost::shared_ptr<ActuationModelAbstractTpl<Scalar> >& ActuationModelNumDiffTpl<Scalar>::get_model() const {
+  return model_;
+}
+
+template <typename Scalar>
+const Scalar& ActuationModelNumDiffTpl<Scalar>::get_disturbance() const {
+  return disturbance_;
+}
+
+template <typename Scalar>
+void ActuationModelNumDiffTpl<Scalar>::set_disturbance(const Scalar& disturbance) {
+  if (disturbance < 0.) {
+    throw_pretty("Invalid argument: "
+                 << "Disturbance value is positive");
+  }
+  disturbance_ = disturbance;
+}
+
+}  // namespace crocoddyl
