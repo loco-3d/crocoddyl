2e9415926d9d374cd637fbb9bf55d47f4f0a3058
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-04-02 12:56:54 +0200

    add timer getduration in us

--- include/crocoddyl/core/utils/timer.hpp
+++ include/crocoddyl/core/utils/timer.hpp
@@ -25,7 +25,13 @@ class Timer {
     duration_ += static_cast<double>(finish_.tv_nsec - start_.tv_nsec) / 1000;
     return duration_ / 1000.;
   }
-
+  
+  inline double get_us_duration() {
+    clock_gettime(CLOCK_MONOTONIC, &finish_);
+    duration_ = static_cast<double>(finish_.tv_sec - start_.tv_sec) * 1000000;
+    duration_ += static_cast<double>(finish_.tv_nsec - start_.tv_nsec) / 1000;
+    return duration_;
+  }
  private:
   struct timespec start_;
   struct timespec finish_;
