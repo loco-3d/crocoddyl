88bc473ded250112206b5713cfde90c1f2a0770c
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-04-18 23:16:49 +0200

    fix LICENSES in files.

--- unittest/test_codegen.cpp
+++ unittest/test_codegen.cpp
@@ -1,9 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2018-2020, LAAS-CNRS, New York University,
-//                          Max Planck Gesellschaft, University of Edinburgh,
-//                          INRIA
+// Copyright (C) 2018-2020, LAAS-CNRS
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
