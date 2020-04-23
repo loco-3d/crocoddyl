b97a5b571eb420e1a6f6fce3f3f42af241d09a0c
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-04-23 09:54:12 +0100

    [cleanup] Fixed some copyrigh info

--- include/crocoddyl/core/numdiff/actuation.hxx
+++ include/crocoddyl/core/numdiff/actuation.hxx
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2018-2020, LAAS-CNRS, University of Edinburgh
+// Copyright (C) 2018-2020, University of Edinburgh, LAAS-CNRS
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
