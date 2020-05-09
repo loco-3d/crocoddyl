3813efadccf688bc33c6413784a493e23c139722
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-05-09 18:26:15 +0100

    [doc] Added the doxygen documentation of the shooting problem + improved the previous Python one

--- bindings/python/crocoddyl/core/optctrl/shooting.cpp
+++ bindings/python/crocoddyl/core/optctrl/shooting.cpp
@@ -31,7 +31,7 @@ void exposeShootingProblem() {
       "ShootingProblem",
       "Declare a shooting problem.\n\n"
       "A shooting problem declares the initial state, a set of running action models and a\n"
-      "terminal action model. It has two main methods - calc, calcDiff and rollout. The\n"
+      "terminal action model. It has three main methods - calc, calcDiff and rollout. The\n"
       "first computes the set of next states and cost values per each action model. calcDiff\n"
       "updates the derivatives of all action models. The last rollouts the stacks of actions\n"
       "models.",
@@ -39,7 +39,7 @@ void exposeShootingProblem() {
                boost::shared_ptr<ActionModelAbstract> >(bp::args("self", "x0", "runningModels", "terminalModel"),
                                                         "Initialize the shooting problem and allocate its data.\n\n"
                                                         ":param x0: initial state\n"
-                                                        ":param runningModels: running action models\n"
+                                                        ":param runningModels: running action models (size T)\n"
                                                         ":param terminalModel: terminal action model"))
       .def(bp::init<Eigen::VectorXd, std::vector<boost::shared_ptr<ActionModelAbstract> >,
                     boost::shared_ptr<ActionModelAbstract>, std::vector<boost::shared_ptr<ActionDataAbstract> >,
@@ -47,27 +47,28 @@ void exposeShootingProblem() {
           bp::args("self", "x0", "runningModels", "terminalModel", "runningDatas", "terminalData"),
           "Initialize the shooting problem (models and datas).\n\n"
           ":param x0: initial state\n"
-          ":param runningModels: running action models\n"
+          ":param runningModels: running action models (size T)\n"
           ":param terminalModel: terminal action model\n"
-          ":param runningDatas: running action datas\n"
+          ":param runningDatas: running action datas  (size T)\n"
           ":param terminalData: terminal action data"))
       .def("calc", &ShootingProblem::calc, bp::args("self", "xs", "us"),
            "Compute the cost and the next states.\n\n"
-           "First, it computes the next state and cost for each action model\n"
-           "along a state and control trajectory.\n"
-           ":param xs: time-discrete state trajectory\n"
-           ":param us: time-discrete control sequence\n"
+           "For each node k, and along the state xs and control us trajectories, it computes the next state x_{k+1}\n"
+           " and cost l_k.\n"
+           ":param xs: time-discrete state trajectory (size T+1)\n"
+           ":param us: time-discrete control sequence (size T)\n"
            ":returns the total cost value")
       .def("calcDiff", &ShootingProblem::calcDiff, bp::args("self", "xs", "us"),
-           "Compute the cost-and-dynamics derivatives.\n\n"
-           "These quantities are computed along a given pair of trajectories xs\n"
-           "(states) and us (controls).\n"
-           ":param xs: time-discrete state trajectory\n"
-           ":param us: time-discrete control sequence\n")
+           "Compute the derivatives of the cost and dynamics.\n\n"
+           "For each node k, and along the state x_s and control u_s trajectories, it computes the derivatives of\n"
+           "the cost (lx, lu, lxx, lxu, luu) and dynamics (fx, fu).\n"
+           ":param xs: time-discrete state trajectory (size T+1)\n"
+           ":param us: time-discrete control sequence (size T)\n"
+           ":returns the total cost value")
       .def("rollout", &ShootingProblem::rollout_us, bp::args("self", "us"),
            "Integrate the dynamics given a control sequence.\n\n"
            "Rollout the dynamics give a sequence of control commands\n"
-           ":param us: time-discrete control sequence")
+           ":param us: time-discrete control sequence (size T)")
       .def("updateNode", &ShootingProblem::updateNode, bp::args("self", "i", "model", "data"),
            "Update the model and data for a specific node.\n\n"
            ":param i: index of the node (0 <= i <= T + 1)\n"
@@ -78,7 +79,7 @@ void exposeShootingProblem() {
            ":param i: index of the node (0 <= i <= T + 1)\n"
            ":param model: new model")
       .add_property("T", bp::make_function(&ShootingProblem::get_T, bp::return_value_policy<bp::return_by_value>()),
-                    "number of nodes")
+                    "number of running nodes")
       .add_property("x0", bp::make_function(&ShootingProblem::get_x0, bp::return_internal_reference<>()),
                     &ShootingProblem::set_x0, "initial state")
       .add_property(
