6952503827e45df22baab57d4dec3dbc9c0a5185
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-05-13 22:54:05 +0100

    [bugfix] Fixed the call of executables in benchmark scripts (those files were renamed)

--- benchmark/lqr_optctrl.py
+++ benchmark/lqr_optctrl.py
@@ -72,7 +72,7 @@ def runShootingProblemCalcDiffBenchmark(xs, us, problem):
 
 print('\033[1m')
 print('C++:')
-popen = subprocess.check_call([os.path.dirname(os.path.abspath(__file__)) + "/lqr", str(T)])
+popen = subprocess.check_call([os.path.dirname(os.path.abspath(__file__)) + "/lqr-optctrl", str(T)])
 
 print('Python bindings:')
 xs, us, problem = createProblem(crocoddyl.ActionModelLQR)
