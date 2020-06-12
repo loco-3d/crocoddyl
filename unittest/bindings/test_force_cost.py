5da4a8a8bc27eb8e783ba7d8db358302676bd07a
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-06-12 08:10:18 +0100

    [unittest] Added test for the contact impulse cost

--- unittest/bindings/test_force_cost.py
+++ unittest/bindings/test_force_cost.py
@@ -8,9 +8,10 @@ crocoddyl.switchToNumpyMatrix()
 # Create robot model and data
 ROBOT_MODEL = example_robot_data.loadICub().model
 ROBOT_DATA = ROBOT_MODEL.createData()
-
-# Create differential action model and data; link the contact data
 ROBOT_STATE = crocoddyl.StateMultibody(ROBOT_MODEL)
+
+# Contact force cost
+# Create differential action model and data; link the contact data
 ACTUATION = crocoddyl.ActuationModelFloatingBase(ROBOT_STATE)
 CONTACTS = crocoddyl.ContactModelMultiple(ROBOT_STATE, ACTUATION.nu)
 CONTACT_6D_1 = crocoddyl.ContactModel6D(
@@ -41,6 +42,45 @@ dnum = MODEL_ND.createData()
 
 x = ROBOT_STATE.rand()
 u = pinocchio.utils.rand(ACTUATION.nu)
+MODEL.calc(DATA, x, u)
+MODEL.calcDiff(DATA, x, u)
+MODEL_ND.calc(dnum, x, u)
+MODEL_ND.calcDiff(dnum, x, u)
+assertNumDiff(DATA.Fx, dnum.Fx, NUMDIFF_MODIFIER *
+              MODEL_ND.disturbance)  # threshold was 2.7e-2, is now 2.11e-4 (see assertNumDiff.__doc__)
+assertNumDiff(DATA.Fu, dnum.Fu, NUMDIFF_MODIFIER *
+              MODEL_ND.disturbance)  # threshold was 2.7e-2, is now 2.11e-4 (see assertNumDiff.__doc__)
+assertNumDiff(DATA.Lx, dnum.Lx, NUMDIFF_MODIFIER *
+              MODEL_ND.disturbance)  # threshold was 2.7e-2, is now 2.11e-4 (see assertNumDiff.__doc__)
+assertNumDiff(DATA.Lu, dnum.Lu, NUMDIFF_MODIFIER *
+              MODEL_ND.disturbance)  # threshold was 2.7e-2, is now 2.11e-4 (see assertNumDiff.__doc__)
+
+# Contact impulse cost
+IMPULSES = crocoddyl.ImpulseModelMultiple(ROBOT_STATE)
+IMPULSE_6D_1 = crocoddyl.ImpulseModel6D(ROBOT_STATE, ROBOT_MODEL.getFrameId('r_sole'))
+IMPULSE_3D_2 = crocoddyl.ImpulseModel3D(ROBOT_STATE, ROBOT_MODEL.getFrameId('l_sole'))
+IMPULSES.addImpulse("r_sole_contact", IMPULSE_6D_1)
+IMPULSES.addImpulse("l_sole_contact", IMPULSE_3D_2)
+COSTS = crocoddyl.CostModelSum(ROBOT_STATE, 0)
+COSTS.addCost(
+    "impulse_6d",
+    crocoddyl.CostModelContactImpulse(ROBOT_STATE,
+                                      crocoddyl.FrameForce(ROBOT_MODEL.getFrameId('r_sole'), pinocchio.Force.Random()),
+                                      6), 1.)
+COSTS.addCost(
+    "impulse_3d",
+    crocoddyl.CostModelContactImpulse(ROBOT_STATE,
+                                      crocoddyl.FrameForce(ROBOT_MODEL.getFrameId('l_sole'), pinocchio.Force.Random()),
+                                      3), 1.)
+MODEL = crocoddyl.ActionModelImpulseFwdDynamics(ROBOT_STATE, IMPULSES, COSTS, 0., 0., True)
+DATA = MODEL.createData()
+
+# Created DAM numdiff
+MODEL_ND = crocoddyl.ActionModelNumDiff(MODEL)
+dnum = MODEL_ND.createData()
+
+x = ROBOT_STATE.rand()
+u = pinocchio.utils.rand(0)
 MODEL.calc(DATA, x, u)
 MODEL.calcDiff(DATA, x, u)
 MODEL_ND.calc(dnum, x, u)
