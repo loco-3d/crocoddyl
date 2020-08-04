f2577dd1c69e74ff153b5bdd3eebdb441eef3f6c
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-08-04 13:14:04 +0200

    [contact][quasi-static] fix for empty jacobian

--- include/crocoddyl/multibody/actions/contact-fwddyn.hxx
+++ include/crocoddyl/multibody/actions/contact-fwddyn.hxx
@@ -196,9 +196,11 @@ void DifferentialActionModelContactFwdDynamicsTpl<Scalar>::quasiStatic(
   contacts_->calc(d->multibody.contacts, x);
   // Allocates memory
   MatrixXs jc_view = d->multibody.contacts->Jc.leftCols(state_->get_nv() - nu_);
-  d->pinocchio.lambda_c.noalias() =
-      pseudoInverse(jc_view).transpose() * d->multibody.actuation->tau.head(state_->get_nv() - nu_);
-  d->multibody.actuation->tau.noalias() -= d->multibody.contacts->Jc.transpose() * d->pinocchio.lambda_c;
+  if (jc_view.size() != 0) {
+    d->pinocchio.lambda_c.noalias() =
+        pseudoInverse(jc_view).transpose() * d->multibody.actuation->tau.head(state_->get_nv() - nu_);
+    d->multibody.actuation->tau.noalias() -= d->multibody.contacts->Jc.transpose() * d->pinocchio.lambda_c;
+  }
   actuation_->get_actuated(d->multibody.actuation, u);
 
   d->multibody.actuation->tau.setZero();
