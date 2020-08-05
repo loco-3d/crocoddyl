aa4846019f588be4d939fba76e19673d600dc036
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-08-05 20:32:16 +0200

    [benchmarks][python] python script to parse and store csv file data. could be used for plotting

---
+++ benchmark/read_csv.py
@@ -0,0 +1,56 @@
+"""
+Reads csv benchmarks provided as input. Creates a DataFrame for each benchmark, and prints the table for easy reading.
+The benchmarks are stored in the object benches
+
+Depends: pandas module (pip install pandas)
+
+How to Run: python -i read_csv.py /tmp/bench1.bench /tmp/bench2.bench ...
+
+Output: benches
+        benches.bench1 (DataFrame), benches.bench2 (DataFrame)...
+
+e.g.: ipython -i read_csv.py /tmp/Anymal_19DoF.bench /tmp/HyQ_19DoF.bench
+e.g.: ipython -i read_csv.py /tmp/*.bench
+
+"""
+
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+import sys
+from os.path import exists, splitext, basename
+from collections import defaultdict
+pd.options.display.width = 0
+
+
+class Benchmarks():
+    pass
+    
+def parseCsvFile(filename, sep=',', delimiter=None):
+    """Inputs a file and ouputs a matrix.
+    input: (string) filename
+    output: (numpy array) seq
+    """
+    col_types = dict(fn_name=str,nthreads=int,with_cg=bool,
+                     mean=float,stddev=float,max=float,min=float,
+                     mean_per_nodes=float,stddev_per_nodes=float)
+    
+    seq = pd.read_csv(filename, header=0,
+                      sep=sep, delim_whitespace=False,
+                      quoting = 2, index_col=0, dtype=col_types)
+    
+    return seq
+
+
+benches = Benchmarks()
+
+filenames = sys.argv[1:]
+for filename in filenames:
+    if not exists(filename):
+        continue
+    benchname = splitext(basename(filename))[0]
+    seq = parseCsvFile(filename)
+    benches.__dict__[benchname] = seq
+    print(benchname)
+    print(seq)
+    print("********************")
