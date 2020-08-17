65cad4de907469ca3e02afd01628b5da81967185
Carlos Mastalli <carlos.mastalli@gmail.com>
2020-08-17 20:23:04 +0100

    [frame] Renamed support_region to box (deprecated old function)

--- include/crocoddyl/multibody/frames.hpp
+++ include/crocoddyl/multibody/frames.hpp
@@ -184,42 +184,37 @@ struct FrameCoPSupportTpl {
   typedef Eigen::Matrix<Scalar, 4, 6> Matrix46;
 
  public:
-  explicit FrameCoPSupportTpl() : id_(0), support_region_(Vector2s::Zero()) { update_A(); }
+  explicit FrameCoPSupportTpl() : id_(0), box_(Vector2s::Zero()) { update_A(); }
   FrameCoPSupportTpl(const FrameCoPSupportTpl<Scalar>& value)
-      : id_(value.get_id()), support_region_(value.get_support_region()), A_(value.get_A()) {}
-  FrameCoPSupportTpl(const FrameIndex& id, const Vector2s& support_region) : id_(id), support_region_(support_region) {
-    update_A();
-  }
+      : id_(value.get_id()), box_(value.get_box()), A_(value.get_A()) {}
+  FrameCoPSupportTpl(const FrameIndex& id, const Vector2s& box) : id_(id), box_(box) { update_A(); }
   friend std::ostream& operator<<(std::ostream& os, const FrameCoPSupportTpl<Scalar>& X) {
-    os << "            id: " << X.get_id() << std::endl
-       << "foot dimension: " << std::endl
-       << X.get_support_region() << std::endl;
+    os << " id: " << X.get_id() << std::endl << "box: " << std::endl << X.get_box() << std::endl;
     return os;
   }
 
   // Define the inequality matrix A to implement A * f >= 0. Compare eq.(18-19) in
   // https://hal.archives-ouvertes.fr/hal-02108449/document
   void update_A() {
-    A_ << Scalar(0), Scalar(0), support_region_[0] / Scalar(2), Scalar(0), Scalar(-1), Scalar(0), Scalar(0), Scalar(0),
-        support_region_[0] / Scalar(2), Scalar(0), Scalar(1), Scalar(0), Scalar(0), Scalar(0),
-        support_region_[1] / Scalar(2), Scalar(1), Scalar(0), Scalar(0), Scalar(0), Scalar(0),
-        support_region_[1] / Scalar(2), Scalar(-1), Scalar(0), Scalar(0);
+    A_ << Scalar(0), Scalar(0), box_[0] / Scalar(2), Scalar(0), Scalar(-1), Scalar(0), Scalar(0), Scalar(0),
+        box_[0] / Scalar(2), Scalar(0), Scalar(1), Scalar(0), Scalar(0), Scalar(0), box_[1] / Scalar(2), Scalar(1),
+        Scalar(0), Scalar(0), Scalar(0), Scalar(0), box_[1] / Scalar(2), Scalar(-1), Scalar(0), Scalar(0);
   }
 
   void set_id(FrameIndex id) { id_ = id; }
-  void set_support_region(const Vector2s& support_region) {
-    support_region_ = support_region;
+  void set_box(const Vector2s& box) {
+    box_ = box;
     update_A();
   }
 
   const FrameIndex& get_id() const { return id_; }
-  const Vector2s& get_support_region() const { return support_region_; }
+  const Vector2s& get_box() const { return box_; }
   const Matrix46& get_A() const { return A_; }
 
  private:
-  FrameIndex id_;            //!< contact frame ID
-  Vector2s support_region_;  //!< cop support region = (length, width)
-  Matrix46 A_;               //!< inequality matrix
+  FrameIndex id_;  //!< contact frame ID
+  Vector2s box_;   //!< cop support region = (length, width)
+  Matrix46 A_;     //!< inequality matrix
 };
 
 }  // namespace crocoddyl
