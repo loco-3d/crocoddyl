35a4c3e62c087cfaf7e5ccfd2aad60e2b4937296
Ewen Dantec <edantec@eboshisan>
2020-09-10 18:16:26 +0200

    Working 2D contact implementation

--- include/crocoddyl/multibody/contacts/contact-2d.hxx
+++ include/crocoddyl/multibody/contacts/contact-2d.hxx
@@ -26,25 +26,26 @@ void ContactModel2DTpl<Scalar>::calc(const boost::shared_ptr<ContactDataAbstract
                                      const Eigen::Ref<const VectorXs>&) {
   Data* d = static_cast<Data*>(data.get());
   pinocchio::updateFramePlacement(*state_->get_pinocchio().get(), *d->pinocchio, xref_.id);
-  d->v = pinocchio::getFrameVelocity(*state_->get_pinocchio().get(), *d->pinocchio, xref_.id, pinocchio::LOCAL);
-  const Vector3s& vw = d->v.angular();
-  const Vector3s& vv = d->v.linear();
-
   pinocchio::getFrameJacobian(*state_->get_pinocchio().get(), *d->pinocchio, xref_.id, pinocchio::LOCAL, d->fJf);
+  d->v = pinocchio::getFrameVelocity(*state_->get_pinocchio().get(), *d->pinocchio, xref_.id);
+  d->a = pinocchio::getFrameAcceleration(*state_->get_pinocchio().get(), *d->pinocchio, xref_.id);
+
   d->Jc.row(0) = d->fJf.row(0);
   d->Jc.row(1) = d->fJf.row(2);
-
-  d->a = pinocchio::getFrameAcceleration(*state_->get_pinocchio().get(), *d->pinocchio, xref_.id);
-  d->a0[0] = d->a.linear()[0] + vw[1] * vv[2] - vw[2] * vv[1];
-  d->a0[1] = d->a.linear()[2] + vw[0] * vv[1] - vw[1] * vv[0];
+  
+  d->vw = d->v.angular();
+  d->vv = d->v.linear();
+  
+  d->a0[0] = d->a.linear()[0] + d->vw[1] * d->vv[2] - d->vw[2] * d->vv[1];
+  d->a0[1] = d->a.linear()[2] + d->vw[0] * d->vv[1] - d->vw[1] * d->vv[0];
 
   if (gains_[0] != 0.) {
     d->a0[0] += gains_[0] * (d->pinocchio->oMf[xref_.id].translation()[0] - xref_.translation[0]);
     d->a0[1] += gains_[0] * (d->pinocchio->oMf[xref_.id].translation()[2] - xref_.translation[2]);
   }
   if (gains_[1] != 0.) {
-    d->a0[0] += gains_[1] * vv[0];
-    d->a0[1] += gains_[1] * vv[2];
+    d->a0[0] += gains_[1] * d->vv[0];
+    d->a0[1] += gains_[1] * d->vv[2];
   }
 }
 
@@ -70,16 +71,11 @@ void ContactModel2DTpl<Scalar>::calcDiff(const boost::shared_ptr<ContactDataAbst
   d->da0_dx.leftCols(nv).row(1).noalias() -= d->vv_skew.row(2) * d->fXjdv_dq.template bottomRows<3>();
 
   d->da0_dx.rightCols(nv).row(0) = d->fXjda_dv.row(0);
-  typename MathBase::RowVector2s vw_skew2D;
-  vw_skew2D(0,0) = d->vw_skew(0,0);
-  vw_skew2D(0,1) = d->vw_skew(0,2);
-  d->da0_dx.rightCols(nv).row(0).noalias() += vw_skew2D * d->Jc;
+  d->da0_dx.rightCols(nv).row(0).noalias() += d->vw_skew.row(0) * d->fJf.template topRows<3>();
   d->da0_dx.rightCols(nv).row(0).noalias() -= d->vv_skew.row(0) * d->fJf.template bottomRows<3>();
 
   d->da0_dx.rightCols(nv).row(1) = d->fXjda_dv.row(2);
-  vw_skew2D(0,0) = d->vw_skew(2,0);
-  vw_skew2D(0,1) = d->vw_skew(2,2);
-  d->da0_dx.rightCols(nv).row(1).noalias() += vw_skew2D * d->Jc;
+  d->da0_dx.rightCols(nv).row(1).noalias() += d->vw_skew.row(2) * d->fJf.template topRows<3>();
   d->da0_dx.rightCols(nv).row(1).noalias() -= d->vv_skew.row(2) * d->fJf.template bottomRows<3>();
 
   if (gains_[0] != 0.) {
