d75c357657120fad59816651e8d5d998f7be6ca0
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-20 14:49:35 +0200

    add deprecated headers, fix #872

---
+++ include/crocoddyl/multibody/costs/control-cost.hpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2020, LAAS-CNRS, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef CROCODDYL_MULTIBODY_COSTS_CONTROL_COST_HPP_
+#define CROCODDYL_MULTIBODY_COSTS_CONTROL_COST_HPP_
+
+#include "crocoddyl/core/utils/deprecate.hpp"
+CROCODDYL_PRAGMA_DEPRECATED_HEADER("crocoddyl/multibody/costs/control-cost.hpp",
+                                   "crocoddyl/core/costs/control-cost.hpp")
+#include "crocoddyl/core/costs/control-cost.hpp"
+
+#endif
