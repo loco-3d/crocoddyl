2276b600e32288f01ef9c3cf466681638c12d840
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2020-10-29 09:24:43 +0000

    [deprecate] Added deprecated header for numdiff cost

---
+++ include/crocoddyl/multibody/numdiff/cost.hpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2018-2020, LAAS-CNRS, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef CROCODDYL_MULTIBODY_NUMDIFF_COST_HPP_
+#define CROCODDYL_MULTIBODY_NUMDIFF_COST_HPP_
+
+#include "crocoddyl/core/utils/deprecate.hpp"
+CROCODDYL_PRAGMA_DEPRECATED_HEADER("crocoddyl/multibody/numdiff/cost-sum.hpp", "crocoddyl/core/numdiff/cost-sum.hpp")
+#include "crocoddyl/core/numdiff/cost.hpp"
+
+#endif  // CROCODDYL_MULTIBODY_NUMDIFF_COST_HPP_
