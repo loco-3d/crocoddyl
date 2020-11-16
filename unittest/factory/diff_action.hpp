b7071ce55a802010177e6ea9cf879ad5a2285019
Diiso <louismontaut@gmail.com>
2020-11-16 15:28:19 +0100

    copyright rectification

--- unittest/factory/diff_action.hpp
+++ unittest/factory/diff_action.hpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2018-2020, LAAS-CNRS
+// Copyright (C) 2018-2020, University of Edinburgh
 // Copyright (C) 2020 CTU, INRIA
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
