0bdcd323dda0189d990852315ccd00b0bd236ea5
Carlos Mastalli <carlos.mastalli@gmail.com>
2021-01-21 18:22:09 +0000

    [friction][unittes] Created constructor based on rotation mat. + added more tests

--- include/crocoddyl/multibody/friction-cone.hxx
+++ include/crocoddyl/multibody/friction-cone.hxx
@@ -17,6 +17,7 @@ FrictionConeTpl<Scalar>::FrictionConeTpl()
       A_(nf_ + 1, 3),
       ub_(nf_ + 1),
       lb_(nf_ + 1),
+      R_(Matrix3s::Identity()),
       nsurf_(Vector3s::UnitZ()),
       mu_(Scalar(0.7)),
       inner_appr_(true),
@@ -31,9 +32,15 @@ FrictionConeTpl<Scalar>::FrictionConeTpl()
 }
 
 template <typename Scalar>
-FrictionConeTpl<Scalar>::FrictionConeTpl(const Vector3s& normal, const Scalar mu, std::size_t nf,
-                                         const bool inner_appr, const Scalar min_nforce, const Scalar max_nforce)
-    : nf_(nf), nsurf_(normal), mu_(mu), inner_appr_(inner_appr), min_nforce_(min_nforce), max_nforce_(max_nforce) {
+FrictionConeTpl<Scalar>::FrictionConeTpl(const Matrix3s& R, const Scalar mu, std::size_t nf, const bool inner_appr,
+                                         const Scalar min_nforce, const Scalar max_nforce)
+    : nf_(nf),
+      R_(R),
+      nsurf_(R_.transpose() * Vector3s::UnitZ()),
+      mu_(mu),
+      inner_appr_(inner_appr),
+      min_nforce_(min_nforce),
+      max_nforce_(max_nforce) {
   if (nf_ % 2 != 0) {
     nf_ = 4;
     std::cerr << "Warning: nf has to be an even number, set to 4" << std::endl;
@@ -47,11 +54,39 @@ FrictionConeTpl<Scalar>::FrictionConeTpl(const Vector3s& normal, const Scalar mu
 }
 
 template <typename Scalar>
+FrictionConeTpl<Scalar>::FrictionConeTpl(const Vector3s& nsurf, const Scalar mu, std::size_t nf, const bool inner_appr,
+                                         const Scalar min_nforce, const Scalar max_nforce)
+    : nf_(nf),
+      R_(Quaternions::FromTwoVectors(nsurf, Vector3s::UnitZ()).toRotationMatrix()),
+      nsurf_(nsurf),
+      mu_(mu),
+      inner_appr_(inner_appr),
+      min_nforce_(min_nforce),
+      max_nforce_(max_nforce) {
+  if (nf_ % 2 != 0) {
+    nf_ = 4;
+    std::cerr << "Warning: nf has to be an even number, set to 4" << std::endl;
+  }
+  if (!nsurf.isUnitary()) {
+    nsurf_ /= nsurf.norm();
+    std::cerr << "Warning: normal is not an unitary vector, then we normalized it" << std::endl;
+  }
+  A_ = MatrixX3s::Zero(nf_ + 1, 3);
+  ub_ = VectorXs::Zero(nf_ + 1);
+  lb_ = VectorXs::Zero(nf_ + 1);
+  R_ = Quaternions::FromTwoVectors(nsurf_, Vector3s::UnitZ()).toRotationMatrix();
+
+  // Update the inequality matrix and bounds
+  update();
+}
+
+template <typename Scalar>
 FrictionConeTpl<Scalar>::FrictionConeTpl(const FrictionConeTpl<Scalar>& cone)
     : nf_(cone.get_nf()),
       A_(cone.get_A()),
       ub_(cone.get_ub()),
       lb_(cone.get_lb()),
+      R_(cone.get_R()),
       nsurf_(cone.get_nsurf()),
       mu_(cone.get_mu()),
       inner_appr_(cone.get_inner_appr()),
@@ -77,14 +112,13 @@ void FrictionConeTpl<Scalar>::update() {
   }
 
   // Update the inequality matrix and the bounds
-  Matrix3s c_R_o = Quaternions::FromTwoVectors(nsurf_, Vector3s::UnitZ()).toRotationMatrix();
   Scalar theta_i;
   Vector3s tsurf_i;
   for (std::size_t i = 0; i < nf_ / 2; ++i) {
     theta_i = theta * static_cast<Scalar>(i);
     tsurf_i << cos(theta_i), sin(theta_i), Scalar(0.);
-    A_.row(2 * i) = (-mu * Vector3s::UnitZ() + tsurf_i).transpose() * c_R_o;
-    A_.row(2 * i + 1) = (-mu * Vector3s::UnitZ() - tsurf_i).transpose() * c_R_o;
+    A_.row(2 * i) = (-mu * Vector3s::UnitZ() + tsurf_i).transpose() * R_;
+    A_.row(2 * i + 1) = (-mu * Vector3s::UnitZ() - tsurf_i).transpose() * R_;
   }
   A_.row(nf_) = nsurf_.transpose();
   lb_(nf_) = min_nforce_;
@@ -94,11 +128,11 @@ void FrictionConeTpl<Scalar>::update() {
 template <typename Scalar>
 void FrictionConeTpl<Scalar>::update(const Vector3s& normal, const Scalar mu, const bool inner_appr,
                                      const Scalar min_nforce, const Scalar max_nforce) {
-  nsurf_ = normal;
-  mu_ = mu;
-  inner_appr_ = inner_appr;
-  min_nforce_ = min_nforce;
-  max_nforce_ = max_nforce;
+  set_nsurf(normal);
+  set_mu(mu_);
+  set_inner_appr(inner_appr);
+  set_min_nforce(min_nforce);
+  set_max_nforce(max_nforce);
 
   // Sanity checks
   if (!normal.isUnitary()) {
@@ -133,6 +167,11 @@ const typename MathBaseTpl<Scalar>::VectorXs& FrictionConeTpl<Scalar>::get_lb() 
 }
 
 template <typename Scalar>
+const typename MathBaseTpl<Scalar>::Matrix3s& FrictionConeTpl<Scalar>::get_R() const {
+  return R_;
+}
+
+template <typename Scalar>
 const typename MathBaseTpl<Scalar>::Vector3s& FrictionConeTpl<Scalar>::get_nsurf() const {
   return nsurf_;
 }
@@ -163,8 +202,20 @@ const Scalar FrictionConeTpl<Scalar>::get_max_nforce() const {
 }
 
 template <typename Scalar>
+void FrictionConeTpl<Scalar>::set_R(const Matrix3s& R) {
+  R_ = R;
+  nsurf_ = R_.transpose() * Vector3s::UnitZ();
+}
+
+template <typename Scalar>
 void FrictionConeTpl<Scalar>::set_nsurf(const Vector3s& nsurf) {
   nsurf_ = nsurf;
+  // Sanity checks
+  if (!nsurf.isUnitary()) {
+    nsurf_ /= nsurf.norm();
+    std::cerr << "Warning: normal is not an unitary vector, then we normalized it" << std::endl;
+  }
+  R_ = Quaternions::FromTwoVectors(nsurf_, Vector3s::UnitZ()).toRotationMatrix();
 }
 
 template <typename Scalar>
@@ -189,7 +240,8 @@ void FrictionConeTpl<Scalar>::set_max_nforce(const Scalar max_nforce) {
 
 template <typename Scalar>
 std::ostream& operator<<(std::ostream& os, const FrictionConeTpl<Scalar>& X) {
-  os << "    normal: " << X.get_nsurf().transpose() << std::endl;
+  os << "        R: " << X.get_R() << std::endl;
+  os << "  (nsurf): " << X.get_nsurf().transpose() << std::endl;
   os << "        mu: " << X.get_mu() << std::endl;
   os << "        nf: " << X.get_nf() << std::endl;
   os << "inner_appr: ";
--- unittest/test_friction_cone.cpp
+++ unittest/test_friction_cone.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2019-2020, University of Edinburgh
+// Copyright (C) 2019-2021, University of Edinburgh
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
@@ -17,20 +17,66 @@ using namespace boost::unit_test;
 using namespace crocoddyl::unittest;
 
 void test_constructor() {
-  // Create the friction cone
-  Eigen::Vector3d cone_normal = Eigen::Vector3d::UnitZ();
+  // Common parameters
   double mu = random_real_in_range(0.01, 1.);
   std::size_t nf = 2 * random_int_in_range(2, 16);
   bool inner_appr = false;
-  crocoddyl::FrictionCone cone(cone_normal, mu, nf, inner_appr);
 
-  BOOST_CHECK((cone.get_nsurf() - cone_normal).isMuchSmallerThan(1.0, 1e-9));
-  BOOST_CHECK(cone.get_mu() == mu);
-  BOOST_CHECK(cone.get_nf() == nf);
-  BOOST_CHECK(cone.get_inner_appr() == inner_appr);
-  BOOST_CHECK(static_cast<std::size_t>(cone.get_A().rows()) == nf + 1);
-  BOOST_CHECK(static_cast<std::size_t>(cone.get_lb().size()) == nf + 1);
-  BOOST_CHECK(static_cast<std::size_t>(cone.get_ub().size()) == nf + 1);
+  // No rotation
+  Eigen::Matrix3d R = Eigen::Matrix3d::Identity();
+  Eigen::Vector3d normal = R * Eigen::Vector3d::UnitZ();
+
+  // Create the friction cone with rotation and surface normal
+  crocoddyl::FrictionCone cone_1(R, mu, nf, inner_appr);
+  crocoddyl::FrictionCone cone_2(normal, mu, nf, inner_appr);
+
+  BOOST_CHECK((cone_1.get_R() - R).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_1.get_nsurf() - normal).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK(cone_1.get_mu() == mu);
+  BOOST_CHECK(cone_1.get_nf() == nf);
+  BOOST_CHECK(cone_1.get_inner_appr() == inner_appr);
+  BOOST_CHECK(static_cast<std::size_t>(cone_1.get_A().rows()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_1.get_lb().size()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_1.get_ub().size()) == nf + 1);
+  BOOST_CHECK((cone_2.get_R() - R).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_2.get_nsurf() - normal).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK(cone_2.get_mu() == mu);
+  BOOST_CHECK(cone_2.get_nf() == nf);
+  BOOST_CHECK(cone_2.get_inner_appr() == inner_appr);
+  BOOST_CHECK(static_cast<std::size_t>(cone_2.get_A().rows()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_2.get_lb().size()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_2.get_ub().size()) == nf + 1);
+  BOOST_CHECK((cone_1.get_A() - cone_2.get_A()).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_1.get_lb() - cone_2.get_lb()).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_1.get_ub() - cone_2.get_ub()).isMuchSmallerThan(1.0, 1e-9));
+
+  // With rotation
+  normal = Eigen::Vector3d(0., sqrt(2) / 2, sqrt(2) / 2);
+  R = Eigen::Quaternion<double>::FromTwoVectors(normal, Eigen::Vector3d::UnitZ()).toRotationMatrix();
+
+  // Create the friction cone with rotation and surface contact
+  cone_1 = crocoddyl::FrictionCone(R, mu, nf, inner_appr);
+  cone_2 = crocoddyl::FrictionCone(normal, mu, nf, inner_appr);
+
+  BOOST_CHECK((cone_1.get_R() - R).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_1.get_nsurf() - normal).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK(cone_1.get_mu() == mu);
+  BOOST_CHECK(cone_1.get_nf() == nf);
+  BOOST_CHECK(cone_1.get_inner_appr() == inner_appr);
+  BOOST_CHECK(static_cast<std::size_t>(cone_1.get_A().rows()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_1.get_lb().size()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_1.get_ub().size()) == nf + 1);
+  BOOST_CHECK((cone_2.get_R() - R).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_2.get_nsurf() - normal).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK(cone_2.get_mu() == mu);
+  BOOST_CHECK(cone_2.get_nf() == nf);
+  BOOST_CHECK(cone_2.get_inner_appr() == inner_appr);
+  BOOST_CHECK(static_cast<std::size_t>(cone_2.get_A().rows()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_2.get_lb().size()) == nf + 1);
+  BOOST_CHECK(static_cast<std::size_t>(cone_2.get_ub().size()) == nf + 1);
+  BOOST_CHECK((cone_1.get_A() - cone_2.get_A()).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_1.get_lb() - cone_2.get_lb()).isMuchSmallerThan(1.0, 1e-9));
+  BOOST_CHECK((cone_1.get_ub() - cone_2.get_ub()).isMuchSmallerThan(1.0, 1e-9));
 }
 
 void test_inner_approximation_of_friction_cone() {
@@ -43,6 +89,26 @@ void test_inner_approximation_of_friction_cone() {
   BOOST_CHECK_CLOSE(A_mu(0), mu * cos((2 * M_PI / static_cast<double>(nf)) / 2.), 1e-9);
   BOOST_CHECK_CLOSE(A_mu(1), mu * cos((2 * M_PI / static_cast<double>(nf)) / 2.), 1e-9);
   BOOST_CHECK_CLOSE(A_mu(2), mu * cos((2 * M_PI / static_cast<double>(nf)) / 2.), 1e-9);
+}
+
+void test_A_matrix_with_rotation_change() {
+  // Common parameters
+  double mu = random_real_in_range(0.01, 1.);
+  std::size_t nf = 2 * random_int_in_range(2, 16);
+  bool inner_appr = false;
+
+  // No rotation
+  Eigen::Matrix3d R = Eigen::Matrix3d::Identity();
+  crocoddyl::FrictionCone cone_1(R, mu, nf, inner_appr);
+
+  // With rotation
+  Eigen::Vector3d normal(0., sqrt(2) / 2, sqrt(2) / 2);
+  R = Eigen::Quaternion<double>::FromTwoVectors(normal, Eigen::Vector3d::UnitZ()).toRotationMatrix();
+  crocoddyl::FrictionCone cone_2(R, mu, nf, inner_appr);
+
+  for (std::size_t i = 0; i < 5; ++i) {
+    BOOST_CHECK((cone_1.get_A().row(i) - cone_2.get_A().row(i) * R.transpose()).isMuchSmallerThan(1.0, 1e-9));
+  }
 }
 
 void test_force_along_friction_cone_normal() {
@@ -118,7 +184,7 @@ void test_force_parallel_to_friction_cone_normal() {
   Eigen::Vector3d force = -random_real_in_range(0., 100.) * Eigen::Vector3d::UnitX();
   Eigen::VectorXd r = cone.get_A() * force;
 
-  // The last value of the residual is equasl to zero since the force is parallel to the
+  // The last value of the residual is equals to zero since the force is parallel to the
   // friction cone orientation
   BOOST_CHECK_CLOSE(r(nf), 0., 1e-9);
 
@@ -130,6 +196,7 @@ void test_force_parallel_to_friction_cone_normal() {
 void register_unit_tests() {
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_constructor)));
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_inner_approximation_of_friction_cone)));
+  framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_A_matrix_with_rotation_change)));
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_force_along_friction_cone_normal)));
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_negative_force_along_friction_cone_normal)));
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_force_parallel_to_friction_cone_normal)));
