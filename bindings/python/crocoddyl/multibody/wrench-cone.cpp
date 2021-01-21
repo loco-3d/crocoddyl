132af1e5555b4bbada735e6d40955c4d85b833d8
Carlos Mastalli <carlos.mastalli@gmail.com>
2021-01-21 15:57:56 +0000

    [python] Registered share pointer + bindings cleaned-up of friction and wrench cones

--- bindings/python/crocoddyl/multibody/wrench-cone.cpp
+++ bindings/python/crocoddyl/multibody/wrench-cone.cpp
@@ -14,6 +14,8 @@ namespace crocoddyl {
 namespace python {
 
 void exposeWrenchCone() {
+  bp::register_ptr_to_python<boost::shared_ptr<WrenchCone> >();
+
   bp::class_<WrenchCone>(
       "WrenchCone", "Model of the wrench cone as lb <= Af <= ub",
       bp::init<Eigen::Matrix3d, double, Eigen::Vector2d, bp::optional<std::size_t, double, double> >(
@@ -34,17 +36,18 @@ void exposeWrenchCone() {
            ":param min_nforce: minimum normal force (default 0.)\n"
            ":param max_nforce: maximum normal force (default sys.float_info.max)")
       .add_property("A", bp::make_function(&WrenchCone::get_A, bp::return_internal_reference<>()), "inequality matrix")
-      .add_property("lb", bp::make_function(&WrenchCone::get_lb, bp::return_internal_reference<>()),
-                    "inequality lower bound")
       .add_property("ub", bp::make_function(&WrenchCone::get_ub, bp::return_internal_reference<>()),
                     "inequality upper bound")
+      .add_property("lb", bp::make_function(&WrenchCone::get_lb, bp::return_internal_reference<>()),
+                    "inequality lower bound")
+      .add_property("nf", bp::make_function(&WrenchCone::get_nf, bp::return_value_policy<bp::return_by_value>()),
+                    "number of facets")
       .add_property("R", bp::make_function(&WrenchCone::get_R, bp::return_internal_reference<>()),
                     bp::make_function(&WrenchCone::set_R), "rotation matrix")
       .add_property("box", bp::make_function(&WrenchCone::get_box, bp::return_internal_reference<>()),
                     bp::make_function(&WrenchCone::set_box), "box size used to define the sole")
       .add_property("mu", bp::make_function(&WrenchCone::get_mu), bp::make_function(&WrenchCone::set_mu),
                     "friction coefficient")
-      .add_property("nf", bp::make_function(&WrenchCone::get_nf), "number of facets")
       .add_property("min_nforce",
                     bp::make_function(&WrenchCone::get_min_nforce, bp::return_value_policy<bp::return_by_value>()),
                     bp::make_function(&WrenchCone::set_min_nforce), "minimum normal force")
