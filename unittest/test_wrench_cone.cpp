b380b1713a10524f798c87bbe930d4555da4279d
Carlos Mastalli <carlos.mastalli@gmail.com>
2021-01-22 18:53:07 +0000

    [unittest][wip] Developing the cop-support vs wrench test

--- unittest/test_wrench_cone.cpp
+++ unittest/test_wrench_cone.cpp
@@ -11,6 +11,7 @@
 
 #include "crocoddyl/multibody/wrench-cone.hpp"
 #include "crocoddyl/multibody/friction-cone.hpp"
+#include "crocoddyl/multibody/cop-support.hpp"
 #include "crocoddyl/core/activations/quadratic-barrier.hpp"
 #include "pinocchio/math/quaternion.hpp"
 #include "unittest_common.hpp"
@@ -84,7 +85,7 @@ void test_constructor() {
   BOOST_CHECK((cone_1.get_ub() - cone_2.get_ub()).isMuchSmallerThan(1.0, 1e-9));
 }
 
-void test_againts_friction_cone() {
+void test_against_friction_cone() {
   // Common parameters
   Eigen::Quaterniond q;
   pinocchio::quaternion::uniformRandom(q);
@@ -106,6 +107,28 @@ void test_againts_friction_cone() {
   }
   BOOST_CHECK((wrench_cone.get_lb().head(nf + 1) - friction_cone.get_lb()).isMuchSmallerThan(1.0, 1e-9));
   BOOST_CHECK((wrench_cone.get_ub().head(nf + 1) - friction_cone.get_ub()).isMuchSmallerThan(1.0, 1e-9));
+}
+
+void test_against_cop_support() {
+  // // Common parameters
+  // Eigen::Quaterniond q;
+  // pinocchio::quaternion::uniformRandom(q);
+  // Eigen::Matrix3d R = q.toRotationMatrix();
+  // double mu = random_real_in_range(0.01, 1.);
+  // Eigen::Vector2d box = Eigen::Vector2d(random_real_in_range(0.01, 0.1), random_real_in_range(0.01, 0.1));
+  // std::size_t nf = 2 * random_int_in_range(2, 16);
+  // bool inner_appr = true;
+
+  // // Create wrench and friction cone
+  // crocoddyl::WrenchCone wrench_cone(R, mu, box, nf, inner_appr, 0., 100.);
+  // crocoddyl::CoPSupport cop_support(R, box);
+
+  // BOOST_CHECK((wrench_cone.get_R() - cop_support.get_R()).isMuchSmallerThan(1.0, 1e-9));
+  // for (std::size_t i = 0; i < 4; ++i) {
+  //   BOOST_CHECK((wrench_cone.get_A().row(nf + i + 1).head(3) - cop_support.get_A().row(i)).isMuchSmallerThan(1.0, 1e-9));
+  // }
+  // BOOST_CHECK((wrench_cone.get_lb().segment(nf + 1, 4) - cop_support.get_lb()).isMuchSmallerThan(1.0, 1e-9));
+  // BOOST_CHECK((wrench_cone.get_ub().segment(nf + 1, 4) - cop_support.get_ub()).isMuchSmallerThan(1.0, 1e-9));
 }
 
 void test_force_along_wrench_cone_normal() {
@@ -191,7 +214,8 @@ void test_force_parallel_to_wrench_cone_normal() {
 
 void register_unit_tests() {
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_constructor)));
-  framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_againts_friction_cone)));
+  framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_against_friction_cone)));
+  framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_against_cop_support)));
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_force_along_wrench_cone_normal)));
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_negative_force_along_wrench_cone_normal)));
   framework::master_test_suite().add(BOOST_TEST_CASE(boost::bind(&test_force_parallel_to_wrench_cone_normal)));
