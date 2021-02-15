2676dd1659c8351a9f37922a4551a27aec9b1f83
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-02-15 10:24:01 +0000

    [cleanup] Removed useless declaration of objects in cost factory

--- unittest/factory/cost.cpp
+++ unittest/factory/cost.cpp
@@ -152,12 +152,10 @@ boost::shared_ptr<crocoddyl::CostModelAbstract> CostModelFactory::create(CostMod
 
   boost::shared_ptr<crocoddyl::ActuationModelFull> actuation =
       boost::make_shared<crocoddyl::ActuationModelFull>(state);
-
-  crocoddyl::FrameIndex frame_index = state->get_pinocchio()->frames.size() - 1;
-  pinocchio::SE3 frame_SE3 = pinocchio::SE3::Random();
   if (nu == std::numeric_limits<std::size_t>::max()) {
     nu = state->get_nv();
   }
+
   switch (cost_type) {
     case CostModelNoFFTypes::CostModelControlGrav:
       cost = boost::make_shared<crocoddyl::CostModelControlGrav>(
