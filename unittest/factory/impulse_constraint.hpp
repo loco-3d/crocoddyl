3eff16c5bb6fbea43e7da88565d41af3c66a7ada
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-03-31 16:04:23 +0100

    [unittest] Added the impulse constraint unittests

---
+++ unittest/factory/impulse_constraint.hpp
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2021, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef CROCODDYL_IMPULSE_CONSTRAINT_FACTORY_HPP_
+#define CROCODDYL_IMPULSE_CONSTRAINT_FACTORY_HPP_
+
+#include "state.hpp"
+#include "actuation.hpp"
+#include "activation.hpp"
+#include "crocoddyl/core/action-base.hpp"
+#include "crocoddyl/core/numdiff/action.hpp"
+
+namespace crocoddyl {
+namespace unittest {
+
+struct ImpulseConstraintModelTypes {
+  enum Type {
+    CostModelResidualImpulseCoMEquality,
+    ConstraintModelResidualImpulseForceEquality,
+    ConstraintModelResidualImpulseCoPPositionInequality,
+    ConstraintModelResidualImpulseFrictionConeInequality,
+    ConstraintModelResidualImpulseWrenchConeInequality,
+    NbImpulseConstraintModelTypes
+  };
+  static std::vector<Type> init_all() {
+    std::vector<Type> v;
+    v.clear();
+    for (int i = 0; i < NbImpulseConstraintModelTypes; ++i) {
+      v.push_back((Type)i);
+    }
+    return v;
+  }
+  static const std::vector<Type> all;
+};
+
+std::ostream &operator<<(std::ostream &os, ImpulseConstraintModelTypes::Type type);
+
+class ImpulseConstraintModelFactory {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  typedef crocoddyl::MathBaseTpl<double> MathBase;
+
+  explicit ImpulseConstraintModelFactory();
+  ~ImpulseConstraintModelFactory();
+
+  boost::shared_ptr<crocoddyl::ActionModelAbstract> create(ImpulseConstraintModelTypes::Type constraint_type,
+                                                           PinocchioModelTypes::Type model_type) const;
+};
+
+}  // namespace unittest
+}  // namespace crocoddyl
+
+#endif  // CROCODDYL_IMPULSE_CONSTRAINT_FACTORY_HPP_
