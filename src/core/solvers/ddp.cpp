c1934351e0579ba629d8c172666339c3255d1438
Wolfgang Merkt <w.merkt@gmail.com>
2021-04-27 00:31:44 +0100

    Update copyright header

--- src/core/solvers/ddp.cpp
+++ src/core/solvers/ddp.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2019-2021, LAAS-CNRS, University of Edinburgh
+// Copyright (C) 2019-2021, LAAS-CNRS, University of Edinburgh, University of Oxford
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
