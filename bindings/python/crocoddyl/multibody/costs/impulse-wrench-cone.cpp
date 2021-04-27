8411fd7bae417cf115f57536eb5fd76f89cae053
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-04-27 14:41:50 +0200

    [impulse-wrench-cone] residual as base

--- bindings/python/crocoddyl/multibody/costs/impulse-wrench-cone.cpp
+++ bindings/python/crocoddyl/multibody/costs/impulse-wrench-cone.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2020-2021, University of Edinburgh
+// Copyright (C) 2020-2021, University of Edinburgh, INRIA
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
@@ -19,7 +19,7 @@ void exposeCostImpulseWrenchCone() {  // TODO: Remove once the deprecated update
 
   bp::register_ptr_to_python<boost::shared_ptr<CostModelImpulseWrenchCone> >();
 
-  bp::class_<CostModelImpulseWrenchCone, bp::bases<CostModelAbstract> >(
+  bp::class_<CostModelImpulseWrenchCone, bp::bases<CostModelResidual> >(
       "CostModelImpulseWrenchCone",
       bp::init<boost::shared_ptr<StateMultibody>, boost::shared_ptr<ActivationModelAbstract>, FrameWrenchCone>(
           bp::args("self", "state", "activation", "fref"),
@@ -34,51 +34,8 @@ void exposeCostImpulseWrenchCone() {  // TODO: Remove once the deprecated update
           "crocoddyl.ActivationModelQuad(6).\n"
           ":param state: state of the multibody system\n"
           ":param fref: frame Wrench cone"))
-      .def<void (CostModelImpulseWrenchCone::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                                const Eigen::Ref<const Eigen::VectorXd>&,
-                                                const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calc", &CostModelImpulseWrenchCone::calc, bp::args("self", "data", "x", "u"),
-          "Compute the impulse Wrench cost.\n\n"
-          ":param data: cost data\n"
-          ":param x: time-discrete state vector\n"
-          ":param u: time-discrete control input")
-      .def<void (CostModelImpulseWrenchCone::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                                const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calc", &CostModelAbstract::calc, bp::args("self", "data", "x"))
-      .def<void (CostModelImpulseWrenchCone::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                                const Eigen::Ref<const Eigen::VectorXd>&,
-                                                const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calcDiff", &CostModelImpulseWrenchCone::calcDiff, bp::args("self", "data", "x", "u"),
-          "Compute the derivatives of the impulse Wrench cone cost.\n\n"
-          "It assumes that calc has been run first.\n"
-          ":param data: action data\n"
-          ":param x: time-discrete state vector\n"
-          ":param u: time-discrete control input\n")
-      .def<void (CostModelImpulseWrenchCone::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                                const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calcDiff", &CostModelAbstract::calcDiff, bp::args("self", "data", "x"))
-      .def("createData", &CostModelImpulseWrenchCone::createData, bp::with_custodian_and_ward_postcall<0, 2>(),
-           bp::args("self", "data"),
-           "Create the impulse Wrench cone cost data.\n\n"
-           "Each cost model has its own data that needs to be allocated. This function\n"
-           "returns the allocated data for a predefined cost.\n"
-           ":param data: shared data\n"
-           ":return cost data.")
       .add_property("reference", &CostModelImpulseWrenchCone::get_reference<FrameWrenchCone>,
                     &CostModelImpulseWrenchCone::set_reference<FrameWrenchCone>, "reference frame Wrench cone");
-
-  bp::register_ptr_to_python<boost::shared_ptr<CostDataImpulseWrenchCone> >();
-
-  bp::class_<CostDataImpulseWrenchCone, bp::bases<CostDataAbstract> >(
-      "CostDataImpulseWrenchCone", "Data for impulse Wrench cone cost.\n\n",
-      bp::init<CostModelImpulseWrenchCone*, DataCollectorAbstract*>(
-          bp::args("self", "model", "data"),
-          "Create impulse Wrench cone cost data.\n\n"
-          ":param model: impulse Wrench cone cost model\n"
-          ":param data: shared data")[bp::with_custodian_and_ward<1, 2, bp::with_custodian_and_ward<1, 3> >()])
-      .add_property("Arr_Rx", bp::make_getter(&CostDataImpulseWrenchCone::Arr_Rx, bp::return_internal_reference<>()),
-                    bp::make_setter(&CostDataImpulseWrenchCone::Arr_Rx),
-                    "Intermediate product of Arr (2nd deriv of Activation) with Rx (deriv of residue)");
 
 #pragma GCC diagnostic pop
 }
--- include/crocoddyl/multibody/costs/impulse-wrench-cone.hpp
+++ include/crocoddyl/multibody/costs/impulse-wrench-cone.hpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2020-2021, University of Edinburgh
+// Copyright (C) 2020-2021, University of Edinburgh, INRIA
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
@@ -10,7 +10,7 @@
 #define CROCODDYL_MULTIBODY_COSTS_IMPULSE_WRENCH_CONE_HPP_
 
 #include "crocoddyl/multibody/fwd.hpp"
-#include "crocoddyl/core/cost-base.hpp"
+#include "crocoddyl/core/costs/residual.hpp"
 #include "crocoddyl/multibody/states/multibody.hpp"
 #include "crocoddyl/multibody/residuals/contact-wrench-cone.hpp"
 #include "crocoddyl/multibody/frames.hpp"
@@ -19,19 +19,16 @@
 namespace crocoddyl {
 
 template <typename _Scalar>
-class CostModelImpulseWrenchConeTpl : public CostModelAbstractTpl<_Scalar> {
+class CostModelImpulseWrenchConeTpl : public CostModelResidualTpl<_Scalar> {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   typedef _Scalar Scalar;
   typedef MathBaseTpl<Scalar> MathBase;
-  typedef CostModelAbstractTpl<Scalar> Base;
-  typedef CostDataImpulseWrenchConeTpl<Scalar> Data;
+  typedef CostModelResidualTpl<Scalar> Base;
   typedef StateMultibodyTpl<Scalar> StateMultibody;
-  typedef CostDataAbstractTpl<Scalar> CostDataAbstract;
   typedef ActivationModelAbstractTpl<Scalar> ActivationModelAbstract;
   typedef ResidualModelContactWrenchConeTpl<Scalar> ResidualModelContactWrenchCone;
-  typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
   typedef FrameWrenchConeTpl<Scalar> FrameWrenchCone;
   typedef typename MathBase::VectorXs VectorXs;
   typedef typename MathBase::MatrixXs MatrixXs;
@@ -41,12 +38,6 @@ class CostModelImpulseWrenchConeTpl : public CostModelAbstractTpl<_Scalar> {
                                 boost::shared_ptr<ActivationModelAbstract> activation, const FrameWrenchCone& fref);
   CostModelImpulseWrenchConeTpl(boost::shared_ptr<StateMultibody> state, const FrameWrenchCone& fref);
   virtual ~CostModelImpulseWrenchConeTpl();
-
-  virtual void calc(const boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
-                    const Eigen::Ref<const VectorXs>& u);
-  virtual void calcDiff(const boost::shared_ptr<CostDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
-                        const Eigen::Ref<const VectorXs>& u);
-  virtual boost::shared_ptr<CostDataAbstract> createData(DataCollectorAbstract* const data);
 
  protected:
   virtual void set_referenceImpl(const std::type_info& ti, const void* pv);
@@ -58,35 +49,6 @@ class CostModelImpulseWrenchConeTpl : public CostModelAbstractTpl<_Scalar> {
 
  private:
   FrameWrenchCone fref_;
-};
-
-template <typename _Scalar>
-struct CostDataImpulseWrenchConeTpl : public CostDataAbstractTpl<_Scalar> {
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-  typedef _Scalar Scalar;
-  typedef MathBaseTpl<Scalar> MathBase;
-  typedef CostDataAbstractTpl<Scalar> Base;
-  typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
-  typedef ImpulseModelMultipleTpl<Scalar> ImpulseModelMultiple;
-  typedef FrameWrenchConeTpl<Scalar> FrameWrenchCone;
-  typedef StateMultibodyTpl<Scalar> StateMultibody;
-  typedef typename MathBase::MatrixXs MatrixXs;
-  typedef typename MathBase::Matrix6xs Matrix6xs;
-
-  template <template <typename Scalar> class Model>
-  CostDataImpulseWrenchConeTpl(Model<Scalar>* const model, DataCollectorAbstract* const data)
-      : Base(model, data), Arr_Rx(model->get_residual()->get_nr(), model->get_state()->get_ndx()) {
-    Arr_Rx.setZero();
-  }
-
-  MatrixXs Arr_Rx;
-  using Base::activation;
-  using Base::cost;
-  using Base::Lx;
-  using Base::Lxx;
-  using Base::residual;
-  using Base::shared;
 };
 
 }  // namespace crocoddyl
