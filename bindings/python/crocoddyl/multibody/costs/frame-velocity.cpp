80a0d4e33dd4dd2f72236fef62e053aa166f25f2
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-04-27 12:27:46 +0200

    [bindings][framevelocity]

--- bindings/python/crocoddyl/multibody/costs/frame-velocity.cpp
+++ bindings/python/crocoddyl/multibody/costs/frame-velocity.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2019-2021, LAAS-CNRS, University of Edinburgh
+// Copyright (C) 2019-2021, LAAS-CNRS, University of Edinburgh, INRIA
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
@@ -20,7 +20,7 @@ void exposeCostFrameVelocity() {  // TODO: Remove once the deprecated update cal
 
   bp::register_ptr_to_python<boost::shared_ptr<CostModelFrameVelocity> >();
 
-  bp::class_<CostModelFrameVelocity, bp::bases<CostModelAbstract> >(
+  bp::class_<CostModelFrameVelocity, bp::bases<CostModelResidual> >(
       "CostModelFrameVelocity",
       "This cost function defines a residual vector as r = v - vref, with v and vref as the current and reference "
       "frame velocities, respectively.",
@@ -52,36 +52,6 @@ void exposeCostFrameVelocity() {  // TODO: Remove once the deprecated update cal
           "state.nv.\n"
           ":param state: state of the multibody system\n"
           ":param vref: reference frame velocity"))
-      .def<void (CostModelFrameVelocity::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                            const Eigen::Ref<const Eigen::VectorXd>&,
-                                            const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calc", &CostModelFrameVelocity::calc, bp::args("self", "data", "x", "u"),
-          "Compute the frame velocity cost.\n\n"
-          ":param data: cost data\n"
-          ":param x: time-discrete state vector\n"
-          ":param u: time-discrete control input")
-      .def<void (CostModelFrameVelocity::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                            const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calc", &CostModelAbstract::calc, bp::args("self", "data", "x"))
-      .def<void (CostModelFrameVelocity::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                            const Eigen::Ref<const Eigen::VectorXd>&,
-                                            const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calcDiff", &CostModelFrameVelocity::calcDiff, bp::args("self", "data", "x", "u"),
-          "Compute the derivatives of the frame velocity cost.\n\n"
-          "It assumes that calc has been run first.\n"
-          ":param data: action data\n"
-          ":param x: time-discrete state vector\n"
-          ":param u: time-discrete control input\n")
-      .def<void (CostModelFrameVelocity::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                            const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calcDiff", &CostModelAbstract::calcDiff, bp::args("self", "data", "x"))
-      .def("createData", &CostModelFrameVelocity::createData, bp::with_custodian_and_ward_postcall<0, 2>(),
-           bp::args("self", "data"),
-           "Create the frame velocity cost data.\n\n"
-           "Each cost model has its own data that needs to be allocated. This function\n"
-           "returns the allocated data for a predefined cost.\n"
-           ":param data: shared data\n"
-           ":return cost data.")
       .add_property("reference", &CostModelFrameVelocity::get_reference<FrameMotion>,
                     &CostModelFrameVelocity::set_reference<FrameMotion>, "reference frame velocity")
       .add_property("vref",
@@ -90,16 +60,6 @@ void exposeCostFrameVelocity() {  // TODO: Remove once the deprecated update cal
                     bp::make_function(&CostModelFrameVelocity::set_reference<FrameMotion>,
                                       deprecated<>("Deprecated. Use reference.")),
                     "reference frame velocity");
-
-  bp::register_ptr_to_python<boost::shared_ptr<CostDataFrameVelocity> >();
-
-  bp::class_<CostDataFrameVelocity, bp::bases<CostDataAbstract> >(
-      "CostDataFrameVelocity", "Data for frame velocity cost.\n\n",
-      bp::init<CostModelFrameVelocity*, DataCollectorAbstract*>(
-          bp::args("self", "model", "data"),
-          "Create frame velocity cost data.\n\n"
-          ":param model: frame Velocity cost model\n"
-          ":param data: shared data")[bp::with_custodian_and_ward<1, 2, bp::with_custodian_and_ward<1, 3> >()]);
 
 #pragma GCC diagnostic pop
 }
