822b13a2e260246a4d5e64773688c9b4d8627d6d
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-04-27 12:18:09 +0200

    [bindings][frameplacement] use costmodelresidual as base

--- bindings/python/crocoddyl/multibody/costs/frame-placement.cpp
+++ bindings/python/crocoddyl/multibody/costs/frame-placement.cpp
@@ -20,7 +20,7 @@ void exposeCostFramePlacement() {  // TODO: Remove once the deprecated update ca
 
   bp::register_ptr_to_python<boost::shared_ptr<CostModelFramePlacement> >();
 
-  bp::class_<CostModelFramePlacement, bp::bases<CostModelAbstract> >(
+  bp::class_<CostModelFramePlacement, bp::bases<CostModelResidual> >(
       "CostModelFramePlacement",
       "This cost function defines a residual vector as r = p - pref, with p and pref as the current and reference "
       "frame placements, respectively.",
@@ -52,36 +52,6 @@ void exposeCostFramePlacement() {  // TODO: Remove once the deprecated update ca
           "state.nv.\n"
           ":param state: state of the multibody system\n"
           ":param Mref: reference frame placement"))
-      .def<void (CostModelFramePlacement::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                             const Eigen::Ref<const Eigen::VectorXd>&,
-                                             const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calc", &CostModelFramePlacement::calc, bp::args("self", "data", "x", "u"),
-          "Compute the frame placement cost.\n\n"
-          ":param data: cost data\n"
-          ":param x: time-discrete state vector\n"
-          ":param u: time-discrete control input")
-      .def<void (CostModelFramePlacement::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                             const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calc", &CostModelAbstract::calc, bp::args("self", "data", "x"))
-      .def<void (CostModelFramePlacement::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                             const Eigen::Ref<const Eigen::VectorXd>&,
-                                             const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calcDiff", &CostModelFramePlacement::calcDiff, bp::args("self", "data", "x", "u"),
-          "Compute the derivatives of the frame placement cost.\n\n"
-          "It assumes that calc has been run first.\n"
-          ":param data: action data\n"
-          ":param x: time-discrete state vector\n"
-          ":param u: time-discrete control input\n")
-      .def<void (CostModelFramePlacement::*)(const boost::shared_ptr<CostDataAbstract>&,
-                                             const Eigen::Ref<const Eigen::VectorXd>&)>(
-          "calcDiff", &CostModelAbstract::calcDiff, bp::args("self", "data", "x"))
-      .def("createData", &CostModelFramePlacement::createData, bp::with_custodian_and_ward_postcall<0, 2>(),
-           bp::args("self", "data"),
-           "Create the frame placement cost data.\n\n"
-           "Each cost model has its own data that needs to be allocated. This function\n"
-           "returns the allocated data for a predefined cost.\n"
-           ":param data: shared data\n"
-           ":return cost data.")
       .add_property("reference", &CostModelFramePlacement::get_reference<FramePlacement>,
                     &CostModelFramePlacement::set_reference<FramePlacement>, "reference frame placement")
       .add_property("Mref",
@@ -90,16 +60,6 @@ void exposeCostFramePlacement() {  // TODO: Remove once the deprecated update ca
                     bp::make_function(&CostModelFramePlacement::set_reference<FramePlacement>,
                                       deprecated<>("Deprecated. Use reference.")),
                     "reference frame placement");
-
-  bp::register_ptr_to_python<boost::shared_ptr<CostDataFramePlacement> >();
-
-  bp::class_<CostDataFramePlacement, bp::bases<CostDataAbstract> >(
-      "CostDataFramePlacement", "Data for frame placement cost.\n\n",
-      bp::init<CostModelFramePlacement*, DataCollectorAbstract*>(
-          bp::args("self", "model", "data"),
-          "Create frame placement cost data.\n\n"
-          ":param model: frame placement cost model\n"
-          ":param data: shared data")[bp::with_custodian_and_ward<1, 2, bp::with_custodian_and_ward<1, 3> >()]);
 
 #pragma GCC diagnostic pop
 }
