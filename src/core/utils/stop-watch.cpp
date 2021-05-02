5de86d2d7f717593de792a0f3c2d8ff00ee3cbf6
Wolfgang Merkt <w.merkt@gmail.com>
2021-05-02 15:51:52 +0100

    Remove Stdafx.hh

--- src/core/utils/stop-watch.cpp
+++ src/core/utils/stop-watch.cpp
@@ -21,8 +21,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ///////////////////////////////////////////////////////////////////////////////
 
-#include "crocoddyl/core/utils/Stdafx.hh"
-
 #ifndef WIN32
 #include <sys/time.h>
 #else
