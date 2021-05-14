5e240e520691bddf0f789f3f5620134f131a51fc
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-05-14 12:26:04 +0100

    [ci] No needed anymore to pass c++11 flag in Travis

--- .travis/build-and-test.sh
+++ .travis/build-and-test.sh
@@ -5,12 +5,7 @@ set -xe
 if [ $CHECK_CLANG_FORMAT ]; then exit 0; fi
 
 mkdir _build ; cd _build
-if [ -z $CMAKE_CXX_STANDARD ]
-then
-  cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
-else
-  cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD
-fi
+cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
 
 make -j1
 make test
