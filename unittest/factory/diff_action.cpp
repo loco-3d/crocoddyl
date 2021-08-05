1a450c4ed79de90de84a5c95996e4122df1c5d0a
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-08-05 16:59:49 +0200

    [unittest][constraint-dynamics] missing curlies for switch

--- unittest/factory/diff_action.cpp
+++ unittest/factory/diff_action.cpp
@@ -287,48 +287,49 @@ DifferentialActionModelFactory::create_constraintFwdDynamics(StateModelTypes::Ty
   actuation = ActuationModelFactory().create(actuation_type, state_type);
 
   cost = boost::make_shared<crocoddyl::CostModelSum>(state, actuation->get_nu());
-
+  const pinocchio::Model& pin_model = *(state->get_pinocchio());
+  
   switch (state_type) {
-    case StateModelTypes::StateMultibody_TalosArm:
-      const pinocchio::Model& pin_model = *(state->get_pinocchio());
-      const pinocchio::Frame& frame_lf = pin_model.frames[pin_model.getFrameId("gripper_left_fingertip_1_link")];
-      contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
-							frame_lf.parent,
-							frame_lf.placement));
-      break;
-    case StateModelTypes::StateMultibody_HyQ:
-      const pinocchio::Model& pin_model = *(state->get_pinocchio());
-      const pinocchio::Frame& frame_lf = pin_model.frames[pin_model.getFrameId("lf_foot")];
-      const pinocchio::Frame& frame_rf = pin_model.frames[pin_model.getFrameId("rf_foot")];
-      const pinocchio::Frame& frame_lh = pin_model.frames[pin_model.getFrameId("lh_foot")];
-      const pinocchio::Frame& frame_rh = pin_model.frames[pin_model.getFrameId("rh_foot")];
-      contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
-							frame_lf.parent,
-							frame_lf.placement));
-      contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
-							frame_rf.parent,
-							frame_rf.placement));
-      contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
-							frame_lh.parent,
-							frame_lh.placement));
-      contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
-							frame_rh.parent,
-							frame_rh.placement));      
-      break;
-    case StateModelTypes::StateMultibody_Talos:
-      const pinocchio::Model& pin_model = *(state->get_pinocchio());
-      const pinocchio::Frame& frame_lf = pin_model.frames[pin_model.getFrameId("left_sole_link")];
-      const pinocchio::Frame& frame_rf = pin_model.frames[pin_model.getFrameId("right_sole_link")];
-      contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_6D,
-							frame_lf.parent,
-							frame_lf.placement));
-      contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_6D,
-							frame_rf.parent,
-							frame_rf.placement));      
-      break;
-    default:
-      throw_pretty(__FILE__ ": Wrong StateModelTypes::Type given");
-      break;
+  case StateModelTypes::StateMultibody_TalosArm: {
+    const pinocchio::Frame& frame_lf1 = pin_model.frames[pin_model.getFrameId("gripper_left_fingertip_1_link")];
+    contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
+						      frame_lf1.parent,
+						      frame_lf1.placement));
+    break;
+  }
+  case StateModelTypes::StateMultibody_HyQ: {
+    const pinocchio::Frame& frame_lf2 = pin_model.frames[pin_model.getFrameId("lf_foot")];
+    const pinocchio::Frame& frame_rf2 = pin_model.frames[pin_model.getFrameId("rf_foot")];
+    const pinocchio::Frame& frame_lh2 = pin_model.frames[pin_model.getFrameId("lh_foot")];
+    const pinocchio::Frame& frame_rh2 = pin_model.frames[pin_model.getFrameId("rh_foot")];
+    contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
+						      frame_lf2.parent,
+						      frame_lf2.placement));
+    contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
+						      frame_rf2.parent,
+						      frame_rf2.placement));
+    contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
+						      frame_lh2.parent,
+						      frame_lh2.placement));
+    contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_3D,
+						      frame_rh2.parent,
+						      frame_rh2.placement));      
+    break;
+  }
+  case StateModelTypes::StateMultibody_Talos: {
+    const pinocchio::Frame& frame_lf3 = pin_model.frames[pin_model.getFrameId("left_sole_link")];
+    const pinocchio::Frame& frame_rf3 = pin_model.frames[pin_model.getFrameId("right_sole_link")];
+    contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_6D,
+						      frame_lf3.parent,
+						      frame_lf3.placement));
+    contact.push_back(pinocchio::RigidConstraintModel(pinocchio::CONTACT_6D,
+						      frame_rf3.parent,
+						      frame_rf3.placement));      
+    break; }
+  default: {
+    throw_pretty(__FILE__ ": Wrong StateModelTypes::Type given");
+    break;
+  }
   }
   cost->addCost("state",
                 CostModelFactory().create(CostModelTypes::CostModelResidualState, state_type,
