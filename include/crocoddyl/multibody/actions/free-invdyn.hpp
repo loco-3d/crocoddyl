84781e74b80508ab1423d437c324d39a7126a342
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-08-17 21:14:20 +0100

    [action] First complete version of the free invdyn class and its RNEA residual

--- include/crocoddyl/multibody/actions/free-invdyn.hpp
+++ include/crocoddyl/multibody/actions/free-invdyn.hpp
@@ -15,12 +15,11 @@
 #include "crocoddyl/core/diff-action-base.hpp"
 #include "crocoddyl/core/costs/cost-sum.hpp"
 #include "crocoddyl/core/constraints/constraint-manager.hpp"
-#include "crocoddyl/core/actuation-base.hpp"
-#include "crocoddyl/multibody/data/multibody.hpp"
 #include "crocoddyl/multibody/states/multibody.hpp"
-#include "crocoddyl/core/utils/exception.hpp"
+#include "crocoddyl/core/actuation-base.hpp"
 #include "crocoddyl/core/residual-base.hpp"
-#include "crocoddyl/core/constraints/residual.hpp"
+#include "crocoddyl/multibody/data/multibody.hpp"
+#include "crocoddyl/core/utils/exception.hpp"
 
 namespace crocoddyl {
 
@@ -30,22 +29,24 @@ class DifferentialActionModelFreeInvDynamicsTpl : public DifferentialActionModel
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   typedef _Scalar Scalar;
+  typedef MathBaseTpl<Scalar> MathBase;
   typedef DifferentialActionModelAbstractTpl<Scalar> Base;
   typedef DifferentialActionDataFreeInvDynamicsTpl<Scalar> Data;
   typedef DifferentialActionDataAbstractTpl<Scalar> DifferentialActionDataAbstract;
-  typedef StateMultibodyTpl<Scalar> StateMultibody;
   typedef CostModelSumTpl<Scalar> CostModelSum;
   typedef ConstraintModelManagerTpl<Scalar> ConstraintModelManager;
+  typedef StateMultibodyTpl<Scalar> StateMultibody;
   typedef ActuationModelAbstractTpl<Scalar> ActuationModelAbstract;
   typedef ConstraintModelResidualTpl<Scalar> ConstraintModelResidual;
-  typedef MathBaseTpl<Scalar> MathBase;
   typedef typename MathBase::VectorXs VectorXs;
-  typedef typename MathBase::MatrixXs MatrixXs;
 
   DifferentialActionModelFreeInvDynamicsTpl(boost::shared_ptr<StateMultibody> state,
                                             boost::shared_ptr<ActuationModelAbstract> actuation,
+                                            boost::shared_ptr<CostModelSum> costs);
+  DifferentialActionModelFreeInvDynamicsTpl(boost::shared_ptr<StateMultibody> state,
+                                            boost::shared_ptr<ActuationModelAbstract> actuation,
                                             boost::shared_ptr<CostModelSum> costs,
-                                            boost::shared_ptr<ConstraintModelManager> constraints = nullptr);
+                                            boost::shared_ptr<ConstraintModelManager> constraints);
   virtual ~DifferentialActionModelFreeInvDynamicsTpl();
 
   virtual void calc(const boost::shared_ptr<DifferentialActionDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
@@ -62,37 +63,7 @@ class DifferentialActionModelFreeInvDynamicsTpl : public DifferentialActionModel
   const boost::shared_ptr<ActuationModelAbstract>& get_actuation() const;
   const boost::shared_ptr<CostModelSum>& get_costs() const;
   const boost::shared_ptr<ConstraintModelManager>& get_constraints() const;
-
-  class ResidualModelRneaTpl : public ResidualModelAbstractTpl<_Scalar> {
-   public:
-    typedef _Scalar Scalar;
-    typedef MathBaseTpl<Scalar> MathBase;
-    typedef StateMultibodyTpl<Scalar> StateMultibody;
-    typedef ResidualDataAbstractTpl<Scalar> ResidualDataAbstract;
-    typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
-    typedef typename MathBase::Vector3s Vector3s;
-    typedef typename MathBase::VectorXs VectorXs;
-
-    ResidualModelRneaTpl(boost::shared_ptr<StateMultibody> state, const std::size_t nu);
-    virtual ~ResidualModelRneaTpl();
-
-    virtual void calc(const boost::shared_ptr<ResidualDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
-                      const Eigen::Ref<const VectorXs>& u);
-
-    virtual void calcDiff(const boost::shared_ptr<ResidualDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
-                          const Eigen::Ref<const VectorXs>& u);
-
-    virtual boost::shared_ptr<ResidualDataAbstract> createData(DataCollectorAbstract* const data);
-
-    virtual void print(std::ostream& os) const;
-
-   protected:
-    using Base::nu_;
-    using Base::state_;
-    using Base::u_dependent_;
-    using Base::unone_;
-    using Base::v_dependent_;
-  };
+  pinocchio::ModelTpl<Scalar>& get_pinocchio() const;
 
   /**
    * @brief Print relevant information of the free forward-dynamics model
@@ -112,8 +83,58 @@ class DifferentialActionModelFreeInvDynamicsTpl : public DifferentialActionModel
   boost::shared_ptr<CostModelSum> costs_;
   boost::shared_ptr<ConstraintModelManager> constraints_;
   pinocchio::ModelTpl<Scalar>& pinocchio_;
-  bool without_armature_;
-  VectorXs armature_;
+
+ public:
+  class ResidualModelRneaTpl : public ResidualModelAbstractTpl<_Scalar> {
+   public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    typedef _Scalar Scalar;
+    typedef MathBaseTpl<Scalar> MathBase;
+    typedef ResidualModelAbstractTpl<Scalar> Base;
+    typedef StateMultibodyTpl<Scalar> StateMultibody;
+    typedef ResidualDataAbstractTpl<Scalar> ResidualDataAbstract;
+    typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
+    typedef typename MathBase::VectorXs VectorXs;
+
+    ResidualModelRneaTpl(boost::shared_ptr<StateMultibody> state, const std::size_t nu)
+        : Base(state, state->get_nv(), state->get_nv() + nu, true, true, true), na_(nu) {}
+    virtual ~ResidualModelRneaTpl() {}
+
+    virtual void calc(const boost::shared_ptr<ResidualDataAbstract>& data, const Eigen::Ref<const VectorXs>&,
+                      const Eigen::Ref<const VectorXs>&) {
+      const boost::shared_ptr<typename Data::ResidualDataRneaTpl>& d =
+          boost::static_pointer_cast<typename Data::ResidualDataRneaTpl>(data);
+      data->r = d->pinocchio->tau - d->actuation->tau;
+    }
+
+    virtual void calcDiff(const boost::shared_ptr<ResidualDataAbstract>& data, const Eigen::Ref<const VectorXs>&,
+                          const Eigen::Ref<const VectorXs>&) {
+      const boost::shared_ptr<typename Data::ResidualDataRneaTpl>& d =
+          boost::static_pointer_cast<typename Data::ResidualDataRneaTpl>(data);
+      const std::size_t nv = state_->get_nv();
+      data->Rx.leftCols(nv) = d->pinocchio->dtau_dq;
+      data->Rx.rightCols(na_) = d->pinocchio->dtau_dv;
+      data->Rx -= d->actuation->dtau_dx;
+      data->Ru.leftCols(nv) = d->pinocchio->M;
+      data->Ru.rightCols(na_) = d->actuation->dtau_du;
+    }
+
+    virtual boost::shared_ptr<ResidualDataAbstract> createData(DataCollectorAbstract* const data) {
+      return boost::allocate_shared<typename Data::ResidualDataRneaTpl>(
+          Eigen::aligned_allocator<typename Data::ResidualDataRneaTpl>(), this, data);
+    }
+
+    virtual void print(std::ostream& os) const {
+      os << "ResidualModelRnea {nx=" << state_->get_nx() << ", ndx=" << state_->get_ndx() << ", nu=" << nu_
+         << ", na=" << na_ << "}";
+    }
+
+   protected:
+    std::size_t na_;
+    using Base::nu_;
+    using Base::state_;
+  };
 };
 
 template <typename _Scalar>
@@ -123,7 +144,6 @@ struct DifferentialActionDataFreeInvDynamicsTpl : public DifferentialActionDataA
   typedef MathBaseTpl<Scalar> MathBase;
   typedef DifferentialActionDataAbstractTpl<Scalar> Base;
   typedef typename MathBase::VectorXs VectorXs;
-  typedef typename MathBase::MatrixXs MatrixXs;
 
   template <template <typename Scalar> class Model>
   explicit DifferentialActionDataFreeInvDynamicsTpl(Model<Scalar>* const model)
@@ -131,52 +151,20 @@ struct DifferentialActionDataFreeInvDynamicsTpl : public DifferentialActionDataA
         pinocchio(pinocchio::DataTpl<Scalar>(model->get_pinocchio())),
         multibody(&pinocchio, model->get_actuation()->createData()),
         costs(model->get_costs()->createData(&multibody)),
-        dtau_dx(model->get_nu(), model->get_state()->get_ndx()),
+        constraints(model->get_constraints()->createData(&multibody)),
         tmp_xstatic(model->get_state()->get_nx()),
         tmp_ustatic(model->get_nu()) {
     costs->shareMemory(this);
-    if (model->get_constraints() != nullptr) {
-      constraints = model->get_constraints()->createData(&multibody);
-      constraints->shareMemory(this);
-    }
-    dtau_dx.setZero();
+    constraints->shareMemory(this);
+    Fu.leftCols(model->get_state()->get_nv()).diagonal().array() = 1;
     tmp_xstatic.setZero();
     tmp_ustatic.setZero();
   }
-
-  struct ResidualDataRneaTpl : public ResidualDataAbstractTpl<_Scalar> {
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-    typedef _Scalar Scalar;
-    typedef MathBaseTpl<Scalar> MathBase;
-    typedef ResidualDataAbstractTpl<Scalar> rneaBase;
-    typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
-    typedef typename MathBase::Matrix3xs Matrix3xs;
-
-    template <template <typename Scalar> class Model>
-    ResidualDataRneaTpl(Model<Scalar>* const model, DataCollectorAbstract* const data) : rneaBase(model, data) {
-      // Check that proper shared data has been passed
-      DataCollectorMultibodyTpl<Scalar>* d = dynamic_cast<DataCollectorMultibodyTpl<Scalar>*>(shared);
-      if (d == NULL) {
-        throw_pretty("Invalid argument: the shared data should be derived from DataCollectorMultibody");
-      }
-
-      // Avoids data casting at runtime
-      pinocchio = d->pinocchio;
-    }
-
-    pinocchio::DataTpl<Scalar>* pinocchio;  //!< Pinocchio data
-    using Base::r;
-    using Base::Ru;
-    using Base::Rx;
-    using Base::shared;
-  };
 
   pinocchio::DataTpl<Scalar> pinocchio;
   DataCollectorActMultibodyTpl<Scalar> multibody;
   boost::shared_ptr<CostDataSumTpl<Scalar> > costs;
   boost::shared_ptr<ConstraintDataManagerTpl<Scalar> > constraints;
-  MatrixXs dtau_dx;
   VectorXs tmp_xstatic;
   VectorXs tmp_ustatic;
   using Base::cost;
@@ -189,6 +177,36 @@ struct DifferentialActionDataFreeInvDynamicsTpl : public DifferentialActionDataA
   using Base::Lxx;
   using Base::r;
   using Base::xout;
+
+  struct ResidualDataRneaTpl : public ResidualDataAbstractTpl<_Scalar> {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    typedef _Scalar Scalar;
+    typedef MathBaseTpl<Scalar> MathBase;
+    typedef ResidualDataAbstractTpl<Scalar> Base;
+    typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
+
+    template <template <typename Scalar> class Model>
+    ResidualDataRneaTpl(Model<Scalar>* const model, DataCollectorAbstract* const data) : Base(model, data) {
+      // Check that proper shared data has been passed
+      DataCollectorActMultibodyTpl<Scalar>* d = dynamic_cast<DataCollectorActMultibodyTpl<Scalar>*>(shared);
+      if (d == NULL) {
+        throw_pretty("Invalid argument: the shared data should be derived from DataCollectorActMultibody");
+      }
+      Ru.rightCols(model->get_state()->get_nv()) = -d->actuation->dtau_du;
+
+      // Avoids data casting at runtime
+      pinocchio = d->pinocchio;
+      actuation = d->actuation.get();
+    }
+
+    pinocchio::DataTpl<Scalar>* pinocchio;        //!< Pinocchio data
+    ActuationDataAbstractTpl<Scalar>* actuation;  //!< Actuation data
+    using Base::r;
+    using Base::Ru;
+    using Base::Rx;
+    using Base::shared;
+  };
 };
 
 }  // namespace crocoddyl
--- include/crocoddyl/multibody/actions/free-invdyn.hxx
+++ include/crocoddyl/multibody/actions/free-invdyn.hxx
@@ -6,18 +6,43 @@
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
 
-#include "crocoddyl/core/utils/exception.hpp"
-#include "crocoddyl/multibody/actions/free-invdyn.hpp"
-
 #include <pinocchio/algorithm/rnea.hpp>
 #include <pinocchio/algorithm/rnea-derivatives.hpp>
-#include <pinocchio/algorithm/compute-all-terms.hpp>
 #include <pinocchio/algorithm/kinematics.hpp>
 #include <pinocchio/algorithm/jacobian.hpp>
-#include <pinocchio/algorithm/frames.hpp>
-#include <pinocchio/algorithm/cholesky.hpp>
+
+#include "crocoddyl/multibody/actions/free-invdyn.hpp"
+#include "crocoddyl/core/constraints/residual.hpp"
 
 namespace crocoddyl {
+
+template <typename Scalar>
+DifferentialActionModelFreeInvDynamicsTpl<Scalar>::DifferentialActionModelFreeInvDynamicsTpl(
+    boost::shared_ptr<StateMultibody> state, boost::shared_ptr<ActuationModelAbstract> actuation,
+    boost::shared_ptr<CostModelSum> costs)
+    : Base(state, state->get_nv() + actuation->get_nu(), costs->get_nr(), 0, state->get_nv()),
+      actuation_(actuation),
+      costs_(costs),
+      constraints_(boost::make_shared<ConstraintModelManager>(state, state->get_nv() + actuation->get_nu())),
+      pinocchio_(*state->get_pinocchio().get()) {
+  if (costs_->get_nu() != nu_) {
+    throw_pretty("Invalid argument: "
+                 << "Costs doesn't have the same control dimension (it should be " + std::to_string(nu_) + ")");
+  }
+  const std::size_t nu = actuation_->get_nu();
+  VectorXs lb = VectorXs::Zero(nu_);
+  VectorXs ub = VectorXs::Zero(nu_);
+  lb.tail(nu) = Scalar(-1.) * pinocchio_.effortLimit.tail(nu);
+  ub.tail(nu) = Scalar(1.) * pinocchio_.effortLimit.tail(nu);
+  Base::set_u_lb(lb);
+  Base::set_u_ub(ub);
+
+  constraints_->addConstraint(
+      "rnea",
+      boost::make_shared<ConstraintModelResidual>(
+          state_, boost::make_shared<typename DifferentialActionModelFreeInvDynamicsTpl<Scalar>::ResidualModelRneaTpl>(
+                      state, nu)));
+}
 
 template <typename Scalar>
 DifferentialActionModelFreeInvDynamicsTpl<Scalar>::DifferentialActionModelFreeInvDynamicsTpl(
@@ -25,23 +50,31 @@ DifferentialActionModelFreeInvDynamicsTpl<Scalar>::DifferentialActionModelFreeIn
     boost::shared_ptr<CostModelSum> costs, boost::shared_ptr<ConstraintModelManager> constraints)
     : Base(state, state->get_nv() + actuation->get_nu(), costs->get_nr(), constraints->get_ng(),
            constraints->get_nh() + state->get_nv()),
-      constraints_(constraints),
       actuation_(actuation),
       costs_(costs),
+      constraints_(constraints),
       pinocchio_(*state->get_pinocchio().get()) {
   if (costs_->get_nu() != nu_) {
     throw_pretty("Invalid argument: "
                  << "Costs doesn't have the same control dimension (it should be " + std::to_string(nu_) + ")");
   }
-  Base::set_u_lb(Scalar(-1.) * pinocchio_.effortLimit.tail(nu_));
-  Base::set_u_ub(Scalar(+1.) * pinocchio_.effortLimit.tail(nu_));
+  if (constraints_->get_nu() != nu_) {
+    throw_pretty("Invalid argument: "
+                 << "Constraints doesn't have the same control dimension (it should be " + std::to_string(nu_) + ")");
+  }
+  const std::size_t nu = actuation_->get_nu();
+  VectorXs lb = VectorXs::Zero(nu_);
+  VectorXs ub = VectorXs::Zero(nu_);
+  lb.tail(nu) = Scalar(-1.) * pinocchio_.effortLimit.tail(nu);
+  ub.tail(nu) = Scalar(1.) * pinocchio_.effortLimit.tail(nu);
+  Base::set_u_lb(lb);
+  Base::set_u_ub(ub);
 
   constraints_->addConstraint(
       "rnea",
       boost::make_shared<ConstraintModelResidual>(
-          state_,
-          boost::make_shared<typename DifferentialActionModelFreeInvDynamicsTpl<Scalar>::ResidualModelRneaTpl<Scalar> >(
-              state_, actuation_->get_nu())));
+          state_, boost::make_shared<typename DifferentialActionModelFreeInvDynamicsTpl<Scalar>::ResidualModelRneaTpl>(
+                      state, nu)));
 }
 
 template <typename Scalar>
@@ -59,25 +92,21 @@ void DifferentialActionModelFreeInvDynamicsTpl<Scalar>::calc(
     throw_pretty("Invalid argument: "
                  << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
   }
-
   Data* d = static_cast<Data*>(data.get());
+  const std::size_t nv = state_->get_nv();
   const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> q = x.head(state_->get_nq());
-  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> v = x.tail(state_->get_nv());
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> v = x.tail(nv);
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> a = u.head(nv);
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> tau = u.tail(actuation_->get_nu());
 
-  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> a = x.head(state_->get_nv());
-  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> tau = x.tail(state_->get_nv());
-
-  d->xout = a pinocchio::rnea(pinocchio_, d->pinocchio, q, v, a);
+  d->xout = a;
+  pinocchio::rnea(pinocchio_, d->pinocchio, q, v, a);
   pinocchio::updateGlobalPlacements(pinocchio_, d->pinocchio);
   pinocchio::computeJointJacobians(pinocchio_, d->pinocchio, q);
-  actuation_->calc(d->multibody.actuation, x, u);
-
-  // Computing the cost value and residuals
+  actuation_->calc(d->multibody.actuation, x, tau);
   costs_->calc(d->costs, x, u);
   d->cost = d->costs->cost;
-  if (constraints_ != nullptr) {
-    constraints_->calc(d->constraints, x, u);
-  }
+  constraints_->calc(d->constraints, x, u);
 }
 
 template <typename Scalar>
@@ -92,26 +121,20 @@ void DifferentialActionModelFreeInvDynamicsTpl<Scalar>::calcDiff(
     throw_pretty("Invalid argument: "
                  << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
   }
-  const std::size_t nq = state_->get_nq();
+  Data* d = static_cast<Data*>(data.get());
   const std::size_t nv = state_->get_nv();
   const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> q = x.head(state_->get_nq());
   const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> v = x.tail(nv);
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> a = u.head(nv);
 
-  Data* d = static_cast<Data*>(data.get());
-
+  pinocchio::computeRNEADerivatives(pinocchio_, d->pinocchio, q, v, a);
   actuation_->calcDiff(d->multibody.actuation, x, u);
-
-  pinocchio::computeRNEADerivatives(pinocchio_, d->pinocchio, q, v, d->xout);
-
-  // Computing the cost derivatives
   costs_->calcDiff(d->costs, x, u);
-  if (constraints_ != nullptr) {
-    constraints_->calcDiff(d->constraints, x, u);
-  }
+  constraints_->calcDiff(d->constraints, x, u);
 }
 
 template <typename Scalar>
-boost::shared_ptr<DifferentialActionDataAbstractTpl<Scalar>>
+boost::shared_ptr<DifferentialActionDataAbstractTpl<Scalar> >
 DifferentialActionModelFreeInvDynamicsTpl<Scalar>::createData() {
   return boost::allocate_shared<Data>(Eigen::aligned_allocator<Data>(), this);
 }
@@ -138,25 +161,25 @@ void DifferentialActionModelFreeInvDynamicsTpl<Scalar>::quasiStatic(
     throw_pretty("Invalid argument: "
                  << "x has wrong dimension (it should be " + std::to_string(state_->get_nx()) + ")");
   }
-  // Static casting the data
-  Data* d = static_cast<Data*>(data.get());
-  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> q = x.head(state_->get_nq());
-
   // Check the velocity input is zero
   assert_pretty(x.tail(state_->get_nv()).isZero(), "The velocity input should be zero for quasi-static to work.");
 
-  d->tmp_xstatic.head(state_->get_nq()) = q;
-  d->tmp_xstatic.tail(state_->get_nq()) *= 0;
+  Data* d = static_cast<Data*>(data.get());
+  const std::size_t nq = state_->get_nq();
+  const std::size_t nv = state_->get_nv();
+  const std::size_t nu = actuation_->get_nu();
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> q = x.head(nq);
+
+  d->tmp_xstatic.head(nq) = q;
+  d->tmp_xstatic.tail(nv) *= 0;
   d->tmp_ustatic.setZero();
 
-  pinocchio::rnea(pinocchio_, d->pinocchio, q, VectorXs::Zero(state_->get_nv()), VectorXs::Zero(state_->get_nv()));
-
-  actuation_->calc(d->multibody.actuation, d->tmp_xstatic, VectorXs::Zero(nu_));
-  actuation_->calcDiff(d->multibody.actuation, d->tmp_xstatic, VectorXs::Zero(nu_));
+  pinocchio::rnea(pinocchio_, d->pinocchio, q, d->tmp_xstatic.tail(nv), d->tmp_xstatic.tail(nv));
+  actuation_->calc(d->multibody.actuation, d->tmp_xstatic, d->tmp_ustatic.tail(nu));
+  actuation_->calcDiff(d->multibody.actuation, d->tmp_xstatic, d->tmp_ustatic.tail(nu));
 
   u.noalias() = pseudoInverse(d->multibody.actuation->dtau_du) * d->pinocchio.tau;
   d->pinocchio.tau.setZero();
-  return d->tmp_ustatic
 }
 
 template <typename Scalar>
@@ -166,54 +189,26 @@ void DifferentialActionModelFreeInvDynamicsTpl<Scalar>::print(std::ostream& os) 
 }
 
 template <typename Scalar>
-const boost::shared_ptr<ActuationModelAbstractTpl<Scalar>>&
+const boost::shared_ptr<ActuationModelAbstractTpl<Scalar> >&
 DifferentialActionModelFreeInvDynamicsTpl<Scalar>::get_actuation() const {
   return actuation_;
 }
 
 template <typename Scalar>
-const boost::shared_ptr<CostModelSumTpl<Scalar>>& DifferentialActionModelFreeInvDynamicsTpl<Scalar>::get_costs()
+const boost::shared_ptr<CostModelSumTpl<Scalar> >& DifferentialActionModelFreeInvDynamicsTpl<Scalar>::get_costs()
     const {
   return costs_;
 }
 
 template <typename Scalar>
-const boost::shared_ptr<ConstraintModelManagerTpl<Scalar>>&
+const boost::shared_ptr<ConstraintModelManagerTpl<Scalar> >&
 DifferentialActionModelFreeInvDynamicsTpl<Scalar>::get_constraints() const {
   return constraints_;
 }
 
 template <typename Scalar>
-void DifferentialActionModelFreeFwdDynamicsTpl<Scalar>::ResidualModelRneaTpl<Scalar>::calc(
-    const boost::shared_ptr<DifferentialActionDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
-    const Eigen::Ref<const VectorXs>& u) {
-  d->r = d->pinocchio->tau - d->actuation->tau
+pinocchio::ModelTpl<Scalar>& DifferentialActionModelFreeInvDynamicsTpl<Scalar>::get_pinocchio() const {
+  return pinocchio_;
 }
-
-template <typename Scalar>
-void DifferentialActionModelFreeFwdDynamicsTpl<Scalar>::ResidualModelRneaTpl<Scalar>::calcDiff(
-    const boost::shared_ptr<DifferentialActionDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
-    const Eigen::Ref<const VectorXs>& u) {
-  d->Rx.leftCols(nv) = d->pinocchio->dtau_dq d->Rx.rightCols(nv) = d->pinocchio->dtau_dv d->Rx -=
-      d->shared.actuation->dtau_dx d->Ru.leftCols(nv) = d->pinocchio->M d->Ru.rightCols(nv) = d->actuation->dtau_du
-}
-
-template <typename Scalar>
-boost::shared_ptr<ResidualDataAbstractTpl<Scalar>>
-DifferentialActionModelFreeInvDynamicsTpl<Scalar>::ResidualModelRneaTpl<Scalar>::createData() {
-  return boost::allocate_shared<typename Data::ResidualDataRneaTpl<Scalar>>(
-      Eigen::aligned_allocator<typename Data::ResidualDataRneaTpl<Scalar>>(), this);
-}
-
-// template <typename Scalar>
-// bool DifferentialActionModelFreeInvDynamicsTpl<Scalar>::ResidualModelRneaTpl<Scalar>::checkData(
-//     const boost::shared_ptr<ResidualDataAbstract>& data) {
-//   boost::shared_ptr<typename Data::ResidualDataRneaTpl<Scalar>> d = boost::dynamic_pointer_cast<typename
-//   Data::ResidualDataRneaTpl<Scalar>>(data); if (d != NULL) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
 
 }  // namespace crocoddyl
