80cc3bf86818624bed2ee2fd33da30bbf6e6e0f3
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-09-02 11:24:39 +0100

    [callback] Printed the dynamic-feasibility value (l1 or linf norms)

--- src/core/utils/callbacks.cpp
+++ src/core/utils/callbacks.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2019, LAAS-CNRS
+// Copyright (C) 2019-2021, LAAS-CNRS, University of Edinburgh
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
@@ -19,17 +19,17 @@ void CallbackVerbose::operator()(SolverAbstract& solver) {
     switch (level) {
       case _1: {
         std::cout << "iter \t cost \t      stop \t    grad \t  xreg";
-        std::cout << " \t      ureg \t step \t feas\n";
+        std::cout << " \t      ureg \t step \t  ||ffeas||\n";
         break;
       }
       case _2: {
         std::cout << "iter \t cost \t      stop \t    grad \t  xreg";
-        std::cout << " \t      ureg \t step \t feas \tdV-exp \t      dV\n";
+        std::cout << " \t      ureg \t step \t  ||ffeas|| \tdV-exp \t      dV\n";
         break;
       }
       default: {
         std::cout << "iter \t cost \t      stop \t    grad \t  xreg";
-        std::cout << " \t      ureg \t step \t feas\n";
+        std::cout << " \t      ureg \t step \t  ||ffeas||\n";
       }
     }
   }
@@ -40,8 +40,8 @@ void CallbackVerbose::operator()(SolverAbstract& solver) {
       std::cout << std::scientific << std::setprecision(5) << solver.get_cost() << "  ";
       std::cout << solver.get_stop() << "  " << -solver.get_d()[1] << "  ";
       std::cout << solver.get_xreg() << "  " << solver.get_ureg() << "   ";
-      std::cout << std::fixed << std::setprecision(4) << solver.get_steplength() << "     ";
-      std::cout << solver.get_is_feasible() << '\n';
+      std::cout << std::fixed << std::setprecision(4) << solver.get_steplength() << "  ";
+      std::cout << std::scientific << std::setprecision(5) << solver.get_ffeas() << '\n';
       break;
     }
     case _2: {
@@ -49,8 +49,8 @@ void CallbackVerbose::operator()(SolverAbstract& solver) {
       std::cout << std::scientific << std::setprecision(5) << solver.get_cost() << "  ";
       std::cout << solver.get_stop() << "  " << -solver.get_d()[1] << "  ";
       std::cout << solver.get_xreg() << "  " << solver.get_ureg() << "   ";
-      std::cout << std::fixed << std::setprecision(4) << solver.get_steplength() << "     ";
-      std::cout << solver.get_is_feasible() << "  ";
+      std::cout << std::fixed << std::setprecision(4) << solver.get_steplength() << "  ";
+      std::cout << std::scientific << std::setprecision(5) << solver.get_ffeas() << "  ";
       std::cout << std::scientific << std::setprecision(5) << solver.get_dVexp() << "  ";
       std::cout << solver.get_dV() << '\n';
       break;
@@ -60,8 +60,8 @@ void CallbackVerbose::operator()(SolverAbstract& solver) {
       std::cout << std::scientific << std::setprecision(5) << solver.get_cost() << "  ";
       std::cout << solver.get_stop() << "  " << -solver.get_d()[1] << "  ";
       std::cout << solver.get_xreg() << "  " << solver.get_ureg() << "   ";
-      std::cout << std::fixed << std::setprecision(4) << solver.get_steplength() << "     ";
-      std::cout << solver.get_is_feasible() << '\n';
+      std::cout << std::fixed << std::setprecision(4) << solver.get_steplength() << "  ";
+      std::cout << std::scientific << std::setprecision(5) << solver.get_ffeas() << '\n';
     }
   }
   std::cout << std::flush;
