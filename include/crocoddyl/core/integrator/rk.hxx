255b94d65194b62528cc969a40bef4df197f2b72
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-10-08 13:47:14 +0100

    [cleanup] Cleaned up the integrator code after rebasing

--- include/crocoddyl/core/integrator/rk.hxx
+++ include/crocoddyl/core/integrator/rk.hxx
@@ -83,7 +83,6 @@ void IntegratedActionModelRKTpl<Scalar>::calc(const boost::shared_ptr<ActionData
   state_->integrate(x, d->dx, d->xnext);
   d->g = k0_data->g;
   d->h = k0_data->h;
-
   if (with_cost_residual_) {
     d->r = k0_data->r;
   }
