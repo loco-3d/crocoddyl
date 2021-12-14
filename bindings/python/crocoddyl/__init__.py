[display] Fixed display of friction-cone rotation and mu

bindings/python/crocoddyl/__init__.py
@@ -182,10 +182,10 @@ class GepettoDisplay(DisplayAbstract):
                             R = np.eye(3)
                             mu = 0.7
                             for k, c in model.differential.costs.costs.todict().items():
-                                if isinstance(c.cost, libcrocoddyl_pywrap.CostModelContactFrictionCone):
-                                    if contact.frame == c.cost.reference.id:
-                                        R = c.cost.reference.cone.R
-                                        mu = c.cost.reference.cone.mu
+                                if isinstance(c.cost.residual, libcrocoddyl_pywrap.ResidualModelContactFrictionCone):
+                                    if contact.frame == c.cost.residual.id:
+                                        R = c.cost.residual.reference.R
+                                        mu = c.cost.residual.reference.mu
                                         continue
                             fc.append({"key": str(joint), "oMf": oMf, "f": force, "R": R, "mu": mu})
                     fs.append(fc)
@@ -200,10 +200,10 @@ class GepettoDisplay(DisplayAbstract):
                             R = np.eye(3)
                             mu = 0.7
                             for k, c in model.differential.costs.costs.todict().items():
-                                if isinstance(c.cost, libcrocoddyl_pywrap.CostModelContactFrictionCone):
-                                    if contact.frame == c.cost.reference.id:
-                                        R = c.cost.reference.cone.R
-                                        mu = c.cost.reference.cone.mu
+                                if isinstance(c.cost.residual, libcrocoddyl_pywrap.ResidualModelContactFrictionCone):
+                                    if contact.frame == c.cost.residual.id:
+                                        R = c.cost.residual.reference.R
+                                        mu = c.cost.residual.reference.mu
                                         continue
                             fc.append({"key": str(joint), "oMf": oMf, "f": force, "R": R, "mu": mu})
                     fs.append(fc)
@@ -218,10 +218,10 @@ class GepettoDisplay(DisplayAbstract):
                         R = np.eye(3)
                         mu = 0.7
                         for k, c in model.costs.costs.todict().items():
-                            if isinstance(c.cost, libcrocoddyl_pywrap.CostModelContactFrictionCone):
-                                if impulse.frame == c.cost.id:
-                                    R = c.cost.cone.R
-                                    mu = c.cost.cone.mu
+                            if isinstance(c.cost.residual, libcrocoddyl_pywrap.ResidualModelContactFrictionCone):
+                                if impulse.frame == c.cost.residual.id:
+                                    R = c.cost.residual.reference.R
+                                    mu = c.cost.residual.reference.mu
                                     continue
                         fc.append({"key": str(joint), "oMf": oMf, "f": force, "R": R, "mu": mu})
                 fs.append(fc)