21e09cc146a1a1c4b3d8b12b02bc04b08b08cb5e
Wolfgang Merkt <w.merkt@gmail.com>
2022-03-30 15:50:57 +0100

    Add check_logfiles.sh
    Checks whether log files differ from the ones checked in to git

---
+++ examples/log/check_logfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+exit_code=0
+echo ">>> Checking log files"
+for f in *.log ; do
+    git diff --quiet "$f"
+    if [ $? -eq 0 ]
+    then
+        echo -e "    $f is ${GREEN}OK${NC}"
+    else
+        echo -e "${RED}    $f DIFFERS${NC}"
+        exit_code=1
+    fi
+done
+
+if [ $exit_code -eq 0 ] ; then echo ">>> All log files match." ; fi
+exit $exit_code
