6291fd5310dd5f9139767aecf6b7808cbb393832
PepMS <jmarti@iri.upc.edu>
2022-06-01 11:59:55 +0200

    docs: revert cmake changes and change bindings variable names

--- bindings/python/crocoddyl/core/solvers/ipopt.cpp
+++ bindings/python/crocoddyl/core/solvers/ipopt.cpp
@@ -15,7 +15,7 @@ void exposeSolverIpopt() {
       bp::init<const boost::shared_ptr<crocoddyl::ShootingProblem>&>(bp::args("self", "problem"), "Initialize solver"))
       .def("solve", &SolverIpopt::solve,
            SolverIpopt_solves(
-               bp::args("self", "init_xs", "init_us", "maxiter", "isFeasible", "regInit"),
+               bp::args("self", "init_xs", "init_us", "maxiter", "is_feasible", "init_reg"),
                "Compute the optimal trajectory xopt, uopt as lists of T+1 and T terms.\n\n"
                "From an initial guess init_xs,init_us (feasible or not), iterate\n"
                "over computeDirection and tryStep until stoppingCriteria is below\n"
@@ -24,9 +24,9 @@ void exposeSolverIpopt() {
                ":param init_xs: initial guess for state trajectory with T+1 elements (default []).\n"
                ":param init_us: initial guess for control trajectory with T elements (default []) (default []).\n"
                ":param maxiter: maximum allowed number of iterations (default 100).\n"
-               ":param isFeasible: true if the init_xs are obtained from integrating the init_us (rollout) (default "
+               ":param is_feasible: true if the init_xs are obtained from integrating the init_us (rollout) (default "
                "False).\n"
-               ":param regInit: initial guess for the regularization value. Very low values are typical\n"
+               ":param init_reg: initial guess for the regularization value. Very low values are typical\n"
                "                used with very good guess points (init_xs, init_us) (default None).\n"
                ":returns the optimal trajectory xopt, uopt and a boolean that describes if convergence was reached."))
       .def("setStringIpoptOption", &SolverIpopt::setStringIpoptOption, bp::args("self", "param_name", "param_value"),
