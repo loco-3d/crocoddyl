871b0a933f1f74f0abb8bbbce0d367c769eeb435
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-07 13:51:11 +0100

    nix: apply pinocchio#2566 on macos to fix python imports

--- flake.nix
+++ flake.nix
@@ -11,14 +11,38 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, self', ... }:
         {
+          pkgs,
+          self',
+          system,
+          ...
+        }:
+        {
+          # fix crocoddyl python imports on OSX.
+          # Remove this for pinocchio > 3.3.1
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                pinocchio = prev.pinocchio.overrideAttrs {
+                  patches = final.lib.optionals final.stdenv.hostPlatform.isDarwin [
+                    (final.fetchpatch {
+                      url = "https://github.com/stack-of-tasks/pinocchio/pull/2566/commits/4758b80c0f8937b5ddc270d29267feba7f637f0f.patch";
+                      hash = "sha256-RaRF1Jo9HfxXiJ3GZAgcvV7UBvCpHCS4TJMOQyC1PvY=";
+                    })
+                  ];
+                };
+              })
+            ];
+          };
           apps.default = {
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
           };
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
+            # expose patched pinocchio to dependent packages
+            inherit (pkgs) pinocchio;
             default = self'.packages.crocoddyl;
             crocoddyl = pkgs.python3Packages.crocoddyl.overrideAttrs (_: {
               src = pkgs.lib.fileset.toSource {
