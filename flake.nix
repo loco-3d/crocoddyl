ab7149fd9b11f1d41d8487a15ea95d29b7e19245
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-10 14:57:35 +0100

    release: tomlkit is required

--- flake.nix
+++ flake.nix
@@ -17,7 +17,10 @@
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
           };
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self'.packages.default ];
+            packages = [ (pkgs.python3.withPackages (p: [p.tomlkit])) ]; # for "make release"
+          };
           packages = {
             default = self'.packages.crocoddyl;
             crocoddyl = pkgs.python3Packages.crocoddyl.overrideAttrs (_: {
