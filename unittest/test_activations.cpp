35b6be1c291172deabb89a38124121287bba5556
Carlos Mastalli <carlos.mastalli@gmail.com>
2025-02-20 12:16:39 +0000

    [unittest] Tested cast functions in activations

--- unittest/test_activations.cpp
+++ unittest/test_activations.cpp
@@ -1,9 +1,9 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2019-2023, LAAS-CNRS, New York University, Max Planck
-// Gesellschaft
-//                          University of Edinburgh, INRIA
+// Copyright (C) 2019-2025, LAAS-CNRS, New York University,
+//                          Max Planck Gesellschaft, University of Edinburgh,
+//                          INRIA, Heriot-Watt University
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
@@ -32,7 +32,10 @@ void test_construct_data(ActivationModelTypes::Type activation_type) {
   tmp << *model;
 
   // create the corresponding data object
-  std::shared_ptr<crocoddyl::ActivationDataAbstract> data = model->createData();
+  const std::shared_ptr<crocoddyl::ActivationDataAbstract>& data =
+      model->createData();
+  const std::shared_ptr<crocoddyl::ActivationDataAbstractTpl<float>>&
+      casted_data = model->cast<float>()->createData();
 }
 
 void test_calc_returns_a_value(ActivationModelTypes::Type activation_type) {
@@ -42,7 +45,8 @@ void test_calc_returns_a_value(ActivationModelTypes::Type activation_type) {
       factory.create(activation_type);
 
   // create the corresponding data object
-  std::shared_ptr<crocoddyl::ActivationDataAbstract> data = model->createData();
+  const std::shared_ptr<crocoddyl::ActivationDataAbstract>& data =
+      model->createData();
 
   // Generating random input vector
   const Eigen::VectorXd r = Eigen::VectorXd::Random(model->get_nr());
@@ -53,6 +57,17 @@ void test_calc_returns_a_value(ActivationModelTypes::Type activation_type) {
 
   // Checking that calc returns a value
   BOOST_CHECK(!std::isnan(data->a_value));
+
+  // Checking that casted computation is the same
+  const std::shared_ptr<crocoddyl::ActivationModelAbstractTpl<float>>&
+      casted_model = model->cast<float>();
+  const std::shared_ptr<crocoddyl::ActivationDataAbstractTpl<float>>&
+      casted_data = casted_model->createData();
+  const Eigen::VectorXf r_f = r.cast<float>();
+  casted_data->a_value = float(nan(""));
+  casted_model->calc(casted_data, r_f);
+  BOOST_CHECK(!std::isnan(casted_data->a_value));
+  BOOST_CHECK(std::abs(data->a_value - casted_data->a_value) < 1e-6);
 }
 
 void test_partial_derivatives_against_numdiff(
@@ -63,7 +78,8 @@ void test_partial_derivatives_against_numdiff(
       factory.create(activation_type);
 
   // create the corresponding data object and set the cost to nan
-  std::shared_ptr<crocoddyl::ActivationDataAbstract> data = model->createData();
+  const std::shared_ptr<crocoddyl::ActivationDataAbstract>& data =
+      model->createData();
 
   crocoddyl::ActivationModelNumDiff model_num_diff(model);
   std::shared_ptr<crocoddyl::ActivationDataAbstract> data_num_diff =
@@ -77,6 +93,7 @@ void test_partial_derivatives_against_numdiff(
   model->calcDiff(data, r);
   model_num_diff.calc(data_num_diff, r);
   model_num_diff.calcDiff(data_num_diff, r);
+
   // Tolerance defined as in
   // http://www.it.uom.gr/teaching/linearalgebra/NumericalRecipiesInC/c5-7.pdf
   double tol = std::pow(model_num_diff.get_disturbance(), 1. / 3.);
@@ -85,6 +102,19 @@ void test_partial_derivatives_against_numdiff(
 
   // numerical differentiation of the Hessian is not good enough to be tested.
   // BOOST_CHECK((data->Arr - data_num_diff->Arr).isMuchSmallerThan(1.0, tol));
+
+  // Checking that casted computation is the same
+  const std::shared_ptr<crocoddyl::ActivationModelAbstractTpl<float>>&
+      casted_model = model->cast<float>();
+  const std::shared_ptr<crocoddyl::ActivationDataAbstractTpl<float>>&
+      casted_data = casted_model->createData();
+  const Eigen::VectorXf r_f = r.cast<float>();
+  casted_model->calc(casted_data, r_f);
+  casted_model->calcDiff(casted_data, r_f);
+  float tol_f =
+      std::pow(model_num_diff.cast<float>().get_disturbance(), float(1. / 3.));
+  BOOST_CHECK(std::abs(data->a_value - casted_data->a_value) < tol_f);
+  BOOST_CHECK((data->Ar.cast<float>() - casted_data->Ar).isZero(tol_f));
 }
 
 void test_activation_bounds_with_infinity() {
@@ -104,6 +134,10 @@ void test_activation_bounds_with_infinity() {
              lb);
   crocoddyl::ActivationBounds bounds(lb, ub, beta);
   BOOST_CHECK(bounds.lb != m - beta * d);
+
+  // Checking that casted computation is the same
+  crocoddyl::ActivationBoundsTpl<float> casted_bounds = bounds.cast<float>();
+  BOOST_CHECK(bounds.lb.cast<float>() == casted_bounds.lb);
 }
 
 //----------------------------------------------------------------------------//
