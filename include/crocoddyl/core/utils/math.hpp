0d9c987daf2615992621d9d2445c2b38aebec415
Carlos Mastalli <carlos.mastalli@gmail.com>
2025-03-03 12:40:47 +0000

    [cleanup] Reorganizing math.h file

--- include/crocoddyl/core/utils/math.hpp
+++ include/crocoddyl/core/utils/math.hpp
@@ -11,94 +11,37 @@
 
 #include <Eigen/Dense>
 #include <algorithm>
-#include <boost/type_traits.hpp>
-#include <limits>
-
-#ifdef CROCODDYL_WITH_CODEGEN
-
 #include <cmath>
-#include <cppad/cg/cg.hpp>
-#include <cppad/cppad.hpp>
+#include <limits>
 #include <type_traits>
 
-namespace CppAD {
-template <class Scalar>
-bool isfinite(const AD<CppAD::cg::CG<Scalar>>& x) {
-  return std::isfinite(static_cast<Scalar>(CppAD::Value(x).getValue()));
-}
-}  // namespace CppAD
+#ifdef CROCODDYL_WITH_CODEGEN
+#include <cppad/cg/cg.hpp>
+#include <cppad/cppad.hpp>
+#endif
 
 namespace crocoddyl {
 
-// Case 1: Use std::pow for floating-point types
+// Use std::pow for floating-point types
 template <typename Scalar, typename ExpScalar>
 typename std::enable_if<std::is_floating_point<Scalar>::value, Scalar>::type
 pow(const Scalar& base, const ExpScalar& exponent) {
   return std::pow(base, exponent);
 }
 
-// Case 2: Use CppAD::pow for CppAD types
-template <typename Scalar>
-typename std::enable_if<!std::is_floating_point<Scalar>::value, Scalar>::type
-pow(const Scalar& base, const Scalar& exponent) {
-  return CppAD::pow(base, exponent);
-}
-
-// Case 1: Use std::pow for floating-point types
+// Use std::pow for floating-point types
 template <typename Scalar>
 typename std::enable_if<std::is_floating_point<Scalar>::value, Scalar>::type
 sqrt(const Scalar& base) {
   return std::sqrt(base);
 }
 
-// Case 2: Use CppAD::pow for CppAD types
-template <typename Scalar>
-typename std::enable_if<!std::is_floating_point<Scalar>::value, Scalar>::type
-sqrt(const Scalar& base) {
-  return CppAD::sqrt(base);
-}
-
-// Case 1: Use std::fabs for floating-point types
+// Use std::fabs for floating-point types
 template <typename Scalar>
 typename std::enable_if<std::is_floating_point<Scalar>::value, Scalar>::type
 fabs(const Scalar& base) {
   return std::fabs(base);
 }
-
-// Case 2: Use CppAD::fabs for CppAD types
-template <typename Scalar>
-typename std::enable_if<!std::is_floating_point<Scalar>::value, Scalar>::type
-fabs(const Scalar& base) {
-  return CppAD::fabs(base);
-}
-}  // namespace crocoddyl
-
-namespace Eigen {
-
-// Overload for Eigen::pow with CppAD-compatible types
-template <typename Derived>
-auto pow(const Eigen::ArrayBase<Derived>& base, double exponent) {
-  return base.unaryExpr([exponent](const typename Derived::Scalar& x) {
-    return CppAD::pow(x, typename Derived::Scalar(exponent));
-  });
-}
-
-// Overload for Eigen::sqrt with CppAD-compatible types
-template <typename Derived>
-typename std::enable_if<std::is_base_of<CppAD::cg::CG<typename Derived::Scalar>,
-                                        typename Derived::Scalar>::value,
-                        Eigen::Array<typename Derived::Scalar, Eigen::Dynamic,
-                                     Eigen::Dynamic>>::type
-sqrt(const Eigen::ArrayBase<Derived>& base) {
-  return base.unaryExpr(
-      [](const typename Derived::Scalar& x) { return CppAD::sqrt(x); });
-}
-
-}  // namespace Eigen
-
-#endif
-
-// fwd
 
 template <typename MatrixLike,
           bool value =
@@ -144,5 +87,63 @@ MatrixLike pseudoInverse(
         Eigen::NumTraits<typename MatrixLike::Scalar>::dummy_precision()) {
   return pseudoInverseAlgo<MatrixLike>::run(a, epsilon);
 }
+
+#ifdef CROCODDYL_WITH_CODEGEN
+// Use CppAD::pow for CppAD types
+template <typename Scalar>
+typename std::enable_if<!std::is_floating_point<Scalar>::value, Scalar>::type
+pow(const Scalar& base, const Scalar& exponent) {
+  return CppAD::pow(base, exponent);
+}
+
+// Use CppAD::pow for CppAD types
+template <typename Scalar>
+typename std::enable_if<!std::is_floating_point<Scalar>::value, Scalar>::type
+sqrt(const Scalar& base) {
+  return CppAD::sqrt(base);
+}
+
+// Case 2: Use CppAD::fabs for CppAD types
+template <typename Scalar>
+typename std::enable_if<!std::is_floating_point<Scalar>::value, Scalar>::type
+fabs(const Scalar& base) {
+  return CppAD::fabs(base);
+}
+#endif
+
+}  // namespace crocoddyl
+
+#ifdef CROCODDYL_WITH_CODEGEN
+
+namespace CppAD {
+template <class Scalar>
+bool isfinite(const CppAD::AD<CppAD::cg::CG<Scalar>>& x) {
+  return std::isfinite(static_cast<Scalar>(CppAD::Value(x).getValue()));
+}
+}  // namespace CppAD
+
+namespace Eigen {
+
+// Overload for Eigen::pow with CppAD-compatible types
+template <typename Derived>
+auto pow(const Eigen::ArrayBase<Derived>& base, double exponent) {
+  return base.unaryExpr([exponent](const typename Derived::Scalar& x) {
+    return CppAD::pow(x, typename Derived::Scalar(exponent));
+  });
+}
+
+// Overload for Eigen::sqrt with CppAD-compatible types
+template <typename Derived>
+typename std::enable_if<std::is_base_of<CppAD::cg::CG<typename Derived::Scalar>,
+                                        typename Derived::Scalar>::value,
+                        Eigen::Array<typename Derived::Scalar, Eigen::Dynamic,
+                                     Eigen::Dynamic>>::type
+sqrt(const Eigen::ArrayBase<Derived>& base) {
+  return base.unaryExpr(
+      [](const typename Derived::Scalar& x) { return CppAD::sqrt(x); });
+}
+
+}  // namespace Eigen
+#endif
 
 #endif  // CROCODDYL_CORE_UTILS_MATH_HPP_
