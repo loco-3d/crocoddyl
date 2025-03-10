208d9cf8da2fdc2f2d1d0d0d8c6fa4061f5e5e9a
Carlos Mastalli <carlos.mastalli@gmail.com>
2025-03-10 17:30:58 +0000

    [deprecated] Reused  FDDP and BoxFDDP code in DDP and BoxDDP respectively

--- include/crocoddyl/core/solvers/box-ddp-deprecated.hpp
+++ include/crocoddyl/core/solvers/box-ddp-deprecated.hpp
@@ -10,34 +10,21 @@
 #ifndef CROCODDYL_CORE_SOLVERS_BOX_DDP_DEPRECATED_HPP_
 #define CROCODDYL_CORE_SOLVERS_BOX_DDP_DEPRECATED_HPP_
 
-#include "crocoddyl/core/solvers/box-qp.hpp"
+#include "crocoddyl/core/solvers/box-fddp.hpp"
 #include "crocoddyl/core/solvers/ddp-deprecated.hpp"
 
 namespace crocoddyl {
 
-class SolverBoxDDP : public SolverDDP {
+class SolverBoxDDP : public SolverBoxFDDP {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   DEPRECATED(
       "Do not use SolverBoxDDP. Instead, you should use SolverBoxFDDP(problem, "
       "DynamicsSolverType::SingleShoot)",
-      explicit SolverBoxDDP(std::shared_ptr<ShootingProblem> problem);)
-  virtual ~SolverBoxDDP();
-
-  virtual void computePolicy(const std::size_t t);
-  virtual void forwardPass(const double steplength);
-  virtual void resizeData();
-
-  const std::vector<Eigen::MatrixXd>& get_Quu_inv() const;
-
- protected:
-  void allocateData();
-
-  BoxQP qp_;
-  std::vector<Eigen::MatrixXd> Quu_inv_;
-  std::vector<Eigen::VectorXd> du_lb_;
-  std::vector<Eigen::VectorXd> du_ub_;
+      explicit SolverBoxDDP(
+          std::shared_ptr<ShootingProblem> problem) : SolverBoxFDDP(problem){})
+  virtual ~SolverBoxDDP() = default;
 };
 
 }  // namespace crocoddyl
