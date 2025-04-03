ee0751258bd861816c019c740d01e29ed0f72a4a
Carlos Mastalli <carlos.mastalli@gmail.com>
2025-04-03 15:17:15 +0100

    [python] Enabled t setup the cast visitor for floating-point scalars only

--- bindings/python/crocoddyl/utils/cast.hpp
+++ bindings/python/crocoddyl/utils/cast.hpp
@@ -25,11 +25,11 @@ namespace bp = boost::python;
  * @brief Add the Python method cast to allow casting of this by predefined cast
  * types.
  */
-template <typename Model>
-struct CastVisitor : public bp::def_visitor<CastVisitor<Model>> {
+template <typename Model, bool FTypesOnly = false>
+struct CastVisitor : public bp::def_visitor<CastVisitor<Model, FTypesOnly>> {
   template <class PyClass>
   void visit(PyClass& cl) const {
-    cl.def("cast", &cast_instance, bp::arg("dtype"),
+    cl.def("cast", &cast_instance<FTypesOnly>, bp::arg("dtype"),
            "Returns a copy of *this.");
   }
 
@@ -48,7 +48,9 @@ struct CastVisitor : public bp::def_visitor<CastVisitor<Model>> {
   }
 
   // Main cast instance function that uses SFINAE
-  static bp::object cast_instance(const Model& self, DType dtype) {
+  template <bool IsFTypesOnly = FTypesOnly>
+  static typename std::enable_if<!IsFTypesOnly, bp::object>::type cast_instance(
+      const Model& self, DType dtype) {
     switch (dtype) {
       case DType::Float64:
         return cast_instance_impl<Float64>(
@@ -61,6 +63,23 @@ struct CastVisitor : public bp::def_visitor<CastVisitor<Model>> {
         return cast_instance_impl<ADFloat64>(
             self, std::is_same<typename Model::Scalar, ADFloat64>());
 #endif
+      default:
+        PyErr_SetString(PyExc_TypeError, "Unsupported dtype.");
+        bp::throw_error_already_set();
+        return bp::object();
+    }
+  }
+
+  template <bool IsFTypesOnly = FTypesOnly>
+  static typename std::enable_if<IsFTypesOnly, bp::object>::type cast_instance(
+      const Model& self, DType dtype) {
+    switch (dtype) {
+      case DType::Float64:
+        return cast_instance_impl<Float64>(
+            self, std::is_same<typename Model::Scalar, Float64>());
+      case DType::Float32:
+        return cast_instance_impl<Float32>(
+            self, std::is_same<typename Model::Scalar, Float32>());
       default:
         PyErr_SetString(PyExc_TypeError, "Unsupported dtype.");
         bp::throw_error_already_set();
