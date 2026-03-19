03cc95968694d23d643b466ab3c4e40f0836163c
Carlos Mastalli <carlos.mastalli@gmail.com>
2026-03-19 16:02:41 +0000

    [activation] This might solve ambiguity in pow

--- include/crocoddyl/core/actuation/squashing/smooth-sat.hpp
+++ include/crocoddyl/core/actuation/squashing/smooth-sat.hpp
@@ -51,8 +51,8 @@ class SquashingModelSmoothSatTpl : public SquashingModelAbstractTpl<_Scalar> {
     // s(u) = 0.5*(lb + ub + sqrt(smooth + (u - lb)^2) - sqrt(smooth + (u -
     // ub)^2))
     data->u = Scalar(0.5) *
-              (Eigen::sqrt(Eigen::pow((s - u_lb_).array(), 2) + a_.array()) -
-               Eigen::sqrt(Eigen::pow((s - u_ub_).array(), 2) + a_.array()) +
+              (Eigen::sqrt(Eigen::pow((s - u_lb_).array(), 2.) + a_.array()) -
+               Eigen::sqrt(Eigen::pow((s - u_ub_).array(), 2.) + a_.array()) +
                u_lb_.array() + u_ub_.array());
   }
 
@@ -60,11 +60,11 @@ class SquashingModelSmoothSatTpl : public SquashingModelAbstractTpl<_Scalar> {
                         const Eigen::Ref<const VectorXs>& s) override {
     data->du_ds.diagonal() =
         Scalar(0.5) *
-        (Eigen::pow(a_.array() + Eigen::pow((s - u_lb_).array(), 2),
+        (Eigen::pow(a_.array() + Eigen::pow((s - u_lb_).array(), 2.),
                     Scalar(-0.5))
                  .array() *
              (s - u_lb_).array() -
-         Eigen::pow(a_.array() + Eigen::pow((s - u_ub_).array(), 2),
+         Eigen::pow(a_.array() + Eigen::pow((s - u_ub_).array(), 2.),
                     Scalar(-0.5))
                  .array() *
              (s - u_ub_).array());
